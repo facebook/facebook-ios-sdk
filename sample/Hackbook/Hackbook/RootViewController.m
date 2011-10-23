05e41f26179fc773c4701dfb02d83a505a316e4a
Daniel Cohen Gindi <Danielgindi@gmail.com>
2011-10-23 14:44:54 +0300

    Wrong enum...

--- sample/Hackbook/Hackbook/RootViewController.m
+++ sample/Hackbook/Hackbook/RootViewController.m
@@ -325,7 +325,7 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     if (cell == nil) {
         cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
-        cell.selectionStyle = UITableViewCellEditingStyleNone;
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
     }
     
     //create the button
