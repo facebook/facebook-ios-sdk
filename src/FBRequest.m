bae4f079f8a94e626cd96b02edc60f3eb3336caf
Grzegorz Adam Hankiewicz <gradha@argon.sabren.com>
2011-12-11 19:31:29 +0100

    Updates SBJSON references to latest versions.
    The previous include and class don't seem to exist any more in recent
    versions of the SBJSON framework.

--- src/FBRequest.m
+++ src/FBRequest.m
@@ -15,7 +15,7 @@
  */
 
 #import "FBRequest.h"
-#import "JSON.h"
+#import "SBJSON.h"
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // global
@@ -180,7 +180,7 @@ static const NSTimeInterval kTimeoutInterval = 180.0;
   NSString* responseString = [[[NSString alloc] initWithData:data
                                                     encoding:NSUTF8StringEncoding]
                               autorelease];
-  SBJSON *jsonParser = [[SBJSON new] autorelease];
+  SBJsonParser *jsonParser = [[SBJsonParser new] autorelease];
   if ([responseString isEqualToString:@"true"]) {
     return [NSDictionary dictionaryWithObject:@"true" forKey:@"result"];
   } else if ([responseString isEqualToString:@"false"]) {
