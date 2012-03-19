360dff1d3e9ebbd252e9c9316e77e5ada2ddc4f3
David B. Bitton <david@codenoevil.com>
2012-03-18 22:15:02 -0400

    changed product to framework from static library

--- src/FBDialog.h
+++ src/FBDialog.h
@@ -18,7 +18,7 @@
 #import <Foundation/Foundation.h>
 #if TARGET_OS_IPHONE
 #import <UIKit/UIKit.h>
-#elif TARGET_OS_MAC
+#elif TARGET_OS_MAC && !TARGET_OS_IPHONE
 #import <AppKit/AppKit.h>
 #import <WebKit/WebKit.h>
 #endif
