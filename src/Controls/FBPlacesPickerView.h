4c82fa24880316eade8c35957cae7961bd7025f3
Vijaye Raji <vijaye@fb.com>
2012-04-03 11:37:45 -0700

    Places picker control - Headers
    Summary:
    Headers for review
    
    FBPlacesPicker: A UI control that allows users to pick a place.  The notification is sent via a delegate that clients can listen to and dismiss the view controller that hosts this view.
    
    (more comments inline)
    
    Test Plan: None for headers
    
    Reviewers: jacl, mmarucheck, gregschechte
    
    Reviewed By: jacl
    
    CC: ekoneil
    
    Differential Revision: https://phabricator.fb.com/D441662

---
+++ src/Controls/FBPlacesPickerView.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2010 Facebook
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+#import "FBPlace.h"
+#import "FBSession.h"
+
+@class FBPlacesPickerView;
+
+@protocol FBPlacesPickerActionDelegate <NSObject>
+
+@required
+
+// Informs the delegate that a place has been picked
+- (void) placesPicker:(FBPlacesPickerView*)placesPicker
+    didPickPlace:(NSDictionary*)place;
+
+@end
+
+
+@interface FBPlacesPickerView : UIView
+
+@property (retain, nonatomic) FBSession* session;
+@property (copy, nonatomic) NSString* searchText;
+@property (assign, nonatomic) NSInteger maxCount;
+@property (assign, nonatomic) NSInteger radius;
+@property (assign, nonatomic) CLLocationCoordinate2D locationCoordinate;
+
+@property (assign, nonatomic) id delegate;
+
+// inits and returns a FBPlacesPickerView
+//
+// Summary:
+//  Initializes the Places picker with a valid session, a location coordinate
+//  and a search text that will be used to filter the places.
+- (id)init;
+- (id)initWithSession:(FBSession*)session;
+- (id)initWithSession:(FBSession*)session
+             location:(CLLocationCoordinate2D)location
+           searchText:(NSString*)searchText;
+           
+- (void)loadData;
+
+@end
