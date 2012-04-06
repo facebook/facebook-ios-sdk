b016791d487b8cc31ce2670620df751673617e52
Greg Schechter <greg@schechterfamily.net>
2012-04-06 13:24:18 -0700

    MyProfile sample to be used for FB Template
    Summary:
    Simple sample with a nav bar, Login, ProfilePic control, and first name display.
    
    Will use as the basis for the Xcode FacebookApp template, but need a review on this first.
    
    Notes:
    - Need to create the .xib for iPhone.  Right now iPad only.  Will do so while this is out for review.
    - Q: do we want the Apache copyright on these samples (the template shouldn't get it, but what about these samples)?
    
    Test Plan: Testing on iPad, need to do iPhone.
    
    Reviewers: jacl, mmarucheck, vijaye, ekoneil
    
    Reviewed By: jacl
    
    Differential Revision: https://phabricator.fb.com/D444527
    
    Revert Plan: Revert

---
+++ samples/MyProfileSample/MyProfile/MPViewController.h
@@ -0,0 +1,13 @@
+//
+//  MPViewController.h
+//  MyProfile
+//
+//  Created by Greg Schechter on 4/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPViewController : UIViewController
+
+@end
---
+++ samples/MyProfileSample/MyProfile/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyProfile
+//
+//  Created by Greg Schechter on 4/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MPAppDelegate class]));
+    }
+}
