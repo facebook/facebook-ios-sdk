2f0ab15d56acb1f3d5480cbf1b792a2dcae225a0
Greg Schechter <greg@schechterfamily.net>
2012-04-08 23:13:59 -0700

    Re-submitting MyProfileSample
    Summary:
    MyProfileSample got yanked on Friday due to issues with #include/#import paths.  Re-submitting it here again.  Pretty much identical to what went in with D444527, except I changed the header comments to be Apache source license ones.
    
    The one technical question I have for you guys:  I built this sample with ARC (which I think we probably should do for all of our samples, just to keep them as simple and focused as possible).  For ARC, I don't know if I want to be doing ##(strong, nonatomic)## for my @property, ##(retain, nonatomic)##, or something else.  Suggestions?
    
    Also, this currently hits a bug in the profile picture control, where setting userid to nil doesn't set the profile picture to the anonymous one.
    
    This sample will still be the basis for an Xcode Template.
    
    Test Plan: Testing against iPhone and iPad.
    
    Reviewers: jacl, mmarucheck, vijaye, ekoneil
    
    Reviewed By: jacl
    
    Differential Revision: https://phabricator.fb.com/D445367
    
    Revert Plan: Revert

---
+++ samples/MyProfileSample/MyProfileSample/MPAppDelegate.m
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2012 Facebook
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import "MPAppDelegate.h"
+
+#import "MPViewController.h"
+#import <FBiOSSDK/FBProfilePictureView.h>
+
+@interface MPAppDelegate ()
+@end		  
+
+@implementation MPAppDelegate
+
+@synthesize window = _window;
+@synthesize viewController = _viewController;
+@synthesize session = _session;
+
+
+// FBSample logic
+// if we have a valid session at the time of openURL call, we handle Facebook transitions
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    // attempt to extract a token from the url
+    return [self.session handleOpenURL:url]; 
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    // FBSample logic
+    // if the app is going away, we invalidate the token if present
+    [self.session invalidate];
+}
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    // BUG:
+    // Nib files require the type to have been loaded before they can do the
+    // wireup successfully.  
+    // http://stackoverflow.com/questions/1725881/unknown-class-myclass-in-interface-builder-file-error-at-runtime
+    [FBProfilePictureView class];
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        self.viewController = [[MPViewController alloc] initWithNibName:@"MPViewController_iPhone" bundle:nil];
+    } else {
+        self.viewController = [[MPViewController alloc] initWithNibName:@"MPViewController_iPad" bundle:nil];
+    }
+    self.window.rootViewController = self.viewController;
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+
+
+@end
