98d983513f44104dc379fe12ce91ee3672d3de7b
GaryRudolph <GaryRudolph@mac.com>
2012-04-12 14:46:45 -0700

    Updated license for compliance

--- src/FBLoginDialog.m
+++ src/FBLoginDialog.m
@@ -1,5 +1,6 @@
 /*
  * Copyright 2010 Facebook
+ * Copyright 2012 Lolay, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
