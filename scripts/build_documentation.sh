29b06759c1b275a35edb8758d1db58f89b0aeaf9
Chris Lang <clang@fb.com>
2012-06-18 12:53:03 -0700

    [ios-sdk] Work around appledoc bug that dropped protocol from property declarations.
    Summary:
    Appledoc has a bug that caused the protocol name to be dropped from property declarations, leaving
    them looking like 'id<> delegate' rather than 'id<SomeProtocol> delegate'. Until the bug gets fixed,
    hacking around it by explicitly replacing the wrong string with the right string.
    
    Test Plan:
    - Ran build_documentation.sh, looked at resulting HTML, saw protocol names
    
    Revert Plan:
    
    Reviewers: jacl, mmarucheck, gregschechte, ayden, caabernathy
    
    Reviewed By: jacl
    
    Differential Revision: https://phabricator.fb.com/D497310

--- scripts/build_documentation.sh
+++ scripts/build_documentation.sh
@@ -38,13 +38,17 @@ test -d $FB_SDK_BUILD \
 cd $FB_SDK_SRC
 
 APPLEDOC=appledoc
+DOCSET="$FB_SDK_BUILD"/com.facebook.Facebook-SDK-3_0-for-iOS.docset
+
+rm -rf $DOCSET
+
 hash $APPLEDOC &>/dev/null
 if [ "$?" -eq "0" ]; then
     APPLEDOC_DOCSET_NAME="Facebook SDK 3.0 for iOS"
     $APPLEDOC --project-name "$APPLEDOC_DOCSET_NAME" \
 	--project-company "Facebook" \
 	--company-id "com.facebook" \
-	--output "$FB_SDK_BUILD"/com.facebook.Facebook-SDK-3_0-for-iOS.docset \
+	--output "$DOCSET" \
 	--preprocess-headerdoc \
 	--docset-bundle-name "$APPLEDOC_DOCSET_NAME" \
 	--docset-feed-name "$APPLEDOC_DOCSET_NAME" \
@@ -59,6 +63,22 @@ if [ "$?" -eq "0" ]; then
 else
     die "appledoc not installed, unable to build documentation"
 fi
+
+# Temporary workaround to an appledoc bug that drops protocol names.
+function replace_string() {
+    perl -pi -e "s/$1/$2/" $3
+}
+
+DOCSDIR="$DOCSET"/docset/Contents/Resources/Documents
+replace_string 'id&lt;&gt; delegate' 'id&lt;FBFriendPickerDelegate&gt; delegate' "$DOCSDIR"/Classes/FBFriendPickerViewController.html
+replace_string 'id&lt;&gt; delegate' 'id&lt;FBPlacePickerDelegate&gt; delegate' "$DOCSDIR"/Classes/FBPlacePickerViewController.html
+replace_string 'id&lt;&gt; selection' 'id&lt;FBGraphPlace&gt; selection' "$DOCSDIR"/Classes/FBPlacePickerViewController.html
+replace_string 'id&lt;&gt; graphObject' 'id&lt;FBGraphObject&gt; graphObject' "$DOCSDIR"/Classes/FBRequest.html
+replace_string 'id&lt;&gt; application' 'id&lt;FBGraphObject&gt; application' "$DOCSDIR"/Protocols/FBOpenGraphAction.html
+replace_string 'id&lt;&gt; from' 'id&lt;FBGraphUser&gt; from' "$DOCSDIR"/Protocols/FBOpenGraphAction.html
+replace_string 'id&lt;&gt; place' 'id&lt;FBGraphPlace&gt; place' "$DOCSDIR"/Protocols/FBOpenGraphAction.html
+replace_string 'id&lt;&gt; location' 'id&lt;FBGraphLocation&gt; location' "$DOCSDIR"/Protocols/FBGraphUser.html
+replace_string 'id&lt;&gt; location' 'id&lt;FBGraphLocation&gt; location' "$DOCSDIR"/Protocols/FBGraphPlace.html
 
 # -----------------------------------------------------------------------------
 # Done
