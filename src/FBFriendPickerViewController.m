facebook/facebook-ios-sdk

commit 94ba6dfeff09e9f2e36bf644de7e2cb7bbcf7d92

Some memory related cleanup
Summary: Did some review and found a few issues with memory management in the view controllers.

Test Plan: Code review

Reviewers: clang, gregschechte, mmarucheck

Reviewed By: clang

CC: msdkexp@

Differential Revision: https://phabricator.fb.com/D516460

Task ID: 1011836

src/FBFriendPickerViewController.m
@@ -65,7 +65,7 @@ int const FBRefreshCacheDelaySeconds = 2;
 
 - (id)init
 {
-    [super init];
+    self = [super init];
 
     if (self) {
         [self initialize];
@@ -76,7 +76,7 @@ int const FBRefreshCacheDelaySeconds = 2;
 
 - (id)initWithCoder:(NSCoder *)aDecoder
 {
-    [super initWithCoder:aDecoder];
+    self = [super initWithCoder:aDecoder];
     
     if (self) {
         [self initialize];
@@ -87,7 +87,7 @@ int const FBRefreshCacheDelaySeconds = 2;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
 
     if (self) {
         [self initialize];
@@ -99,22 +99,24 @@ int const FBRefreshCacheDelaySeconds = 2;
 - (void)initialize
 {
     // Data Source
-    FBGraphObjectTableDataSource *dataSource = [[FBGraphObjectTableDataSource alloc]
-                                                init];
+    FBGraphObjectTableDataSource *dataSource = [[[FBGraphObjectTableDataSource alloc]
+                                                 init]
+                                                autorelease];
     dataSource.defaultPicture = [UIImage imageNamed:defaultImageName];
     dataSource.controllerDelegate = self;
     dataSource.itemTitleSuffixEnabled = YES;
-    self.dataSource = dataSource;
 
     // Selection Manager
-    FBGraphObjectTableSelection *selectionManager = [[FBGraphObjectTableSelection alloc]
-                                                     initWithDataSource:dataSource];
+    FBGraphObjectTableSelection *selectionManager = [[[FBGraphObjectTableSelection alloc]
+                                                      initWithDataSource:dataSource]
+                                                     autorelease];
     selectionManager.delegate = self;
 
     // Paging loader
-    self.loader = [[FBGraphObjectPagingLoader alloc] initWithDataSource:self.dataSource
-                                                             pagingMode:FBGraphObjectPagingModeImmediate];
-    [_loader release];
+    id loader = [[[FBGraphObjectPagingLoader alloc] initWithDataSource:self.dataSource
+                                                            pagingMode:FBGraphObjectPagingModeImmediate]
+                 autorelease];
+    self.loader = loader;
     self.loader.delegate = self;
 
     // Self
@@ -126,10 +128,6 @@ int const FBRefreshCacheDelaySeconds = 2;
     self.userID = @"me";
     self.sortOrdering = FBFriendSortByFirstName;
     self.displayOrdering = FBFriendDisplayByFirstName;
-    
-    // cleanup
-    [selectionManager release];
-    [dataSource release];
 }
 
 - (void)dealloc
@@ -199,24 +197,24 @@ int const FBRefreshCacheDelaySeconds = 2;
     CGRect bounds = self.view.bounds;
 
     if (!self.tableView) {
-        UITableView *tableView = [[UITableView alloc] initWithFrame:bounds];
+        UITableView *tableView = [[[UITableView alloc] initWithFrame:bounds] autorelease];
         tableView.autoresizingMask =
             UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
 
         self.tableView = tableView;
         [self.view addSubview:tableView];
-        [tableView release];
     }
 
     if (!self.spinner) {
-        UIActivityIndicatorView *spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+        UIActivityIndicatorView *spinner = [[[UIActivityIndicatorView alloc]
+                                             initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray]
+                                            autorelease];
         spinner.hidesWhenStopped = YES;
         // We want user to be able to scroll while we load.
         spinner.userInteractionEnabled = NO;
         
         self.spinner = spinner;
         [self.view addSubview:spinner];
-        [spinner release];
     }
 
     self.selectionManager.allowsMultipleSelection = self.allowsMultipleSelection;

src/FBPlacePickerViewController.m
@@ -72,7 +72,7 @@ static NSString *defaultImageName =
 
 - (id)init
 {
-    [super init];
+    self = [super init];
 
     if (self) {
         [self initialize];
@@ -83,7 +83,7 @@ static NSString *defaultImageName =
 
 - (id)initWithCoder:(NSCoder *)aDecoder
 {
-    [super initWithCoder:aDecoder];
+    self = [super initWithCoder:aDecoder];
     
     if (self) {
         [self initialize];
@@ -94,7 +94,7 @@ static NSString *defaultImageName =
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
 
 
     if (self) {
@@ -107,22 +107,24 @@ static NSString *defaultImageName =
 - (void)initialize
 {
     // Data Source
-    FBGraphObjectTableDataSource *dataSource = [[FBGraphObjectTableDataSource alloc]
-                                                init];
+    FBGraphObjectTableDataSource *dataSource = [[[FBGraphObjectTableDataSource alloc]
+                                                 init]
+                                                autorelease];
     dataSource.defaultPicture = [UIImage imageNamed:defaultImageName];
     dataSource.controllerDelegate = self;
     dataSource.itemSubtitleEnabled = YES;
-    self.dataSource = dataSource;
 
     // Selection Manager
-    FBGraphObjectTableSelection *selectionManager = [[FBGraphObjectTableSelection alloc]
-                                                     initWithDataSource:dataSource];
+    FBGraphObjectTableSelection *selectionManager = [[[FBGraphObjectTableSelection alloc]
+                                                      initWithDataSource:dataSource]
+                                                     autorelease];
     selectionManager.delegate = self;
 
     // Paging loader
-    self.loader = [[FBGraphObjectPagingLoader alloc] initWithDataSource:self.dataSource
-                                                             pagingMode:FBGraphObjectPagingModeAsNeeded];
-    [_loader release];
+    id loader = [[[FBGraphObjectPagingLoader alloc] initWithDataSource:self.dataSource
+                                                            pagingMode:FBGraphObjectPagingModeAsNeeded]
+                 autorelease];
+    self.loader = loader;
     self.loader.delegate = self;
 
     // Self
@@ -133,10 +135,6 @@ static NSString *defaultImageName =
     self.resultsLimit = defaultResultsLimit;
     self.radiusInMeters = defaultRadius;
     self.itemPicturesEnabled = YES;
-
-    // cleanup
-    [selectionManager release];
-    [dataSource release];
 }
 
 - (void)dealloc
@@ -240,7 +238,8 @@ static NSString *defaultImageName =
                                                               radiusInMeters:radiusInMeters
                                                                   searchText:searchText
                                                                 resultsLimit:resultsLimit
-                                                            fieldsForRequest:fieldsForRequest] autorelease];
+                                                            fieldsForRequest:fieldsForRequest]
+            autorelease];
 }
 
 #pragma mark - private methods
@@ -253,24 +252,24 @@ static NSString *defaultImageName =
     CGRect bounds = self.view.bounds;
 
     if (!self.tableView) {
-        UITableView *tableView = [[UITableView alloc] initWithFrame:bounds];
+        UITableView *tableView = [[[UITableView alloc] initWithFrame:bounds] autorelease];
         tableView.autoresizingMask =
             UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
         
         self.tableView = tableView;
         [self.view addSubview:tableView];
-        [tableView release];
     }
 
     if (!self.spinner) {
-        UIActivityIndicatorView *spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+        UIActivityIndicatorView *spinner = [[[UIActivityIndicatorView alloc]
+                                             initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray]
+                                            autorelease];
         spinner.hidesWhenStopped = YES;
         // We want user to be able to scroll while we load.
         spinner.userInteractionEnabled = NO;
 
         self.spinner = spinner;
         [self.view addSubview:spinner];
-        [spinner release];
     }
 
     self.tableView.delegate = self.selectionManager;

src/FBFriendPickerCacheDescriptor.m
@@ -78,8 +78,7 @@
 
 - (void)prefetchAndCacheForSession:(FBSession*)session {
     // datasource has some field ownership, so we need one here
-    FBGraphObjectTableDataSource *datasource = [[FBGraphObjectTableDataSource alloc]
-                                                init];
+    FBGraphObjectTableDataSource *datasource = [[[FBGraphObjectTableDataSource alloc] init] autorelease];
     datasource.groupByField = @"name";
     
     // me or one of my friends that also uses the app
@@ -101,10 +100,10 @@
     }
     
     self.loader.delegate = nil;
-    self.loader = [[FBGraphObjectPagingLoader alloc] initWithDataSource:datasource
-                                                             pagingMode:FBGraphObjectPagingModeImmediateViewless];
+    self.loader = [[[FBGraphObjectPagingLoader alloc] initWithDataSource:datasource
+                                                              pagingMode:FBGraphObjectPagingModeImmediateViewless]
+                   autorelease];
     self.loader.session = session;
-    [self.loader release];
     
     self.loader.delegate = self;
     
@@ -115,8 +114,6 @@
     [self.loader startLoadingWithRequest:request
                            cacheIdentity:FBFriendPickerCacheIdentity
                    skipRoundtripIfCached:NO];
-    
-    [datasource release];
 }
 
 - (void)setUsePageLimitOfOne {

src/FBLoginView.m
@@ -386,15 +386,15 @@ NSString *const FBLoginViewCacheIdentity = @"FBLoginView";
             [FBSession sessionOpenWithPermissions:self.permissions completionHandler:nil];
         } else { // logout action sheet
             NSString *name = self.user.name;
-            UIActionSheet *sheet = [[UIActionSheet alloc] initWithTitle:[NSString stringWithFormat:@"Logged in %@",
-                                                                         name ? [NSString stringWithFormat:@"as %@", name] : @"using Facebook"]
-                                                               delegate:self
-                                                      cancelButtonTitle:@"Cancel"
-                                                 destructiveButtonTitle:@"Log Out"
-                                                      otherButtonTitles:nil];                
+            UIActionSheet *sheet = [[[UIActionSheet alloc] initWithTitle:[NSString stringWithFormat:@"Logged in %@",
+                                                                          name ? [NSString stringWithFormat:@"as %@", name] : @"using Facebook"]
+                                                                delegate:self
+                                                       cancelButtonTitle:@"Cancel"
+                                                  destructiveButtonTitle:@"Log Out"
+                                                       otherButtonTitles:nil]
+                                    autorelease];
             // Show the sheet
             [sheet showInView:self];
-            [sheet release];
         }
     } else { // state of view out of sync with active session
         // so resync

src/FBPlacePickerCacheDescriptor.m
@@ -69,8 +69,7 @@
 
 - (void)prefetchAndCacheForSession:(FBSession*)session {
     // datasource has some field ownership, so we need one here
-    FBGraphObjectTableDataSource *datasource = [[FBGraphObjectTableDataSource alloc]
-                                                init];
+    FBGraphObjectTableDataSource *datasource = [[[FBGraphObjectTableDataSource alloc] init] autorelease];
     //datasource.groupByField = @"name";
     
     // create the request object that we will start with
@@ -83,12 +82,10 @@
                                                                                  session:session];
     
     self.loader.delegate = nil;
-    self.loader = [[FBGraphObjectPagingLoader alloc]
-                   initWithDataSource:datasource
-                           pagingMode:FBGraphObjectPagingModeImmediateViewless];
+    self.loader = [[[FBGraphObjectPagingLoader alloc] initWithDataSource:datasource
+                                                              pagingMode:FBGraphObjectPagingModeImmediateViewless]
+                   autorelease];
     self.loader.session = session;
-    [self.loader release];
-    
     self.loader.delegate = self;
     
     // make sure we are around to handle the delegate call
@@ -98,8 +95,6 @@
     [self.loader startLoadingWithRequest:request
                            cacheIdentity:FBPlacePickerCacheIdentity
                    skipRoundtripIfCached:NO];
-    
-    [datasource release];
 }
 
 - (void)pagingLoaderDidFinishLoading:(FBGraphObjectPagingLoader *)pagingLoader {
@@ -107,7 +102,7 @@
     self.loader = nil;
     self.hasCompletedFetch = YES;
     
-    // this feels like suicide!
+    // achieving detachment
     [self release];    
 }
 

src/FBProfilePictureView.m
@@ -117,10 +117,9 @@
         return;
     }
     
-    UIImageView* imageView = [[UIImageView alloc] initWithFrame:self.bounds];
+    UIImageView* imageView = [[[UIImageView alloc] initWithFrame:self.bounds] autorelease];
     imageView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     self.imageView = imageView;
-    [imageView release];
 
     self.autoresizesSubviews = YES;
     self.clipsToBounds = YES;
@@ -163,9 +162,9 @@
                                newImageSize];
         NSURL *url = [NSURL URLWithString:urlString];
         
-        self.connection = [[[FBURLConnection alloc]
-                             initWithURL:url
-                             completionHandler:handler] autorelease];
+        self.connection = [[[FBURLConnection alloc] initWithURL:url
+                                              completionHandler:handler]
+                           autorelease];
     } else {
         NSString *blankImageName = 
             [NSString 

src/FBSession.m
@@ -309,8 +309,7 @@ static NSSet *g_loggingBehavior;
           self.state == FBSessionStateCreatedTokenLoaded)) {
         // login may only be called once, and only from one of the two initial states
         [[NSException exceptionWithName:FBInvalidOperationException
-                                 reason:@"FBSession: an attempt was made to login an already logged in or invalid"
-                                        @"session"
+                                 reason:@"FBSession: an attempt was made to open an already opened or closed session"
                                userInfo:nil]
          raise];
     }

src/FBTestSession.m
@@ -247,10 +247,11 @@ tokenCachingStrategy:(FBSessionTokenCachingStrategy*)tokenCachingStrategy
 {
     NSDictionary *userInfo = nil;
     if (innerError) {
-        userInfo = [[NSDictionary dictionaryWithObjectsAndKeys:
-                    innerError, FBErrorInnerErrorKey, 
-                    nil] autorelease];
+        userInfo = [NSDictionary dictionaryWithObjectsAndKeys:
+                        innerError, FBErrorInnerErrorKey,
+                        nil];
     }
+    
     [[NSException exceptionWithName:FBInvalidOperationException
                              reason:@"FBTestSession encountered an error"
                            userInfo:userInfo]

commit 6192c59e2df602836d40ed6e2d59c87068361066

Fixes a bug in the pickers
Summary:
Change 94ba6df introduced a regression in the pickers that caused their data
not to display; this fixes that

Test Plan: tried the pickers in scrumptious

Reviewers: clang, ayden

Reviewed By: clang

CC: msdkexp@

Differential Revision: https://phabricator.fb.com/D520551

Task ID: 1193295

src/FBFriendPickerViewController.m
@@ -106,7 +106,7 @@ int const FBRefreshCacheDelaySeconds = 2;
     dataSource.defaultPicture = [UIImage imageNamed:defaultImageName];
     dataSource.controllerDelegate = self;
     dataSource.itemTitleSuffixEnabled = YES;
-
+    
     // Selection Manager
     FBGraphObjectTableSelection *selectionManager = [[[FBGraphObjectTableSelection alloc]
                                                       initWithDataSource:dataSource]
@@ -114,7 +114,7 @@ int const FBRefreshCacheDelaySeconds = 2;
     selectionManager.delegate = self;
 
     // Paging loader
-    id loader = [[[FBGraphObjectPagingLoader alloc] initWithDataSource:self.dataSource
+    id loader = [[[FBGraphObjectPagingLoader alloc] initWithDataSource:dataSource
                                                             pagingMode:FBGraphObjectPagingModeImmediate]
                  autorelease];
     self.loader = loader;

src/FBPlacePickerViewController.m
@@ -122,7 +122,7 @@ static NSString *defaultImageName =
     selectionManager.delegate = self;
 
     // Paging loader
-    id loader = [[[FBGraphObjectPagingLoader alloc] initWithDataSource:self.dataSource
+    id loader = [[[FBGraphObjectPagingLoader alloc] initWithDataSource:dataSource
                                                             pagingMode:FBGraphObjectPagingModeAsNeeded]
                  autorelease];
     self.loader = loader;