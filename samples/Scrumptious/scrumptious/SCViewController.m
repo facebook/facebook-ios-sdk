98c0dc9eecf22e63ca46ce0a4e296b056e85206c
aydenw <ayden.wong.zy@gmail.com>
2012-08-06 16:47:51 -0700

    Fix image previews in Scrumptious (and other minor changes)
    Summary:
    Fixed image previews to scale with aspect ratio instead of stretching
    Fixed orientation of uploaded photos
    Set the announce button to disabled when uploading photo
    Changed capitalization of "Select one" to be consistent
    
    Test Plan: Ran Scrumptious; specifically tested uploading photos
    
    Reviewers: jacl, clang
    
    Reviewed By: jacl
    
    CC: msdkexp@, bhiller
    
    Differential Revision: https://phabricator.fb.com/D545658
    
    Task ID: 1176760

--- samples/Scrumptious/scrumptious/SCViewController.m
+++ samples/Scrumptious/scrumptious/SCViewController.m
@@ -167,6 +167,7 @@
                                          self.selectedMeal = nil;
                                          self.selectedPlace = nil;
                                          self.selectedFriends = nil;
+                                         self.selectedPhoto = nil;
                                          [self updateSelections];
                                      } else {
                                          alertText = [NSString stringWithFormat:@"error: domain = %@, code = %d",
@@ -218,6 +219,7 @@
 // Handles the user clicking the Announce button, by either creating an Open Graph Action
 // or first uploading a photo and then creating the action.
 - (IBAction)announce:(id)sender {
+    self.announceButton.enabled = false;
     [self centerAndShowActivityIndicator];
     [self.view setUserInteractionEnabled:NO];
     
@@ -257,6 +259,7 @@
     } else {
         [self dismissModalViewControllerAnimated:YES];
     }
+    self.photoViewController = nil;
 }
 
 #pragma mark -
@@ -327,10 +330,10 @@
 - (void)updateSelections {
     [self updateCellIndex:0 withSubtitle:(self.selectedMeal ?
                                           self.selectedMeal : 
-                                          @"Select One")];
+                                          @"Select one")];
     [self updateCellIndex:1 withSubtitle:(self.selectedPlace ?
                                           self.selectedPlace.name :
-                                          @"Select One")];
+                                          @"Select one")];
     
     NSString *friendsSubtitle = @"Select friends";
     int friendCount = self.selectedFriends.count;
@@ -658,32 +661,69 @@
 	NSLog(@"%@", error);
 }
 
-#pragma mark - 
+#pragma mark -
 
-// Rotates an image to the correct orientation, and removes the orientation EXIF data
-// Uploaded photos ignore the orientation EXIF data, so we need to rotate before uploading
-- (UIImage *)normalizedImage:(UIImage *)image {
-    CGSize imageSize = image.size;
-    CGSize newSize;
-    switch (image.imageOrientation) {
-        case UIImageOrientationUp:
-        case UIImageOrientationDown:
-        case UIImageOrientationUpMirrored:
-        case UIImageOrientationDownMirrored:
-            newSize = CGSizeMake(imageSize.width, imageSize.height);
+- (UIImage*) normalizedImage:(UIImage*)image {
+	CGImageRef          imgRef = image.CGImage;
+	CGFloat             width = CGImageGetWidth(imgRef);
+	CGFloat             height = CGImageGetHeight(imgRef);
+	CGAffineTransform   transform = CGAffineTransformIdentity;
+	CGRect              bounds = CGRectMake(0, 0, width, height);
+    CGSize              imageSize = bounds.size;
+	CGFloat             boundHeight;
+    UIImageOrientation  orient = image.imageOrientation;
+    
+	switch (orient) { 
+		case UIImageOrientationUp: //EXIF = 1
+			transform = CGAffineTransformIdentity;
+			break;
+            
+		case UIImageOrientationDown: //EXIF = 3
+			transform = CGAffineTransformMakeTranslation(imageSize.width, imageSize.height);
+			transform = CGAffineTransformRotate(transform, M_PI);
+			break;
+            
+		case UIImageOrientationLeft: //EXIF = 6
+			boundHeight = bounds.size.height;
+			bounds.size.height = bounds.size.width;
+			bounds.size.width = boundHeight;
+			transform = CGAffineTransformMakeTranslation(imageSize.height, imageSize.width);
+			transform = CGAffineTransformScale(transform, -1.0, 1.0);
+			transform = CGAffineTransformRotate(transform, 3.0 * M_PI / 2.0);
+			break;
+            
+		case UIImageOrientationRight: //EXIF = 8
+			boundHeight = bounds.size.height;
+			bounds.size.height = bounds.size.width;
+			bounds.size.width = boundHeight;
+			transform = CGAffineTransformMakeTranslation(0.0, imageSize.width);
+			transform = CGAffineTransformRotate(transform, 3.0 * M_PI / 2.0);
+			break;
+            
+		default:
+            // image is not auto-rotated by the photo picker, so whatever the user
+            // sees is what they expect to get. No modification necessary
+            transform = CGAffineTransformIdentity;
             break;
-        case UIImageOrientationLeft:
-        case UIImageOrientationRight:
-        case UIImageOrientationLeftMirrored:
-        case UIImageOrientationRightMirrored:
-            newSize = CGSizeMake(imageSize.height, imageSize.width);
-            break;
+	}
+    
+	UIGraphicsBeginImageContext(bounds.size);
+	CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    if ((image.imageOrientation == UIImageOrientationDown) || 
+        (image.imageOrientation == UIImageOrientationRight) || 
+        (image.imageOrientation == UIImageOrientationUp)) {
+        // flip the coordinate space upside down
+        CGContextScaleCTM(context, 1, -1);
+        CGContextTranslateCTM(context, 0, -height);
     }
-    UIGraphicsBeginImageContext(newSize);
-    [image drawInRect:CGRectMake(0,0,newSize.width, newSize.height)];
-    UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return newImage;
+    
+	CGContextConcatCTM(context, transform);
+	CGContextDrawImage(UIGraphicsGetCurrentContext(), CGRectMake(0, 0, width, height), imgRef);
+	UIImage *imageCopy = UIGraphicsGetImageFromCurrentImageContext();
+	UIGraphicsEndImageContext();
+    
+	return imageCopy;
 }
 
 @end
