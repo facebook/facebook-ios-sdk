b1d4a0f0c8987ce86a2ecdcb31b709426309feee
Михаил Куприянов <mpkupriyanov@gmail.com>
2016-02-12 16:18:49 +0300

    fix login with native app

--- FBSDKLoginKit/FBSDKLoginKit/FBSDKLoginManager.m
+++ FBSDKLoginKit/FBSDKLoginKit/FBSDKLoginManager.m
@@ -344,7 +344,7 @@ static NSString *const FBSDKExpectedChallengeKey = @"expected_login_challenge";
     case FBSDKLoginBehaviorNative: {
       if ([FBSDKInternalUtility isFacebookAppInstalled]) {
         [FBSDKServerConfigurationManager loadServerConfigurationWithCompletionBlock:^(FBSDKServerConfiguration *serverConfiguration, NSError *loadError) {
-          BOOL useNativeDialog = [serverConfiguration useNativeDialogForDialogName:FBSDKDialogConfigurationNameLogin];
+          BOOL useNativeDialog = [serverConfiguration useNativeDialogForDialogName:FBSDKDialogConfigurationNameMessage];
           if (useNativeDialog && loadError == nil) {
             [self performNativeLogInWithParameters:loginParams handler:^(BOOL openedURL, NSError *openedURLError) {
               if (openedURLError) {
