a2e5c46fdb773341e759a882b7a47b3d11dcce52
Abhijeet Kumar <abhijeet@nextdoor.com>
2017-05-03 09:42:19 -0700

    Remove NSLog statements when keychain store uses user defaults on sim

--- FBSDKCoreKit/FBSDKCoreKit/Internal/TokenCaching/FBSDKKeychainStore.m
+++ FBSDKCoreKit/FBSDKCoreKit/Internal/TokenCaching/FBSDKKeychainStore.m
@@ -90,7 +90,6 @@
     }
 
 #if TARGET_OS_SIMULATOR
-    NSLog(@"Falling back to storing access token in NSUserDefaults because of simulator bug");
     [[NSUserDefaults standardUserDefaults] setObject:value forKey:key];
 
     return [[NSUserDefaults standardUserDefaults] synchronize];
@@ -131,7 +130,6 @@
     }
 
 #if TARGET_OS_SIMULATOR
-    NSLog(@"Falling back to loading access token from NSUserDefaults because of simulator bug");
     return [[NSUserDefaults standardUserDefaults] dataForKey:key];
 #else
     NSMutableDictionary *query = [self queryForKey:key];
