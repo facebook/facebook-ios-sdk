846be4add11f4f4dc86a33cbd9563e0819dba8f7
sangpan <sangpan@paran.com>
2018-07-30 18:13:48 +0900

    setCurrentAccessToken changed. changed to checking argument token from checking global variable token 'g_currentAccessToken' .

--- FBSDKCoreKit/FBSDKCoreKit/FBSDKAccessToken.m
+++ FBSDKCoreKit/FBSDKCoreKit/FBSDKAccessToken.m
@@ -83,12 +83,15 @@ static FBSDKAccessToken *g_currentAccessToken;
 
 + (void)setCurrentAccessToken:(FBSDKAccessToken *)token
 {
+  if(token != nil && token.isExpired)
+    token = nil;
+  
   if (token != g_currentAccessToken) {
     NSMutableDictionary *userInfo = [NSMutableDictionary dictionary];
     [FBSDKInternalUtility dictionary:userInfo setObject:token forKey:FBSDKAccessTokenChangeNewKey];
     [FBSDKInternalUtility dictionary:userInfo setObject:g_currentAccessToken forKey:FBSDKAccessTokenChangeOldKey];
     // We set this flag also when the current Access Token was not valid, since there might be legacy code relying on it
-    if (![g_currentAccessToken.userID isEqualToString:token.userID] || ![self currentAccessTokenIsActive]) {
+    if (![g_currentAccessToken.userID isEqualToString:token.userID]) {
       userInfo[FBSDKAccessTokenDidChangeUserID] = @YES;
     }
 
