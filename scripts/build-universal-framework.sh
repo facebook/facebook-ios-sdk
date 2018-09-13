dfcb95915fcfe2cecacc5d9e0175be120ffb3121
Nonnus <np@hellofresh.com>
2018-09-12 17:40:28 -0700

    Xcode10 build fix when using carthage (#908)
    Summary:
    Thanks for proposing a pull request.
    
    To help us review the request, please complete the following:
    
    - [x] sign [contributor license agreement](https://developers.facebook.com/opensource/cla)
    - [x] submit against our `dev` branch, not `master`.
    - [x] describe the change (for example, what happens before the change, and after the change)
    
    This pr fixes compiling with latest xcode 10 using carthage by implementing the fixes described in https://github.com/facebook/facebook-sdk-swift/issues/246
    Pull Request resolved: https://github.com/facebook/facebook-ios-sdk/pull/908
    
    Reviewed By: jingping2015
    
    Differential Revision: D9798577
    
    Pulled By: codytwinton
    
    fbshipit-source-id: cf1f2caec5300d5465468996a3e6ae4e5a56cdde

--- scripts/build-universal-framework.sh
+++ scripts/build-universal-framework.sh
@@ -7,8 +7,8 @@ mkdir -p "${UNIVERSAL_OUTPUT_FOLDER}"
 rm -rf "${UNIVERSAL_OUTPUT_FOLDER}/${PROJECT_NAME}.framework"
 
 # Step 1. Build Device and Simulator versions
-xcodebuild -target "${PROJECT_NAME}" ONLY_ACTIVE_ARCH=NO -configuration ${CONFIGURATION} -sdk iphoneos  BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
-xcodebuild -target "${PROJECT_NAME}" ONLY_ACTIVE_ARCH=NO -configuration ${CONFIGURATION} -sdk iphonesimulator BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
+xcodebuild -target "${PROJECT_NAME}" ONLY_ACTIVE_ARCH=NO -configuration ${CONFIGURATION} -sdk iphoneos  BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}"
+xcodebuild -target "${PROJECT_NAME}" ONLY_ACTIVE_ARCH=NO -configuration ${CONFIGURATION} -sdk iphonesimulator BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}"
 
 # Step 2. Copy the framework structure to the universal folder
 cp -R "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${PROJECT_NAME}.framework" "${UNIVERSAL_OUTPUT_FOLDER}/"
