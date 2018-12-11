0b0425d7f42bca2edb3f48d89b1aacddf44d2c89
Rob Hudson <rhudson@fb.com>
2018-12-11 10:31:19 -0800

    Revert change to URL encoding
    Summary:
    Revert D13380987
    
    This change reintroduced the curly brace encoding issue that is causing Facebook Login to fail in FBSDKCoreKit
    
    Reviewed By: codytwinton
    
    Differential Revision: D13417825
    
    fbshipit-source-id: 505d5b7b4d6c09489f6f1edbd52110316fa9d7a9

--- FBSDKCoreKit/FBSDKCoreKit/FBSDKUtility.m
+++ FBSDKCoreKit/FBSDKCoreKit/FBSDKUtility.m
@@ -62,23 +62,18 @@
 
 + (NSString *)URLDecode:(NSString *)value
 {
-  value = [value stringByReplacingOccurrencesOfString:@"+" withString:@" "];
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-  value = [value stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-#pragma clang diagnostic pop
-  return value;
+  return [value
+          stringByReplacingOccurrencesOfString:@"+"
+          withString:@" "].stringByRemovingPercentEncoding;
 }
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
 + (NSString *)URLEncode:(NSString *)value
 {
-  return (__bridge_transfer NSString *)CFURLCreateStringByAddingPercentEscapes(NULL,
-                                                                               (CFStringRef)value,
-                                                                               NULL, // characters to leave unescaped
-                                                                               CFSTR(":!*();@/&?+$,='"),
-                                                                               kCFStringEncodingUTF8);
+  NSCharacterSet *urlAllowedSet = [NSCharacterSet
+                                   characterSetWithCharactersInString:@" !*();:'@&=+$,/?%#[]{}\""].invertedSet;
+  return [value stringByAddingPercentEncodingWithAllowedCharacters:urlAllowedSet];
 }
 #pragma clang diagnostic pop
 
