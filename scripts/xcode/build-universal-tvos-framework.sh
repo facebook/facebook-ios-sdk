6e7279a4743cf6aa0ec7a835b3eab08cf020a556
Cody Winton <cody.t.winton@gmail.com>
2019-02-07 13:04:19 -0800

    Updating Towards a Continuous Release Process
    * Release process updates (#2)
    
    * Deleted old package scripts, unused
    
    * Removed old scripts
    
    * Added editorconfig
    
    * Initial transition to scripts
    
    * Added tests for common
    
    * test_build updates
    
    * Updated and added tests
    
    * Added tests for Travis
    
    * Minor renames
    
    * Script updates
    
    * Updated Scripts
    
    * Re-added Xcode scripts
    
    * Added SDK_KITS and POD_SPECS
    
    * Improved tests for build sdk
    
    * Improved scripts layout
    
    * Improved travis
    
    * Test travis
    
    * Change stage name to test
    
    * Updated scripts
    
    * Updated travis config
    
    * Updated releases
    
    * Added to Stages
    
    * Added carthage job if tag is blank
    
    testing with present
    
    * Testing
    
    * Minor update
    
    * Test Carthage if tag is blank
    
    * Test non-master tag
    
    * Minor update
    
    * Improved release process
    
    * Minor updates
    
    * Testing release process
    
    * Re-enabled full scripts
    
    * Improved naming
    
    * Minor update
    
    * Minor update
    
    * Minor updates
    
    * Updated run and test scripts
    
    * Updated Scripts
    
    * Updated scripts
    
    * Updated Run script
    
    * Minor update
    
    * Minor updates
    
    * Added lint-podspecs command
    
    * Updated run script
    
    * Re-formatted scripts
    
    * Minor whitespace change
    
    * Improved to check for valid Sem versions
    
    * Updated travis condition for sem ver tags
    
    * Improved regex for sem ver testing
    
    * Improved layout of script
    
    * Updated stages
    
    * Minor restructure
    
    * Updated version
    
    * Updated scripts
    
    * Added prettierrc.yml
    
    * Updated Travis formatting
    
    * Changed to bot secure api key

--- scripts/xcode/build-universal-tvos-framework.sh
+++ scripts/xcode/build-universal-tvos-framework.sh
@@ -17,26 +17,24 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-UNIVERSAL_OUTPUT_FOLDER=../build/
+# --------------
+# Main Script
+# --------------
+
+UNIVERSAL_TV_BUILD_FOLDER=../build/tv/
 
 # make the output directory and delete the framework directory
-mkdir -p "${UNIVERSAL_OUTPUT_FOLDER}"
-rm -rf "${UNIVERSAL_OUTPUT_FOLDER}/${PROJECT_NAME}.framework"
+mkdir -p "${UNIVERSAL_TV_BUILD_FOLDER}"
+rm -rf "${UNIVERSAL_TV_BUILD_FOLDER}/${PROJECT_NAME}.framework"
 
 # Step 1. Build Device and Simulator versions
-xcodebuild -target "${PROJECT_NAME}" ONLY_ACTIVE_ARCH=NO -configuration ${CONFIGURATION} -sdk iphoneos  BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}"
-xcodebuild -target "${PROJECT_NAME}" ONLY_ACTIVE_ARCH=NO -configuration ${CONFIGURATION} -sdk iphonesimulator BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}"
+xcodebuild -target "${PROJECT_NAME}_TV" ONLY_ACTIVE_ARCH=NO -configuration ${CONFIGURATION} -sdk appletvos BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
+xcodebuild -target "${PROJECT_NAME}_TV" ONLY_ACTIVE_ARCH=NO -configuration ${CONFIGURATION} -sdk appletvsimulator BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
 
 # Step 2. Copy the framework structure to the universal folder
-cp -R "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${PROJECT_NAME}.framework" "${UNIVERSAL_OUTPUT_FOLDER}/"
+cp -R "${BUILD_DIR}/${CONFIGURATION}-appletvos/${PROJECT_NAME}.framework" "${UNIVERSAL_TV_BUILD_FOLDER}/"
 
 # Step 3. Create universal binary file using lipo and place the combined executable in the copied framework directory
-lipo -create -output "${UNIVERSAL_OUTPUT_FOLDER}/${PROJECT_NAME}.framework/${PROJECT_NAME}" "${BUILD_DIR}/${CONFIGURATION}-iphonesimulator/${PROJECT_NAME}.framework/${PROJECT_NAME}" "${BUILD_DIR}/${CONFIGURATION}-iphoneos/${PROJECT_NAME}.framework/${PROJECT_NAME}"
-
-# Step 4. Copy strings bundle if exists
-STRINGS_INPUT_FOLDER="${PROJECT_NAME}Strings.bundle"
-if [ -d "${STRINGS_INPUT_FOLDER}" ]; then
-  STRINGS_OUTPUT_FOLDER="${UNIVERSAL_OUTPUT_FOLDER}/${PROJECT_NAME}Strings.bundle"
-  rm -rf "${STRINGS_OUTPUT_FOLDER}"
-  cp -R "${STRINGS_INPUT_FOLDER}" "${STRINGS_OUTPUT_FOLDER}"
-fi
+lipo -create -output "${UNIVERSAL_TV_BUILD_FOLDER}/${PROJECT_NAME}.framework/${PROJECT_NAME}" \
+"${BUILD_DIR}/${CONFIGURATION}-appletvsimulator/${PROJECT_NAME}.framework/${PROJECT_NAME}" \
+"${BUILD_DIR}/${CONFIGURATION}-appletvos/${PROJECT_NAME}.framework/${PROJECT_NAME}"
