6636725a6129b0acf670928c22b803cd4303ed17
Cody Winton <cody.t.winton@gmail.com>
2019-03-05 14:04:58 -0800

    Added FBSDKLoginKitSwift

---
+++ FBSDKLoginKitSwift/FBSDKLoginKitSwift/FBSDKLoginKitSwift.h
@@ -0,0 +1,19 @@
+//
+//  FBSDKLoginKitSwift.h
+//  FBSDKLoginKitSwift
+//
+//  Created by Cody Winton on 12/13/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FBSDKLoginKitSwift.
+FOUNDATION_EXPORT double FBSDKLoginKitSwiftVersionNumber;
+
+//! Project version string for FBSDKLoginKitSwift.
+FOUNDATION_EXPORT const unsigned char FBSDKLoginKitSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FBSDKLoginKitSwift/PublicHeader.h>
+
+
