f587c2e3d11522f6fc5484fb73b82d2d36cc61d4
Tianqi Li <tianqibt@fb.com>
2019-03-12 19:29:28 -0700

    add unit tests for product log and user data in app events
    Summary:
    Add unit tests for:
    - log product item
    - set & clear user data
    
    Reviewed By: YOUDAN
    
    Differential Revision: D14391415
    
    fbshipit-source-id: 9cfd5b99374f636b78177804ecfb626fabe9b331

--- FBSDKCoreKit/FBSDKCoreKitTests/Internal/AppEvents/FBSDKAppEventsTests.m
+++ FBSDKCoreKit/FBSDKCoreKitTests/Internal/AppEvents/FBSDKAppEventsTests.m
@@ -21,21 +21,87 @@
 #import <OCMock/OCMock.h>
 
 #import "FBSDKAppEvents.h"
+#import "FBSDKGraphRequestConnection.h"
 
 @interface FBSDKAppEventsTests : XCTestCase
-
+{
+  id _mockAppEvents;
+  id _mockGraphRequestConnection;
+}
 @end
 
 @implementation FBSDKAppEventsTests
+
+- (void)setUp
+{
+  _mockAppEvents = [OCMockObject niceMockForClass:[FBSDKAppEvents class]];
+  _mockGraphRequestConnection = [OCMockObject niceMockForClass:[FBSDKGraphRequestConnection class]];
+}
 
 - (void)testLogPurchase
 {
   double mockPurchaseAmount = 1.0;
   NSString *mockCurrency = @"USD";
-  id mockAppEvents = [OCMockObject niceMockForClass:[FBSDKAppEvents class]];
-  [[mockAppEvents expect] logEvent:FBSDKAppEventNamePurchased valueToSum:@(mockPurchaseAmount) parameters:[OCMArg any] accessToken:[OCMArg any]];
+  [[_mockAppEvents expect] logEvent:FBSDKAppEventNamePurchased valueToSum:@(mockPurchaseAmount) parameters:[OCMArg any] accessToken:[OCMArg any]];
   [FBSDKAppEvents logPurchase:mockPurchaseAmount currency:mockCurrency];
-  [mockAppEvents verify];
+  [_mockAppEvents verify];
+}
+
+- (void)testLogProductItem
+{
+  NSDictionary<NSString *, NSString *> *expectedDict = @{
+                                 @"fb_product_availability":@"IN_STOCK",
+                                 @"fb_product_brand":@"PHILZ",
+                                 @"fb_product_condition":@"NEW",
+                                 @"fb_product_description":@"description",
+                                 @"fb_product_gtin":@"BLUE MOUNTAIN",
+                                 @"fb_product_image_link":@"https://www.sample.com",
+                                 @"fb_product_item_id":@"F40CEE4E-471E-45DB-8541-1526043F4B21",
+                                 @"fb_product_link":@"https://www.sample.com",
+                                 @"fb_product_mpn":@"BLUE MOUNTAIN",
+                                 @"fb_product_price_amount":@"1.000",
+                                 @"fb_product_price_currency":@"USD",
+                                 @"fb_product_title":@"title",
+                                 };
+  [[_mockAppEvents expect] logEvent:@"fb_mobile_catalog_update"
+                         parameters:expectedDict];
+
+  [FBSDKAppEvents logProductItem:@"F40CEE4E-471E-45DB-8541-1526043F4B21"
+                    availability:FBSDKProductAvailabilityInStock
+                       condition:FBSDKProductConditionNew
+                     description:@"description"
+                       imageLink:@"https://www.sample.com"
+                            link:@"https://www.sample.com"
+                           title:@"title"
+                     priceAmount:1.0
+                        currency:@"USD"
+                            gtin:@"BLUE MOUNTAIN"
+                             mpn:@"BLUE MOUNTAIN"
+                           brand:@"PHILZ"
+                      parameters:@{}];
+
+  [_mockAppEvents verify];
+}
+
+- (void)testSetAndClearUserData
+{
+  [FBSDKAppEvents setUserEmail:@"em"
+                     firstName:@"fn"
+                      lastName:@"ln"
+                         phone:@"123"
+                   dateOfBirth:nil
+                        gender:nil
+                          city:nil
+                         state:nil
+                           zip:nil
+                       country:nil];
+  NSString *expectedUserData = @"{\"ln\":\"e545c2c24e6463d7c4fe3829940627b226c0b9be7a8c7dbe964768da48f1ab9d\",\"ph\":\"a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3\",\"em\":\"84a47f61dd341ce731390149a904abcd58a6044263071abf44a475cf91563029\",\"fn\":\"0f1e18bb4143dc4be22e61ea4deb0491c2bf7018c6504ad631038aed5ca4a0ca\"}";
+  NSString *userData = [FBSDKAppEvents getUserData];
+  XCTAssertEqualObjects(userData, expectedUserData);
+
+  [FBSDKAppEvents clearUserData];
+  NSString *clearedUserData = [FBSDKAppEvents getUserData];
+  XCTAssertEqualObjects(clearedUserData, @"{}");
 }
 
 - (void)testSetAndClearUserID
