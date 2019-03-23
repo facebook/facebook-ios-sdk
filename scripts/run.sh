e8a46b37928e31ee1be550b3e616b9ebcb4c3af3
Cody Winton <codytwinton@fb.com>
2019-03-23 13:06:50 -0700

    Improved Run Scripts for Cocoapod Linting
    Summary: Title
    
    Reviewed By: joesus
    
    Differential Revision: D14587866
    
    fbshipit-source-id: 56f1274f6da3ff8349517fc4ed67548427f6476a

--- scripts/run.sh
+++ scripts/run.sh
@@ -248,14 +248,37 @@ build_sdk() {
 lint_sdk() {
   # Lint Podspecs
   lint_cocoapods() {
+    pod_lint_failures=()
+
     for spec in "${SDK_POD_SPECS[@]}"; do
       if [ ! -f "$spec" ]; then
         echo "*** ERROR: unable to lint $spec"
         continue
       fi
 
-      pod lib lint "$spec" "$@"
+      if [ "$spec" == "AccountKit/AccountKit.podspec" ]; then
+        continue
+      fi
+
+      local pod_lint_type="lib"
+
+      if [ "$spec" == "FBSDKMarketingKit.podspec" ]; then
+        pod_lint_type="spec"
+      fi
+
+      set +e
+      if ! pod "$pod_lint_type" lint "$spec" "$@"; then
+        pod_lint_failures+=("$spec")
+      fi
+      set -e
     done
+
+    if [ ${#pod_lint_failures[@]} -ne 0 ]; then
+      echo "Failed lint for: ${pod_lint_failures[*]}"
+      exit 1
+    else
+      exit 0
+    fi
   }
 
   local lint_type=${1:-}
