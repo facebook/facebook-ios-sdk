1950a03d3ef76c216289a5f1556bc174c2bcb8f3
Stan Wu <dreamolight@fb.com>
2019-04-11 01:28:12 -0700

    Fix current view controller name
    Summary: When `rootViewController` is the top visible view controller, its `presentedViewController` will be nil. But it's actually the top visible view controller.
    
    Reviewed By: robtimp
    
    Differential Revision: D14890172
    
    fbshipit-source-id: 55d78222c5cba1bd6d70ea78658635769b7879e0

--- FBSDKCoreKit/FBSDKCoreKit/AppEvents/FBSDKAppEvents.m
+++ FBSDKCoreKit/FBSDKCoreKit/AppEvents/FBSDKAppEvents.m
@@ -1138,7 +1138,8 @@ static NSString *g_overrideAppID = nil;
     // We only collect the view controller when on the main thread, as the behavior off
     // the main thread is unpredictable.  Besides, UI state for off-main-thread computations
     // isn't really relevant anyhow.
-    UIViewController *vc = [UIApplication sharedApplication].keyWindow.rootViewController.presentedViewController;
+    UIViewController *vc = [UIApplication sharedApplication].keyWindow.rootViewController;
+    vc = vc.presentedViewController ?: vc;
     if (vc) {
       currentViewControllerName = [[vc class] description];
     } else {
