0ca2fb3706e8a55b49ee4394af7fa6b518f71ec3
Lina Jin <lina@fb.com>
2019-08-14 21:27:19 -0700

    Local storage for error report data
    Summary:
    - FBSDKErrorReportStorage
       - Store error info as dictionary in local file
       - Next diff: load the existing local error data while launching the app
    
    Reviewed By: dreamolight
    
    Differential Revision: D16708224
    
    fbshipit-source-id: 70eb642bb76746c418ba4d646bd70684bcdeafab

--- FBSDKCoreKit/FBSDKCoreKit/Internal/Instrument/ErrorReport/FBSDKErrorReport.m
+++ FBSDKCoreKit/FBSDKCoreKit/Internal/Instrument/ErrorReport/FBSDKErrorReport.m
@@ -18,10 +18,30 @@
 
 #import "FBSDKErrorReport.h"
 
+#import "FBSDKLogger.h"
+#import "FBSDKSettings.h"
 
 @implementation FBSDKErrorReport
 
+static NSString *ErrorReportStorageDirName = @"instrument/";
+static NSString *directoryPath;
+
+NSString *const kFBSDKErrorCode = @"error_code";
+NSString *const kFBSDKErrorDomain = @"error_domain";
+NSString *const kFBSDKErrorTimestamp = @"error_time_stamp";
+
 # pragma mark - Class Methods
+
++ (void)initialize
+{
+  NSString *dirPath = [NSTemporaryDirectory() stringByAppendingPathComponent:ErrorReportStorageDirName];
+  if (![[NSFileManager defaultManager] fileExistsAtPath:dirPath]) {
+    if (![[NSFileManager defaultManager] createDirectoryAtPath:dirPath withIntermediateDirectories:NO attributes:NULL error:NULL]) {
+      [FBSDKLogger singleShotLogEntry:FBSDKLoggingBehaviorInformational formatString:@"Failed to create library at %@", dirPath];
+    }
+  }
+  directoryPath = dirPath;
+}
 
 - (void)enable
 {
@@ -32,7 +52,41 @@
       errorDomain:(NSErrorDomain)errorDomain
           message:(nullable NSString *)message
 {
-  // TODO (linajin) T48558698 Load Error Report in Local Storage
+  NSString *timestamp = [NSString stringWithFormat:@"%.0lf", [[NSDate date] timeIntervalSince1970]];
+  [self saveErrorInfoToDisk: @{
+                               kFBSDKErrorCode:@(errorCode),
+                               kFBSDKErrorDomain:errorDomain,
+                               kFBSDKErrorTimestamp:timestamp,
+                               }];
 }
 
++ (NSDictionary<NSString *,id> *)loadErrorInfo
+{
+  // TODO (linajin) T48556791 Error Report Logger behavior upon APP loading
+  return @{};
+}
+
++ (void)clearErrorInfo
+{
+  NSArray<NSString *> *files = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:directoryPath error:nil];
+  for (NSUInteger i = 0; i < files.count; i++) {
+    if ([files[i] hasPrefix:@"error_report"]) {
+      [[NSFileManager defaultManager] removeItemAtPath:[directoryPath stringByAppendingPathComponent:files[i]] error:nil];
+    }
+  }
+}
+
+#pragma mark - disk operations
+
++ (void)saveErrorInfoToDisk:(NSDictionary<NSString *, id> *)errorInfo
+{
+  [errorInfo writeToFile:[self pathToErrorInfoFile]
+              atomically:YES];
+  }
+
++ (NSString *)pathToErrorInfoFile
+{
+  NSString *timestamp = [NSString stringWithFormat:@"%.0lf", [[NSDate date] timeIntervalSince1970]];
+  return [directoryPath stringByAppendingPathComponent:[NSString stringWithFormat:@"error_report_%@.plist",timestamp]];
+}
 @end
