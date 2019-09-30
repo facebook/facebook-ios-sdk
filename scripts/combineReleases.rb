6a2d3530aae9343b2b6043f52b2547c531e3d880
joesusnick <joesusnick@gmail.com>
2019-09-30 14:47:12 -0700

    Maintaining existing Carthage frameworks zip name (#1065)
    Summary:
    Thanks for proposing a pull request!
    
    To help us review the request, please complete the following:
    
    - [x] sign [contributor license agreement](https://developers.facebook.com/opensource/cla)
    - [x] I've ensured that all existing tests pass and added tests (when/where necessary)
    - [x] I've updated the documentation (when/where necessary) and [Changelog](CHANGELOG.md) (when/where necessary)
    - [x] I've added the proper label to this pull request (e.g. `bug` for bug fixes)
    
    ## Pull Request Details
    
    Should have been included in **https://github.com/facebook/facebook-objc-sdk/issues/1062**
    Pull Request resolved: https://github.com/facebook/facebook-objc-sdk/pull/1065
    
    Test Plan: N/A
    
    Reviewed By: jingping2015
    
    Differential Revision: D17674022
    
    Pulled By: joesus
    
    fbshipit-source-id: 4d22f2045bccb2839327439bb80c627e5d007e61

--- scripts/combineReleases.rb
+++ scripts/combineReleases.rb
@@ -14,7 +14,6 @@ class GitHubConstants
 end
 
 class Filenames
-  Carthage = 'Carthage.frameworks.zip'
   RemoteObjCFrameworks = 'FacebookSDK_Dynamic.framework.zip'
   RemoteSwiftFrameworks = 'SwiftDynamic.zip'
 
@@ -69,4 +68,6 @@ end
 
 system "zip -r -m Artifacts.zip Artifacts"
 system 'rm -rf Artifacts'
-system "mv Artifacts.zip build/Release/#{Filenames::Carthage}"
+
+# Use the same name as the original release to preserve CI for existing users.
+system "mv Artifacts.zip build/Release/#{Filenames::RemoteObjCFrameworks}"
