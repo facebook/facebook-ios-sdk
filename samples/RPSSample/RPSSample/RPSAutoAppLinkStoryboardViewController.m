26833c58d0b53c4f40aa4f4f83c170e8bdcd1561
Tao Chen <oliverct@fb.com>
2019-10-28 14:59:40 -0700

    Add RPSAutoAppLinkStoryboardViewController to RPSSample
    Summary: - add a UIViewCrontroller implemented with Storyboard in ObjC for auto applink demo
    
    Reviewed By: KylinChang
    
    Differential Revision: D18140947
    
    fbshipit-source-id: 87aa0f784d4ff3f024b4ca7ed8aa4a41468d49f1

---
+++ samples/RPSSample/RPSSample/RPSAutoAppLinkStoryboardViewController.m
@@ -0,0 +1,61 @@
+// Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+//
+// You are hereby granted a non-exclusive, worldwide, royalty-free license to use,
+// copy, modify, and distribute this software in source code or binary form for use
+// in connection with the web services and APIs provided by Facebook.
+//
+// As with any software that integrates with the Facebook platform, your use of
+// this software is subject to the Facebook Developer Principles and Policies
+// [http://developers.facebook.com/policy/]. This copyright notice shall be
+// included in all copies or substantial portions of the software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#import "RPSAutoAppLinkStoryboardViewController.h"
+
+@interface RPSAutoAppLinkStoryboardViewController()
+
+@property (strong, nonatomic) Coffee* product;
+@property (nonatomic, copy) NSDictionary<NSString *, id> *data;
+
+@property (weak, nonatomic) IBOutlet UILabel* labelName;
+@property (weak, nonatomic) IBOutlet UILabel* labelDesc;
+@property (weak, nonatomic) IBOutlet UILabel* labelPrice;
+@property (weak, nonatomic) IBOutlet UILabel* labelData;
+
+@end
+
+@implementation RPSAutoAppLinkStoryboardViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    if (self.product == nil) {
+        self.product = [[Coffee alloc] initWithName:@"Coffee" desc:@"I am just a STORYBOARD coffee" price:1];
+    }
+
+    self.labelName.text = self.product.name;
+    self.labelDesc.text = [@"Description: " stringByAppendingString:self.product.desc];
+    self.labelPrice.text = [@"Price: $" stringByAppendingString:[@(self.product.price) stringValue]];
+
+    if (self.data != nil) {
+        self.labelData.text = [NSString stringWithFormat:@"data is: %@", self.data];
+    }
+}
+
+#pragma mark - Auto App Link
+- (void)setAutoAppLinkData:(NSDictionary<NSString *, id> *)data {
+    NSString *productIndex = [@([data[@"product_id"] integerValue]) stringValue];
+    NSString *name = [@"STORYBOARD Coffee " stringByAppendingString:productIndex];
+    NSString *description = [@"I am auto app link STORYBOARD coffee " stringByAppendingString:productIndex];
+    self.product = [[Coffee alloc] initWithName:name desc:description price:10];
+    self.data = [data copy];
+}
+
+@end
