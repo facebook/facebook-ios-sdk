55bbf71a6d930d3004cbf1968ba5c87de8b51643
Tianqi Li <tianqibt@fb.com>
2019-10-28 13:30:39 -0700

    correct copyright in test file
    Summary: as title
    
    Differential Revision: D18179912
    
    fbshipit-source-id: fe96d554e674e2c8ab9a85f0f12f97dc278ede27

--- FBSDKCoreKit/FBSDKCoreKitTests/Internal/Instrument/FBSDKCrashShieldTests.m
+++ FBSDKCoreKit/FBSDKCoreKitTests/Internal/Instrument/FBSDKCrashShieldTests.m
@@ -1,3 +1,5 @@
+// Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+//
 // You are hereby granted a non-exclusive, worldwide, royalty-free license to use,
 // copy, modify, and distribute this software in source code or binary form for use
 // in connection with the web services and APIs provided by Facebook.
