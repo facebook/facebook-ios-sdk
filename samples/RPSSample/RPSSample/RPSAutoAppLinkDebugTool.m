89bc03d0ebe8abc26ad9d15daa54c8a5604836c7
Tao Chen <oliverct@fb.com>
2019-11-04 14:24:11 -0800

    Add Auto Applink Debug Tool Logic
    Summary:
    - Generate NSURL from App ID and product id
     - open the generated url
    
    Reviewed By: KylinChang
    
    Differential Revision: D18094569
    
    fbshipit-source-id: 16765ffe454ce3e56dba4b33abe936ae5b9be1cd

--- samples/RPSSample/RPSSample/RPSAutoAppLinkDebugTool.m
+++ samples/RPSSample/RPSSample/RPSAutoAppLinkDebugTool.m
@@ -18,6 +18,10 @@
 
 #import "RPSAutoAppLinkDebugTool.h"
 
+#import <Foundation/Foundation.h>
+
+#import <FBSDKCoreKit/FBSDKUtility.h>
+
 static const int paddingLen = 10;
 static const int frameHeight = 30;
 
@@ -42,16 +46,17 @@ static const int frameHeight = 30;
     labelDesc.text = @"Enter your FB App ID and product ID to get your auto applink";
     labelDesc.numberOfLines = 0;
 
-    self.appIDView = [self textFieldWithText: @"FB App ID (ex. 111222333)"];
+    self.appIDView = [self textFieldWithText: @"FB App ID (ex. 111222333)" keyBoardType:UIKeyboardTypeNumberPad];
     self.appIDView.frame = CGRectMake(paddingLen, 150, frameWidth, frameHeight);
 
-    self.productIDView = [self textFieldWithText: @"Product ID (ex. 123)"];
+    self.productIDView = [self textFieldWithText: @"Product ID (ex. 123)" keyBoardType:UIKeyboardTypeDefault];
     self.productIDView.frame = CGRectMake(paddingLen, 200, frameWidth, frameHeight);
 
     UIButton *sendButton = [[UIButton alloc] initWithFrame:CGRectMake(paddingLen, 250, frameWidth, frameHeight + 10)];
     [sendButton setBackgroundColor:[[UIColor blueColor] colorWithAlphaComponent:0.4]];
     [sendButton setTitle:@"Send" forState:UIControlStateNormal];
     [sendButton setTitleColor:UIColor.whiteColor forState:UIControlStateNormal];
+    [sendButton addTarget:self action:@selector(sendAutoAppLink:) forControlEvents:UIControlEventTouchUpInside];
 
     [scrollView addSubview:labelName];
     [scrollView addSubview:labelDesc];
@@ -62,16 +67,56 @@ static const int frameHeight = 30;
 }
 
 - (UITextField *)textFieldWithText:(NSString *)text
+                      keyBoardType:(UIKeyboardType)type
 {
     UITextField *textField;
     textField = [[UITextField alloc] init];
     textField.layer.borderColor = [UIColor lightGrayColor].CGColor;
     textField.layer.borderWidth = 1;
+    [textField setKeyboardType:type];
     textField.placeholder = text;
     UIView *paddingView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 5, 20)];
     textField.leftView = paddingView;
     textField.leftViewMode = UITextFieldViewModeAlways;
     return textField;
+}
+
+- (void)sendAutoAppLink:(UIButton *)button
+{
+    NSString *autoAppLink = [NSString stringWithFormat:@"fb%@://applinks?al_applink_data=", self.appIDView.text];
+    NSDictionary<NSString *, NSString *> *data = @{@"product_id" : self.productIDView.text,
+                                                  @"is_auto_applink" : @"true"
+                                                  };
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:data options:0 error:nil];
+    if (self.appIDView.text.length>0 && self.productIDView.text.length>0 && jsonData) {
+        NSString *encodeData = [FBSDKUtility URLEncode:[[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding]];
+        NSString *encodeURL = [autoAppLink stringByAppendingString:encodeData];
+        NSURL *url = [NSURL URLWithString:encodeURL];
+        if (![[UIApplication sharedApplication] openURL:url]) {
+            [self showAlert: @"Cannot open the URL!"];
+        }
+    }
+    else {
+        if (self.appIDView.text.length == 0) {
+            [self showAlert: @"Invalid App ID!"];
+        }
+        else if (self.productIDView.text.length == 0) {
+            [self showAlert: @"Invalid Product ID!"];
+        }
+        else {
+            [self showAlert: @"Cannot generate url from input!"];
+        }
+    }
+}
+
+- (void)showAlert:(NSString *) message
+{
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                        message:message
+                                                       delegate:self
+                                              cancelButtonTitle:@"Close"
+                                              otherButtonTitles:nil];
+    [alertView show];
 }
 
 @end
