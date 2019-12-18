a85e21aafa21dbb63ef0fd5c217394abfb07c9ef
Rob Hudson <rhudson@fb.com>
2019-12-17 17:57:17 -0800

    Set FBSDKGraphErrorRecoveryProcessorDelegate property to weak
    Summary:
    From https://github.com/facebook/facebook-ios-sdk/issues/1142:
    
    ## Checklist
    
    - [x] I've updated to the latest released version of the SDK
    - [x] I've searched for existing [GitHub issues](https://github.com/facebook/facebook-ios-sdk/issues)
    - [x] I've looked for existing answers on [Stack Overflow](https://facebook.stackoverflow.com), the [Facebook Developer Community Forum](https://developers.facebook.com/community/) and the [Facebook Developers Group](https://www.facebook.com/groups/fbdevelopers)
    - [x] I've read the [Code of Conduct](CODE_OF_CONDUCT.md)
    - [x] This issue is not security related and can safely be disclosed publicly on GitHub
    
    ## Environment
    
    Describe your dev environment here, giving as many details as possible. If you have them, make sure to include:
    
    - Xcode Version: 'Version 10.3 (10G8)'
    - Swift Version: `4.2` (if issue is Swift related)
    - Installation Platform & Verison: `[Cocoapods]` version `5.0.0`
    
    **Leaking memory on startup of the app.**
    
    [Screen Shot 2019-12-05 at 9 19 55 am](https://user-images.githubusercontent.com/12591229/70187111-8f237480-1741-11ea-8450-471132547948.png)
    
    [Screen Shot 2019-12-06 at 11 36 45 am](https://user-images.githubusercontent.com/12591229/70285768-c31f9800-181c-11ea-8ff4-ffb857665b03.png)
    
    Differential Revision: D19147270
    
    fbshipit-source-id: a4faf931ef74133573e0da406851a35c804ca6f7

--- FBSDKCoreKit/FBSDKCoreKit/FBSDKGraphErrorRecoveryProcessor.m
+++ FBSDKCoreKit/FBSDKCoreKit/FBSDKGraphErrorRecoveryProcessor.m
@@ -26,7 +26,7 @@
   NSError *_error;
 }
 
-@property (nonatomic, strong, nullable) id<FBSDKGraphErrorRecoveryProcessorDelegate>delegate;
+@property (nonatomic, weak, nullable) id<FBSDKGraphErrorRecoveryProcessorDelegate> delegate;
 
 @end
 
