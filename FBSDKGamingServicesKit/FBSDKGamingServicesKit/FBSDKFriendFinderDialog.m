3d9940ed42ddee67d9a01d5adc3505f2c63b5fd0
Daniel Loomb <dloomb@fb.com>
2020-02-05 16:08:26 -0800

    Adding Friend Finder call to SDK
    Differential Revision: D19650190
    
    fbshipit-source-id: 852964a93613db9566783624dda380a5e6d2e0c8

--- FBSDKGamingServicesKit/FBSDKGamingServicesKit/FBSDKFriendFinderDialog.m
+++ FBSDKGamingServicesKit/FBSDKGamingServicesKit/FBSDKFriendFinderDialog.m
@@ -18,5 +18,109 @@
 
 #import "FBSDKFriendFinderDialog.h"
 
+#import "FBSDKCoreKit+Internal.h"
+
+static NSString *const kFriendFinderUrlFormat = @"https://fb.gg/me/friendfinder/%@";
+
+@interface FBSDKFriendFinderDialog () <FBSDKURLOpening>
+@end
+
 @implementation FBSDKFriendFinderDialog
+{
+  BOOL _isPerformingFriendFinding;
+  FBSDKFriendFinderCompletionBlock _completionBlock;
+}
+
++ (void)launchFriendFinderDialogWithCompletionBlock:(FBSDKFriendFinderCompletionBlock)completionBlock
+{
+  if ([FBSDKAccessToken currentAccessToken] == nil) {
+    completionBlock(nil, [FBSDKError
+                          errorWithCode:FBSDKErrorAccessTokenRequired
+                          message:@"A valid access token is required to launch the Friend Finder"]);
+
+    return;
+  }
+
+  FBSDKFriendFinderDialog *const instance = [[self alloc] init];
+  if (instance) {
+    instance->_completionBlock = completionBlock;
+    instance->_isPerformingFriendFinding = true;
+  }
+
+  NSURL *const url =
+  [NSURL URLWithString:
+   [NSString
+    stringWithFormat:kFriendFinderUrlFormat, FBSDKAccessToken.currentAccessToken.appID]];
+
+  __weak FBSDKFriendFinderDialog *weakSelf = instance;
+  [[FBSDKBridgeAPI sharedInstance]
+   openURL:url
+   sender:weakSelf
+   handler:^(BOOL success, NSError * _Nullable error) {
+    if (!success) {
+      [weakSelf handleBridgeAPIError:error];
+    }
+  }];
+
+  return;
+}
+
+- (void)handleBridgeAPIError:(NSError *)error
+{
+  _isPerformingFriendFinding = false;
+  if (error) {
+    _completionBlock(false, [FBSDKError
+                             errorWithCode:FBSDKErrorBridgeAPIInterruption
+                             message:@"Error occured while launching Friend Finder"
+                             underlyingError:error]);
+  } else {
+    _completionBlock(false, [FBSDKError
+                             errorWithCode:FBSDKErrorBridgeAPIInterruption
+                             message:@"An Unknown error occured while launching Friend Finder"]);
+  }
+
+  _completionBlock = nil;
+}
+
+- (void)completeSuccessfully
+{
+  _isPerformingFriendFinding = false;
+  _completionBlock(true, nil);
+  _completionBlock = nil;
+}
+
+#pragma mark - FBSDKURLOpeningma
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+  const BOOL isFriendFinderURL =
+  [self
+   canOpenURL:url
+   forApplication:application
+   sourceApplication:sourceApplication
+   annotation:annotation];
+
+  if (isFriendFinderURL) {
+    [self completeSuccessfully];
+  }
+
+  return isFriendFinderURL;
+}
+
+- (BOOL)canOpenURL:(NSURL *)url
+    forApplication:(UIApplication *)application
+ sourceApplication:(NSString *)sourceApplication
+        annotation:(id)annotation
+{
+  // verify the URL is intended as a callback for the SDK's friend finder
+  return [url.scheme hasPrefix:[NSString stringWithFormat:@"fb%@", [FBSDKSettings appID]]] &&
+  [url.host isEqualToString:@"friend-finder"];
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+  if (_isPerformingFriendFinding) {
+    [self completeSuccessfully];
+  }
+}
+
 @end
