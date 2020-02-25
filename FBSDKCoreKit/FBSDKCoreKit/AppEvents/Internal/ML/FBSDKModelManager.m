29be3bd8807187996bafb26f4c4c4cec19e3d68e
Zebing Zong <zzong@fb.com>
2020-02-24 16:54:18 -0800

    update ModelManager, Inferences and FeatureExtra to support MTML (#1247)
    Summary:
    Pull Request resolved: https://github.com/facebook/facebook-ios-sdk/pull/1247
    
    1. add check features and execute logic for MTML with new feature flag
    2. update FBSDKAddressInferencer, FBSDKEventInferencer, FBSDKFeatureExtractor to support useCaseKey as a new parameter to support MTML
    3. update with new asset download logic for MTML tasks
    
    Reviewed By: KylinChang
    
    Differential Revision: D19945774
    
    fbshipit-source-id: b2eb390a2f728212fe54d087b30e89afb790ac8a

--- FBSDKCoreKit/FBSDKCoreKit/AppEvents/Internal/ML/FBSDKModelManager.m
+++ FBSDKCoreKit/FBSDKCoreKit/AppEvents/Internal/ML/FBSDKModelManager.m
@@ -45,6 +45,10 @@ static NSString *const VERSION_ID_KEY = @"version_id";
 static NSString *const MODEL_DATA_KEY = @"data";
 static NSString *const ADDRESS_FILTERING_KEY = @"DATA_DETECTION_ADDRESS";
 
+static NSString *const MTMLPrefixKey = @"mtml";
+static NSString *const MTMLTaskAppEventPredKey = @"mtml_app_event_pred";
+static NSString *const MTMLTaskAddressDetectKey = @"mtml_address_detect";
+
 static NSString *_directoryPath;
 static NSMutableDictionary<NSString *, id> *_modelInfo;
 
@@ -86,42 +90,26 @@ NS_ASSUME_NONNULL_BEGIN
       // update cache
       [[NSUserDefaults standardUserDefaults] setObject:modelInfo forKey:MODEL_INFO_KEY];
 
-      [FBSDKFeatureManager checkFeature:FBSDKFeatureSuggestedEvents completionBlock:^(BOOL enabled) {
+      [FBSDKFeatureManager checkFeature:FBSDKFeatureMTML completionBlock:^(BOOL enabled) {
         if (enabled) {
-          [self getModelAndRules:SUGGEST_EVENT_KEY handler:^(BOOL success){
-            if (success) {
-              [FBSDKEventInferencer loadWeights];
-              [FBSDKFeatureExtractor loadRules];
-              [FBSDKSuggestedEventsIndexer enable];
-            }
-          }];
-        }
-      }];
-
-      [FBSDKFeatureManager checkFeature:FBSDKFeaturePIIFiltering completionBlock:^(BOOL enabled) {
-        if (enabled) {
-          [self getModelAndRules:ADDRESS_FILTERING_KEY handler:^(BOOL success){
-            if (success) {
-              [FBSDKAddressInferencer loadWeights];
-              [FBSDKAddressInferencer initializeDenseFeature];
-              [FBSDKAddressFilterManager enable];
-            }
-          }];
+          [self checkFeaturesAndExecuteForMTML];
+        } else {
+          [self checkFeaturesAndExecute];
         }
       }];
     }];
   });
 }
 
-+ (nullable NSDictionary *)getRules
++ (nullable NSDictionary *)getRulesForKey:(NSString *)useCaseKey
 {
   NSDictionary<NSString *, id> *cachedModelInfo = [[NSUserDefaults standardUserDefaults] objectForKey:MODEL_INFO_KEY];
   if (!cachedModelInfo) {
     return nil;
   }
-  NSDictionary<NSString *, id> *model = [cachedModelInfo objectForKey:SUGGEST_EVENT_KEY];
+  NSDictionary<NSString *, id> *model = [cachedModelInfo objectForKey:useCaseKey];
   if (model && model[VERSION_ID_KEY]) {
-    NSString *filePath = [_directoryPath stringByAppendingPathComponent:[NSString stringWithFormat:@"%@_%@.rules", SUGGEST_EVENT_KEY, model[VERSION_ID_KEY]]];
+    NSString *filePath = [_directoryPath stringByAppendingPathComponent:[NSString stringWithFormat:@"%@_%@.rules", useCaseKey, model[VERSION_ID_KEY]]];
     if (filePath) {
       NSData *ruelsData = [NSData dataWithContentsOfFile:filePath options:NSDataReadingMappedIfSafe error:nil];
       NSDictionary *rules = [NSJSONSerialization JSONObjectWithData:ruelsData options:0 error:nil];
@@ -145,6 +133,60 @@ NS_ASSUME_NONNULL_BEGIN
 }
 
 #pragma mark - Private methods
+
++ (void)checkFeaturesAndExecuteForMTML
+{
+  [FBSDKFeatureManager checkFeature:FBSDKFeatureSuggestedEvents completionBlock:^(BOOL enabled) {
+    if (enabled) {
+      [self getModelAndRules:MTMLTaskAppEventPredKey handler:^(BOOL success) {
+        if (success) {
+          [FBSDKEventInferencer loadWeightsForKey:MTMLPrefixKey];
+          [FBSDKFeatureExtractor loadRulesForKey:MTMLTaskAppEventPredKey];
+          [FBSDKSuggestedEventsIndexer enable];
+        }
+      }];
+    }
+  }];
+
+  [FBSDKFeatureManager checkFeature:FBSDKFeaturePIIFiltering completionBlock:^(BOOL enabled) {
+    if (enabled) {
+      [self getModelAndRules:MTMLTaskAddressDetectKey handler:^(BOOL success) {
+        if (success) {
+          [FBSDKAddressInferencer loadWeightsForKey:MTMLPrefixKey];
+          [FBSDKAddressInferencer initializeDenseFeature];
+          [FBSDKAddressFilterManager enable];
+        }
+      }];
+    }
+  }];
+}
+
++ (void)checkFeaturesAndExecute
+{
+  [FBSDKFeatureManager checkFeature:FBSDKFeatureSuggestedEvents completionBlock:^(BOOL enabled) {
+    if (enabled) {
+      [self getModelAndRules:SUGGEST_EVENT_KEY handler:^(BOOL success){
+        if (success) {
+          [FBSDKEventInferencer loadWeightsForKey:SUGGEST_EVENT_KEY];
+          [FBSDKFeatureExtractor loadRulesForKey:SUGGEST_EVENT_KEY];
+          [FBSDKSuggestedEventsIndexer enable];
+        }
+      }];
+    }
+  }];
+
+  [FBSDKFeatureManager checkFeature:FBSDKFeaturePIIFiltering completionBlock:^(BOOL enabled) {
+    if (enabled) {
+      [self getModelAndRules:ADDRESS_FILTERING_KEY handler:^(BOOL success){
+        if (success) {
+          [FBSDKAddressInferencer loadWeightsForKey:ADDRESS_FILTERING_KEY];
+          [FBSDKAddressInferencer initializeDenseFeature];
+          [FBSDKAddressFilterManager enable];
+        }
+      }];
+    }
+  }];
+}
 
 + (void)getModelAndRules:(NSString *)useCaseKey
                  handler:(FBSDKDownloadCompletionBlock)handler
@@ -177,12 +219,19 @@ NS_ASSUME_NONNULL_BEGIN
     }
   }
 
-  // download model asset
+  // download model asset only if not exist before
   NSString *assetUrlString = [model objectForKey:ASSET_URI_KEY];
   NSString *assetFilePath;
   if (assetUrlString.length > 0) {
-    assetFilePath = [_directoryPath stringByAppendingPathComponent:[NSString stringWithFormat:@"%@_%@.weights", useCaseKey, model[VERSION_ID_KEY]]];
-    [self download:assetUrlString filePath:assetFilePath queue:queue group:group];
+    NSString *fileName = useCaseKey;
+    if ([useCaseKey hasPrefix:MTMLPrefixKey]) {
+      // all mtml tasks share the same weights file
+      fileName = MTMLPrefixKey;
+    }
+    assetFilePath = [_directoryPath stringByAppendingPathComponent:[NSString stringWithFormat:@"%@_%@.weights", fileName, model[VERSION_ID_KEY]]];
+    if ([[NSFileManager defaultManager] fileExistsAtPath:assetFilePath] == false) {
+      [self download:assetUrlString filePath:assetFilePath queue:queue group:group];
+    }
   }
 
   // download rules
@@ -206,8 +255,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (void)download:(NSString *)urlString
         filePath:(NSString *)filePath
-          queue:(dispatch_queue_t)queue
-          group:(dispatch_group_t)group
+           queue:(dispatch_queue_t)queue
+           group:(dispatch_group_t)group
 {
   if (!filePath || [[NSFileManager defaultManager] fileExistsAtPath:filePath]) {
     return;
