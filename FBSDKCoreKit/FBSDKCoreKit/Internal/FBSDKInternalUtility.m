efd9f4799f9d550262712be649ee261da07a7dcf
Rob Hudson <rhudson@fb.com>
2020-02-26 09:02:20 -0800

    Fix findWindow issue with higher window levels
    Summary: The `findWindow:` method in FBSDKInternalUtility returns `nil` if the keyWindow is set to a higher level than UIWindowLevelNormal. This was causing the login dialog to not be visible when there is a keyWindow with a higher window level.
    
    Differential Revision: D20102956
    
    fbshipit-source-id: ddfbfaa5665f0484484e6bf839c791ea702fae75

--- FBSDKCoreKit/FBSDKCoreKit/Internal/FBSDKInternalUtility.m
+++ FBSDKCoreKit/FBSDKCoreKit/Internal/FBSDKInternalUtility.m
@@ -491,33 +491,34 @@ static NSMapTable *_transientObjects;
 
 + (UIWindow *)findWindow
 {
-  UIWindow *window = [UIApplication sharedApplication].keyWindow;
-  if (window == nil || window.windowLevel != UIWindowLevelNormal) {
-    for (window in [UIApplication sharedApplication].windows) {
-      if (window.windowLevel == UIWindowLevelNormal) {
-        break;
+  UIWindow *topWindow = [UIApplication sharedApplication].keyWindow;
+  if (topWindow == nil || topWindow.windowLevel < UIWindowLevelNormal) {
+    for (UIWindow *window in [UIApplication sharedApplication].windows) {
+      if (window.windowLevel >= topWindow.windowLevel && !window.isHidden) {
+        topWindow = window;
       }
     }
+  }
+
+  if (topWindow != nil) {
+    return topWindow;
   }
 
   // Find active key window from UIScene
   if (@available(iOS 13.0, tvOS 13, *)) {
     NSSet *scenes = [[UIApplication sharedApplication] valueForKey:@"connectedScenes"];
     for (id scene in scenes) {
-      if (window) {
-        break;
-      }
-
       id activationState = [scene valueForKeyPath:@"activationState"];
       BOOL isActive = activationState != nil && [activationState integerValue] == 0;
       if (isActive) {
         Class WindowScene = NSClassFromString(@"UIWindowScene");
         if ([scene isKindOfClass:WindowScene]) {
           NSArray<UIWindow *> *windows = [scene valueForKeyPath:@"windows"];
-          for (UIWindow *w in windows) {
-            if (w.isKeyWindow) {
-              window = w;
-              break;
+          for (UIWindow *window in windows) {
+            if (window.isKeyWindow) {
+              return window;
+            } else if (window.windowLevel >= topWindow.windowLevel && !window.isHidden) {
+              topWindow = window;
             }
           }
         }
@@ -525,11 +526,11 @@ static NSMapTable *_transientObjects;
     }
   }
 
-  if (window == nil) {
+  if (topWindow == nil) {
     [FBSDKLogger singleShotLogEntry:FBSDKLoggingBehaviorDeveloperErrors
-                       formatString:@"Unable to find a valid UIWindow", nil];
+                     formatString:@"Unable to find a valid UIWindow", nil];
   }
-  return window;
+  return topWindow;
 }
 
 + (UIViewController *)topMostViewController
