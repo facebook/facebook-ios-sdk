0173600043cd4fc5038f78c8ccad9956b0055072
Daniel Loomb <dloomb@fb.com>
2020-03-02 18:12:50 -0800

    Adding a class that wraps Gaming video upload (#1264)
    Summary:
    Pull Request resolved: https://github.com/facebook/facebook-ios-sdk/pull/1264
    
    We want to provide a clean way for 3rd party devs to implement the gaming video upload call. This adds a simple wrapper around the video upload logic.
    
    Follows a similar style to GamingImageUploader.
    
    Reviewed By: joesus
    
    Differential Revision: D20122377
    
    fbshipit-source-id: 4ea9131209da2ba107b84eeebb5577606f9e177c

--- FBSDKGamingServicesKit/FBSDKGamingServicesKit/FBSDKGamingVideoUploaderConfiguration.m
+++ FBSDKGamingServicesKit/FBSDKGamingServicesKit/FBSDKGamingVideoUploaderConfiguration.m
@@ -16,23 +16,18 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#if defined FBSDKCOCOAPODS || defined BUCK
-
-#import <FBSDKGamingServicesKit/FBSDKFriendFinderDialog.h>
-#import <FBSDKGamingServicesKit/FBSDKGamingImageUploader.h>
-#import <FBSDKGamingServicesKit/FBSDKGamingImageUploaderConfiguration.h>
-#import <FBSDKGamingServicesKit/FBSDKGamingServiceCompletionHandler.h>
-#import <FBSDKGamingServicesKit/FBSDKGamingVideoUploader.h>
-#import <FBSDKGamingServicesKit/FBSDKGamingVideoUploaderConfiguration.h>
-
-#else
-
-#import "FBSDKFriendFinderDialog.h"
-#import "FBSDKGamingImageUploader.h"
-#import "FBSDKGamingImageUploaderConfiguration.h"
-#import "FBSDKGamingServiceCompletionHandler.h"
-#import "FBSDKGamingVideoUploader.h"
 #import "FBSDKGamingVideoUploaderConfiguration.h"
 
-#endif
+@implementation FBSDKGamingVideoUploaderConfiguration
 
+- (instancetype)initWithVideoURL:(NSURL * _Nonnull)videoURL
+                         caption:(NSString * _Nullable)caption;
+{
+  if (self = [super init]) {
+    _videoURL = videoURL;
+    _caption = caption;
+  }
+  return self;
+}
+
+@end
