76850c9c9f928527232f1952c604859955dbaa06
Tao Chen <oliverct@fb.com>
2020-03-20 17:51:33 -0700

    Adjust model structure based on usecase
    Summary:
    Support Old Model Structure:
    https://our.intern.facebook.com/intern/diffusion/FBS/browse/master/fbobjc/ios-sdk/FBSDKCoreKit/FBSDKCoreKit/AppEvents/Internal/ML/FBSDKModelRuntime.hpp?commit=4633dcc29098f0303e4e6e272bd0f9538bd3e4fb&lines=258-280
    
    Reviewed By: KylinChang
    
    Differential Revision: D20554106
    
    fbshipit-source-id: aa43b45963e25c6544b2f8e1405d279bf07eac66

--- FBSDKCoreKit/FBSDKCoreKit/AppEvents/Internal/ML/FBSDKModelRuntime.hpp
+++ FBSDKCoreKit/FBSDKCoreKit/AppEvents/Internal/ML/FBSDKModelRuntime.hpp
@@ -31,7 +31,8 @@
 
 #define SEQ_LEN 128
 #define ALPHABET_SIZE 256
-#define EMBEDDING_SIZE 32
+#define MTML_EMBEDDING_SIZE 32
+#define NON_MTML_EMBEDDING_SIZE 64
 #define DENSE_FEATURE_LEN 30
 
 namespace mat1 {
@@ -210,7 +211,7 @@ namespace mat1 {
         return a;
     }
 
-    static float* predict(const std::string task, const char *texts, const std::unordered_map<std::string, mat::MTensor>& weights, const float *df) {
+    static float* predictOnMTML(const std::string task, const char *texts, const std::unordered_map<std::string, mat::MTensor>& weights, const float *df) {
         int *x;
         float *embed_x;
         float *c0, *c1, *c2;
@@ -222,21 +223,21 @@ namespace mat1 {
         std::string final_layer_bias_key = task + ".bias";
 
         const mat::MTensor& embed_t = weights.at("embed.weight");
-        const mat::MTensor& conv0w_t = weights.at("convs.0.weight"); // (32, 64, 2)
+        const mat::MTensor& conv0w_t = weights.at("convs.0.weight");
         const mat::MTensor& conv1w_t = weights.at("convs.1.weight");
         const mat::MTensor& conv2w_t = weights.at("convs.2.weight");
         const mat::MTensor& conv0b_t = weights.at("convs.0.bias");
         const mat::MTensor& conv1b_t = weights.at("convs.1.bias");
         const mat::MTensor& conv2b_t = weights.at("convs.2.bias");
-        const mat::MTensor& fc1w_t = weights.at("fc1.weight"); // (128, 126)
-        const mat::MTensor& fc1b_t = weights.at("fc1.bias");  // 128
+        const mat::MTensor& fc1w_t = weights.at("fc1.weight"); // (128, 190)
+        const mat::MTensor& fc1b_t = weights.at("fc1.bias"); // 128
         const mat::MTensor& fc2w_t = weights.at("fc2.weight"); // (64, 128)
         const mat::MTensor& fc2b_t = weights.at("fc2.bias"); // 64
-        const mat::MTensor& final_layer_weight_t = weights.at(final_layer_weight_key); // (2, 64) or (4, 64)
-        const mat::MTensor& final_layer_bias_t = weights.at(final_layer_bias_key); // 2 or 4
+        const mat::MTensor& final_layer_weight_t = weights.at(final_layer_weight_key); // (2, 64) or (5, 64)
+        const mat::MTensor& final_layer_bias_t = weights.at(final_layer_bias_key); // 2 or 5
 
         const float *embed_weight = embed_t.data<float>();
-        const float *convs_0_weight = transpose3D(conv0w_t.data<float>(), (int)conv0w_t.size(0), (int)conv0w_t.size(1), (int)conv0w_t.size(2)); // (2, 64, 32)
+        const float *convs_0_weight = transpose3D(conv0w_t.data<float>(), (int)conv0w_t.size(0), (int)conv0w_t.size(1), (int)conv0w_t.size(2));
         const float *convs_1_weight = transpose3D(conv1w_t.data<float>(), (int)conv1w_t.size(0), (int)conv1w_t.size(1), (int)conv1w_t.size(2));
         const float *convs_2_weight = transpose3D(conv2w_t.data<float>(), (int)conv2w_t.size(0), (int)conv2w_t.size(1), (int)conv2w_t.size(2));
         const float *convs_0_bias = conv0b_t.data<float>();
@@ -255,11 +256,11 @@ namespace mat1 {
         x = vectorize(texts, (int)strlen(texts), SEQ_LEN);
 
         // embedding
-        embed_x = embedding(x, embed_weight, 1, SEQ_LEN, EMBEDDING_SIZE); // (1, 128, 32)
+        embed_x = embedding(x, embed_weight, 1, SEQ_LEN, MTML_EMBEDDING_SIZE); // (1, 128, 32)
         free(x);
 
         // conv0
-        c0 = conv1D(embed_x, convs_0_weight, 1, SEQ_LEN, EMBEDDING_SIZE, (int)conv0w_t.size(2), (int)conv0w_t.size(0)); // (1, 126, 32)
+        c0 = conv1D(embed_x, convs_0_weight, 1, SEQ_LEN, MTML_EMBEDDING_SIZE, (int)conv0w_t.size(2), (int)conv0w_t.size(0)); // (1, 126, 32)
         c0_shape = (int)(SEQ_LEN - conv0w_t.size(2) + 1);
         add(c0, convs_0_bias, 1, c0_shape, (int)conv0w_t.size(0));
         relu(c0, c0_shape * (int)conv0w_t.size(0));
@@ -280,9 +281,112 @@ namespace mat1 {
         relu(c2, c2_shape * (int)conv2w_t.size(0));
 
         // max pooling
-        ca = maxPool1D(c0, 1, c0_shape, (int)conv0w_t.size(0), c0_shape); // (1, 1, 32)
-        cb = maxPool1D(c1, 1, c1_shape, (int)conv1w_t.size(0), c1_shape); // (1, 1, 64)
-        cc = maxPool1D(c2, 1, c2_shape, (int)conv2w_t.size(0), c2_shape); // (1, 1, 64)
+        ca = maxPool1D(c0, 1, c0_shape, (int)conv0w_t.size(0), c0_shape);
+        cb = maxPool1D(c1, 1, c1_shape, (int)conv1w_t.size(0), c1_shape);
+        cc = maxPool1D(c2, 1, c2_shape, (int)conv2w_t.size(0), c2_shape);
+        free(c0);
+        free(c1);
+        free(c2);
+
+        // concatenate
+        float *concat = (float *)malloc((size_t)(sizeof(float) * (conv0w_t.size(0) + conv1w_t.size(0) + conv2w_t.size(0) + 30)));
+        concatenate(concat, ca, cb, (int)conv0w_t.size(0), (int)conv1w_t.size(0));
+        concatenate(concat + conv0w_t.size(0) + conv1w_t.size(0), cc, df, (int)conv2w_t.size(0), 30);
+        free(ca);
+        free(cb);
+        free(cc);
+
+        // dense + relu
+        dense1_x = dense(concat, fc1_weight, fc1_bias, 1, (int)fc1w_t.size(1), (int)fc1w_t.size(0));
+        free(concat);
+        relu(dense1_x, (int)fc1b_t.size(0));
+        dense2_x = dense(dense1_x, fc2_weight, fc2_bias, 1, (int)fc2w_t.size(1), (int)fc2w_t.size(0));
+        relu(dense2_x, (int)fc2b_t.size(0));
+        free(dense1_x);
+        final_layer_dense_x = dense(dense2_x,
+                                    final_layer_weight,
+                                    final_layer_bias,
+                                    1,
+                                    (int)final_layer_weight_t.size(1),
+                                    (int)final_layer_weight_t.size(0));
+        free(dense2_x);
+        softmax(final_layer_dense_x, (int)final_layer_bias_t.size(0));
+        return final_layer_dense_x;
+    }
+
+    static float* predictOnNonMTML(const std::string task, const char *texts, const std::unordered_map<std::string, mat::MTensor>& weights, const float *df) {
+        int *x;
+        float *embed_x;
+        float *c0, *c1, *c2;
+        int c0_shape, c1_shape, c2_shape;
+        float *ca, *cb, *cc;
+        float *dense1_x, *dense2_x;
+        float *final_layer_dense_x;
+        std::string final_layer_weight_key = task + ".weight";
+        std::string final_layer_bias_key = task + ".bias";
+
+        const mat::MTensor& embed_t = weights.at("embed.weight");
+        const mat::MTensor& conv0w_t = weights.at("convs.0.weight");
+        const mat::MTensor& conv1w_t = weights.at("convs.1.weight");
+        const mat::MTensor& conv2w_t = weights.at("convs.2.weight");
+        const mat::MTensor& conv0b_t = weights.at("convs.0.bias");
+        const mat::MTensor& conv1b_t = weights.at("convs.1.bias");
+        const mat::MTensor& conv2b_t = weights.at("convs.2.bias");
+        const mat::MTensor& fc1w_t = weights.at("fc1.weight"); // (128, 126)
+        const mat::MTensor& fc1b_t = weights.at("fc1.bias"); // 128
+        const mat::MTensor& fc2w_t = weights.at("fc2.weight"); // (64, 128)
+        const mat::MTensor& fc2b_t = weights.at("fc2.bias"); // 64
+        const mat::MTensor& final_layer_weight_t = weights.at(final_layer_weight_key); // (2, 64) or (4, 64)
+        const mat::MTensor& final_layer_bias_t = weights.at(final_layer_bias_key); // 2 or 4
+
+        const float *embed_weight = embed_t.data<float>();
+        const float *convs_0_weight = transpose3D(conv0w_t.data<float>(), (int)conv0w_t.size(0), (int)conv0w_t.size(1), (int)conv0w_t.size(2));
+        const float *convs_1_weight = transpose3D(conv1w_t.data<float>(), (int)conv1w_t.size(0), (int)conv1w_t.size(1), (int)conv1w_t.size(2));
+        const float *convs_2_weight = transpose3D(conv2w_t.data<float>(), (int)conv2w_t.size(0), (int)conv2w_t.size(1), (int)conv2w_t.size(2));
+        const float *convs_0_bias = conv0b_t.data<float>();
+        const float *convs_1_bias = conv1b_t.data<float>();
+        const float *convs_2_bias = conv2b_t.data<float>();
+        const float *fc1_weight = transpose2D(fc1w_t.data<float>(), (int)fc1w_t.size(0), (int)fc1w_t.size(1));
+        const float *fc2_weight = transpose2D(fc2w_t.data<float>(), (int)fc2w_t.size(0), (int)fc2w_t.size(1));
+        const float *final_layer_weight = transpose2D(final_layer_weight_t.data<float>(),
+                                                      (int)final_layer_weight_t.size(0),
+                                                      (int)final_layer_weight_t.size(1));
+        const float *fc1_bias = fc1b_t.data<float>();
+        const float *fc2_bias = fc2b_t.data<float>();
+        const float *final_layer_bias = final_layer_bias_t.data<float>();
+
+        // vectorize text
+        x = vectorize(texts, (int)strlen(texts), SEQ_LEN);
+
+        // embedding
+        embed_x = embedding(x, embed_weight, 1, SEQ_LEN, NON_MTML_EMBEDDING_SIZE); // (1, 128, 64)
+        free(x);
+
+        // conv1D
+        c0 = conv1D(embed_x, convs_0_weight, 1, SEQ_LEN, NON_MTML_EMBEDDING_SIZE, (int)conv0w_t.size(2), (int)conv0w_t.size(0)); // (1, 127, 32)
+        c1 = conv1D(embed_x, convs_1_weight, 1, SEQ_LEN, NON_MTML_EMBEDDING_SIZE, (int)conv1w_t.size(2), (int)conv1w_t.size(0)); // (1, 126, 32)
+        c2 = conv1D(embed_x, convs_2_weight, 1, SEQ_LEN, NON_MTML_EMBEDDING_SIZE, (int)conv2w_t.size(2), (int)conv2w_t.size(0)); // (1, 124, 32)
+        free(embed_x);
+
+        // shape
+        c0_shape = (int)(SEQ_LEN - conv0w_t.size(2) + 1);
+        c1_shape = (int)(SEQ_LEN - conv1w_t.size(2) + 1);
+        c2_shape = (int)(SEQ_LEN - conv2w_t.size(2) + 1);
+
+        // add bias
+        add(c0, convs_0_bias, 1, c0_shape, (int)conv0w_t.size(0));
+        add(c1, convs_1_bias, 1, c1_shape, (int)conv1w_t.size(0));
+        add(c2, convs_2_bias, 1, c2_shape, (int)conv2w_t.size(0));
+
+        // relu
+        relu(c0, c0_shape * (int)conv0w_t.size(0));
+        relu(c1, c1_shape * (int)conv1w_t.size(0));
+        relu(c2, c2_shape * (int)conv2w_t.size(0));
+
+        // max pooling
+        ca = maxPool1D(c0, 1, c0_shape, (int)conv0w_t.size(0), c0_shape);
+        cb = maxPool1D(c1, 1, c1_shape, (int)conv1w_t.size(0), c1_shape);
+        cc = maxPool1D(c2, 1, c2_shape, (int)conv2w_t.size(0), c2_shape);
         free(c0);
         free(c1);
         free(c2);
@@ -316,11 +420,11 @@ namespace mat1 {
     static float* predictOnText(const std::string key, const char *texts, const std::unordered_map<std::string, mat::MTensor>& weights, const float *df) {
         // switch to MTML key if needed
         if (key.compare("MTML_APP_EVENT_PRED") == 0) {
-            return predict("app_event_pred", texts, weights, df);
+            return predictOnMTML("app_event_pred", texts, weights, df);
         } else if (key.compare("MTML_ADDRESS_DETECT") == 0) {
-            return predict("address_detect", texts, weights, df);
+            return predictOnMTML("address_detect", texts, weights, df);
         }
-        return predict("fc3", texts, weights, df);
+        return predictOnNonMTML("fc3", texts, weights, df);
     }
 }
 
