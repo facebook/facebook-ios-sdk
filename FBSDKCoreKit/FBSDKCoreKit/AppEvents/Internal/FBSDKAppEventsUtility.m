e11752e5cda8e68d72074e27e9442fe6e11216ef
Zilin Zhang <kylinchang@fb.com>
2020-04-02 02:27:36 -0700

    Add flag for advertiser_id_collection_enabled
    Reviewed By: YOUDAN
    
    Differential Revision: D20725662
    
    fbshipit-source-id: 8c09730f8500995d7dd47103f1bd003dda9b776d

--- FBSDKCoreKit/FBSDKCoreKit/AppEvents/Internal/FBSDKAppEventsUtility.m
+++ FBSDKCoreKit/FBSDKCoreKit/AppEvents/Internal/FBSDKAppEventsUtility.m
@@ -69,6 +69,7 @@
   }
 
   parameters[@"application_tracking_enabled"] = @(!FBSDKSettings.limitEventAndDataUsage).stringValue;
+  parameters[@"advertiser_id_collection_enabled"] = @(FBSDKSettings.advertiserIDCollectionEnabled).stringValue;
 
   NSString *userID = [FBSDKAppEvents userID];
   if (userID) {
