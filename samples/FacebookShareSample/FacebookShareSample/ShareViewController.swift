9829363632c0740f58521a93d7c375086bd844ac
joesusnick <joesusnick@gmail.com>
2020-05-05 12:16:03 -0700

    Adding photo example, fixing plist issues (#1369)
    Summary:
    Thanks for proposing a pull request!
    
    To help us review the request, please complete the following:
    
    - [x] sign [contributor license agreement](https://developers.facebook.com/opensource/cla)
    - [x] I've ensured that all existing tests pass and added tests (when/where necessary)
    - [x] I've updated the documentation (when/where necessary) and [Changelog](CHANGELOG.md) (when/where necessary)
    - [x] I've added the proper label to this pull request (e.g. `bug` for bug fixes)
    
    ## Pull Request Details
    
    Adding photo sharing to Sharing sample code.
    Pull Request resolved: https://github.com/facebook/facebook-ios-sdk/pull/1369
    
    Test Plan: N/A
    
    Reviewed By: dreamolight
    
    Differential Revision: D21407320
    
    Pulled By: joesus
    
    fbshipit-source-id: 74e8b92fc6e9b47d0f549b5d3a1245854c9d6770

--- samples/FacebookShareSample/FacebookShareSample/ShareViewController.swift
+++ samples/FacebookShareSample/FacebookShareSample/ShareViewController.swift
@@ -33,12 +33,45 @@ class ShareViewController: UITableViewController {
         dialog(withContent: content).show()
     }
 
-    func dialog(withContent content: SharingContent) -> ShareDialog {
-        let dialog = ShareDialog()
-        dialog.delegate = self
-        dialog.shareContent = content
+    @IBAction func sharePhoto() {
+        #if targetEnvironment(simulator)
+        presentAlert(
+            title: "Error",
+            message: "Sharing an image will not work on a simulator. Please build to a device and try again."
+        )
+        return
+        #endif
 
-        return dialog
+        guard let image = UIImage(named: "puppy") else {
+            presentAlert(
+                title: "Invalid image",
+                message: "Could not find image to share"
+            )
+            return
+        }
+
+        let photo = SharePhoto(image: image, userGenerated: true)
+        let content = SharePhotoContent()
+        content.photos = [photo]
+
+        let dialog = self.dialog(withContent: content)
+
+        // Recommended to validate before trying to display the dialog
+        do {
+            try dialog.validate()
+        } catch {
+            presentAlert(for: error)
+        }
+
+        dialog.show()
+    }
+
+    func dialog(withContent content: SharingContent) -> ShareDialog {
+        return ShareDialog(
+            fromViewController: self,
+            content: content,
+            delegate: self
+        )
     }
 
 }
