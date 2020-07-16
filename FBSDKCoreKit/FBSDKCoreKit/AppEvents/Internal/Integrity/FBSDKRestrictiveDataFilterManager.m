534875c7ff36ada5d205c17e4f699802b4e536b9
Dustin Shahidehpour <dust@fb.com>
2020-07-16 13:33:41 -0700

    build break
    Summary:
    $title
    
    build-break
    overriding_review_checks_triggers_an_audit_and_retroactive_review
    Oncall Short Name: fbobjc_sheriff
    
    Differential Revision: D22580973
    
    fbshipit-source-id: 600b1155ca2258e057d52b1ae58e8e6176dddc18

--- FBSDKCoreKit/FBSDKCoreKit/AppEvents/Internal/Integrity/FBSDKRestrictiveDataFilterManager.m
+++ FBSDKCoreKit/FBSDKCoreKit/AppEvents/Internal/Integrity/FBSDKRestrictiveDataFilterManager.m
@@ -16,8 +16,6 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#import <atomic>
-
 #import "FBSDKRestrictiveDataFilterManager.h"
 
 #import "FBSDKBasicUtility.h"
@@ -55,10 +53,7 @@
 
 @implementation FBSDKRestrictiveDataFilterManager
 
-namespace {
-  std::atomic<BOOL> g_isRestrictiveEventFilterEnabled;
-}
-
+static BOOL g_isRestrictiveEventFilterEnabled;
 static NSMutableArray<FBSDKRestrictiveEventFilter *>  *_params;
 static NSMutableSet<NSString *> *_restrictedEvents;
 
