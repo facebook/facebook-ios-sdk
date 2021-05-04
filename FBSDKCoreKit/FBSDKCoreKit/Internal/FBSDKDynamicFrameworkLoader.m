5d6206b3210b840f24c1deb9a1247767328ee5dc
Joe Susnick <joesusnick@fb.com>
2021-05-04 15:49:29 -0700

    Removing unused symbols from Dynamic Framework Loader
    Summary: Symbols are either not used, are from deprecated libraries, or are available from the minimum supported iOS version onwards.
    
    Reviewed By: jingping2015
    
    Differential Revision: D28191619
    
    fbshipit-source-id: 11bd55fd602072d180b48ca82c53456097894a62

--- FBSDKCoreKit/FBSDKCoreKit/Internal/FBSDKDynamicFrameworkLoader.m
+++ FBSDKCoreKit/FBSDKCoreKit/Internal/FBSDKDynamicFrameworkLoader.m
@@ -165,11 +165,6 @@ _fbsdkdfl_handle_get_impl_(Security)
   _fbsdkdfl_Security_get_and_return_k(kSecAttrService);
 }
 
-+ (CFTypeRef)loadkSecAttrGeneric
-{
-  _fbsdkdfl_Security_get_and_return_k(kSecAttrGeneric);
-}
-
 + (CFTypeRef)loadkSecValueData
 {
   _fbsdkdfl_Security_get_and_return_k(kSecValueData);
@@ -259,11 +254,6 @@ NSString *fbsdkdfl_SLServiceTypeFacebook(void)
   __weak _fbsdkdfl_Social_get_and_return_constant(SLServiceTypeFacebook);
 }
 
-NSString *fbsdkdfl_SLServiceTypeTwitter(void)
-{
-  __weak _fbsdkdfl_Social_get_and_return_constant(SLServiceTypeTwitter);
-}
-
 #pragma mark - Social Classes
 
 #define _fbsdkdfl_Social_get_c(SYMBOL) _fbsdkdfl_symbol_get_c(Social, SYMBOL)
@@ -271,25 +261,6 @@ NSString *fbsdkdfl_SLServiceTypeTwitter(void)
 Class fbsdkdfl_SLComposeViewControllerClass(void)
 {
   _fbsdkdfl_Social_get_c(SLComposeViewController);
-  return c;
-}
-
-#pragma mark - MessageUI Classes
-
-_fbsdkdfl_load_framework_once_impl_(MessageUI)
-_fbsdkdfl_handle_get_impl_(MessageUI)
-
-#define _fbsdkdfl_MessageUI_get_c(SYMBOL) _fbsdkdfl_symbol_get_c(MessageUI, SYMBOL)
-
-Class fbsdkdfl_MFMailComposeViewControllerClass(void)
-{
-  _fbsdkdfl_MessageUI_get_c(MFMailComposeViewController);
-  return c;
-}
-
-Class fbsdkdfl_MFMessageComposeViewControllerClass(void)
-{
-  _fbsdkdfl_MessageUI_get_c(MFMessageComposeViewController);
   return c;
 }
 
@@ -377,66 +348,6 @@ Class fbsdkdfl_ASWebAuthenticationSessionClass(void)
   return c;
 }
 
-#pragma mark - Accounts Constants
-
-_fbsdkdfl_load_framework_once_impl_(Accounts)
-_fbsdkdfl_handle_get_impl_(Accounts)
-
-#define _fbsdkdfl_Accounts_get_and_return_NSString(SYMBOL) __weak _fbsdkdfl_get_and_return_NSString(Accounts, SYMBOL)
-
-NSString *fbsdkdfl_ACFacebookAppIdKey(void)
-{
-  _fbsdkdfl_Accounts_get_and_return_NSString(ACFacebookAppIdKey);
-}
-
-NSString *fbsdkdfl_ACFacebookAudienceEveryone(void)
-{
-  _fbsdkdfl_Accounts_get_and_return_NSString(ACFacebookAudienceEveryone);
-}
-
-NSString *fbsdkdfl_ACFacebookAudienceFriends(void)
-{
-  _fbsdkdfl_Accounts_get_and_return_NSString(ACFacebookAudienceFriends);
-}
-
-NSString *fbsdkdfl_ACFacebookAudienceKey(void)
-{
-  _fbsdkdfl_Accounts_get_and_return_NSString(ACFacebookAudienceKey);
-}
-
-NSString *fbsdkdfl_ACFacebookAudienceOnlyMe(void)
-{
-  _fbsdkdfl_Accounts_get_and_return_NSString(ACFacebookAudienceOnlyMe);
-}
-
-NSString *fbsdkdfl_ACFacebookPermissionsKey(void)
-{
-  _fbsdkdfl_Accounts_get_and_return_NSString(ACFacebookPermissionsKey);
-}
-
-#pragma mark - Accounts Classes
-
-#define _fbsdkdfl_Accounts_get_c(SYMBOL) _fbsdkdfl_symbol_get_c(Accounts, SYMBOL);
-
-Class fbsdkdfl_ACAccountStoreClass(void)
-{
-  _fbsdkdfl_Accounts_get_c(ACAccountStore);
-  return c;
-}
-
-#pragma mark - AssetsLibrary Classes
-
-_fbsdkdfl_load_framework_once_impl_(AssetsLibrary)
-_fbsdkdfl_handle_get_impl_(AssetsLibrary)
-
-#define _fbsdkdfl_AssetsLibrary_get_c(SYMBOL) _fbsdkdfl_symbol_get_c(AssetsLibrary, SYMBOL);
-
-Class fbsdkdfl_ALAssetsLibraryClass(void)
-{
-  _fbsdkdfl_AssetsLibrary_get_c(ALAssetsLibrary);
-  return c;
-}
-
 #pragma mark - CoreTelephony Classes
 
 _fbsdkdfl_load_framework_once_impl_(CoreTelephony)
@@ -448,96 +359,4 @@ Class fbsdkdfl_CTTelephonyNetworkInfoClass(void)
 {
   _fbsdkdfl_CoreTelephonyLibrary_get_c(CTTelephonyNetworkInfo);
   return c;
-}
-
-#pragma mark - CoreImage
-
-_fbsdkdfl_load_framework_once_impl_(CoreImage)
-_fbsdkdfl_handle_get_impl_(CoreImage)
-
-#define _fbsdkdfl_CoreImage_get_c(SYMBOL) _fbsdkdfl_symbol_get_c(CoreImage, SYMBOL);
-#define _fbsdkdfl_CoreImage_get_and_return_NSString(SYMBOL) _fbsdkdfl_get_and_return_NSString(CoreImage, SYMBOL)
-
-Class fbsdkdfl_CIImageClass(void)
-{
-  _fbsdkdfl_CoreImage_get_c(CIImage);
-  return c;
-}
-
-Class fbsdkdfl_CIFilterClass(void)
-{
-  _fbsdkdfl_CoreImage_get_c(CIFilter);
-  return c;
-}
-
-NSString *fbsdkdfl_kCIInputImageKey(void)
-{
-  __weak _fbsdkdfl_CoreImage_get_and_return_NSString(kCIInputImageKey);
-}
-
-NSString *fbsdkdfl_kCIInputRadiusKey(void)
-{
-  __weak _fbsdkdfl_CoreImage_get_and_return_NSString(kCIInputRadiusKey);
-}
-
-NSString *fbsdkdfl_kCIOutputImageKey(void)
-{
-  __weak _fbsdkdfl_CoreImage_get_and_return_NSString(kCIOutputImageKey);
-}
-
-#pragma mark - Photos.framework
-
-_fbsdkdfl_load_framework_once_impl_(Photos)
-_fbsdkdfl_handle_get_impl_(Photos)
-
-#define _fbsdkdfl_Photos_get_c(SYMBOL) _fbsdkdfl_symbol_get_c(Photos, SYMBOL);
-#define _fbsdkdfl_Photos_get_and_return_NSString(SYMBOL) _fbsdkdfl_get_and_return_NSString(Photos, SYMBOL)
-
-Class fbsdkdfl_PHPhotoLibrary(void)
-{
-  _fbsdkdfl_Photos_get_c(PHPhotoLibrary);
-  return c;
-}
-
-Class fbsdkdfl_PHAssetChangeRequest(void)
-{
-  _fbsdkdfl_Photos_get_c(PHAssetChangeRequest);
-  return c;
-}
-
-#pragma mark - MobileCoreServices
-
-_fbsdkdfl_load_framework_once_impl_(MobileCoreServices)
-_fbsdkdfl_handle_get_impl_(MobileCoreServices)
-
-#define _fbsdkdfl_MobileCoreServices_get_k(SYMBOL) _fbsdkdfl_symbol_get_k(MobileCoreServices, SYMBOL, CFStringRef *)
-
-#define _fbsdkdfl_MobileCoreServices_get_and_return_k(SYMBOL) \
-  _fbsdkdfl_MobileCoreServices_get_k(SYMBOL); \
-  _fbsdkdfl_return_k(MobileCoreServices, SYMBOL)
-
-#define _fbsdkdfl_MobileCoreServices_get_f(SYMBOL) _fbsdkdfl_symbol_get_f(MobileCoreServices, SYMBOL)
-
-typedef CFStringRef (*UTTypeCopyPreferredTagWithClass_type)(CFStringRef inUTI, CFStringRef inTagClass);
-
-CFStringRef fbsdkdfl_UTTypeCopyPreferredTagWithClass(CFStringRef inUTI,
-                                                     CFStringRef inTagClass)
-{
-  _fbsdkdfl_MobileCoreServices_get_f(UTTypeCopyPreferredTagWithClass);
-  return f(inUTI, inTagClass);
-}
-
-CFStringRef fbsdkdfl_kUTTagClassMIMEType(void)
-{
-  _fbsdkdfl_MobileCoreServices_get_and_return_k(kUTTagClassMIMEType);
-}
-
-CFStringRef fbsdkdfl_kUTTypeJPEG(void)
-{
-  _fbsdkdfl_MobileCoreServices_get_and_return_k(kUTTypeJPEG);
-}
-
-CFStringRef fbsdkdfl_kUTTypePNG(void)
-{
-  _fbsdkdfl_MobileCoreServices_get_and_return_k(kUTTypePNG);
 }
