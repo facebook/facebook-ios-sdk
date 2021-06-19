046eeb749a6a2cbb602561b7f3bcea669b7a33c0
Al Tirba <altirba@fb.com>
2021-06-18 19:28:02 -0700

    Dialogs For Switch And Create
    Summary: This changes the create and switch context dialogs into their own individual dialogs that share context web dialog code.Which was created in the previous diff with the FBSDKContextWebDialog.
    
    Differential Revision: D28450308
    
    fbshipit-source-id: b6b4153e168b1a4a88e20a58e29db549e1747a61

---
+++ FBSDKGamingServicesKit/FBSDKGamingServicesKit/FBSDKCreateContextDialog.m
@@ -0,0 +1,82 @@
+// Copyright (c) 2014-present, Facebook, Inc. All rights reserved.
+//
+// You are hereby granted a non-exclusive, worldwide, royalty-free license to use,
+// copy, modify, and distribute this software in source code or binary form for use
+// in connection with the web services and APIs provided by Facebook.
+//
+// As with any software that integrates with the Facebook platform, your use of
+// this software is subject to the Facebook Developer Principles and Policies
+// [http://developers.facebook.com/policy/]. This copyright notice shall be
+// included in all copies or substantial portions of the software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#import <UIKit/UIKit.h>
+
+#import "TargetConditionals.h"
+
+#if !TARGET_OS_TV
+
+ #import "FBSDKCreateContextDialog.h"
+
+ #import "FBSDKContextCreateAsyncContent.h"
+ #import "FBSDKContextDialog.h"
+ #import "FBSDKCoreKitInternalImport.h"
+
+ #define FBSDK_CONTEXT_METHOD_NAME @"context"
+ #define FBSDKWEBDIALOGFRAMEWIDTH 300
+ #define FBSDKWEBDIALOGFRAMEHEIGHT 170
+@implementation FBSDKCreateContextDialog
+
++ (instancetype)dialogWithContent:(FBSDKContextCreateAsyncContent *)content delegate:(id<FBSDKContextDialogDelegate>)delegate
+{
+  FBSDKCreateContextDialog *dialog = [self new];
+  dialog.dialogContent = content;
+  dialog.delegate = delegate;
+  return dialog;
+}
+
+- (BOOL)show
+{
+  NSError *error;
+  if (![self validateWithError:&error]) {
+    [self.delegate contextDialog:self didFailWithError:error];
+    return NO;
+  }
+
+  NSMutableDictionary *parameters = [NSMutableDictionary new];
+
+  if ([self.dialogContent isKindOfClass:[FBSDKContextCreateAsyncContent class]] && self.dialogContent) {
+    FBSDKContextCreateAsyncContent *content = (FBSDKContextCreateAsyncContent *)self.dialogContent;
+    [FBSDKTypeUtility dictionary:parameters setObject:content.playerID forKey:@"player_id"];
+  }
+
+  CGRect frame = [self createWebDialogFrameWithWidth:FBSDKWEBDIALOGFRAMEWIDTH height:FBSDKWEBDIALOGFRAMEHEIGHT];
+  self.currentWebDialog = [FBSDKWebDialog createAndShow:FBSDK_CONTEXT_METHOD_NAME parameters:parameters frame:frame delegate:self];
+
+  [FBSDKInternalUtility registerTransientObject:self];
+  return YES;
+}
+
+- (BOOL)validateWithError:(NSError *__autoreleasing *)errorRef
+{
+  if (!self.dialogContent) {
+    *errorRef = [FBSDKError invalidArgumentErrorWithDomain:FBSDKErrorDomain
+                                                      name:@"content"
+                                                     value:self.dialogContent
+                                                   message:nil];
+    return NO;
+  }
+  if ([self.dialogContent respondsToSelector:@selector(validateWithError:)]) {
+    return [self.dialogContent validateWithError:errorRef];
+  }
+  return NO;
+}
+
+@end
+#endif
