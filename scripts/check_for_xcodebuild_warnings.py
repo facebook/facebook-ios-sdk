8f04f6833f08f90a5af035324c2169b658f40f66
Jawwad Ahmad <jawwad@fb.com>
2021-10-12 09:40:49 -0700

    Fix warning: Unable to find a destination matching the provided destination specifier
    Reviewed By: samodom
    
    Differential Revision: D31571129
    
    fbshipit-source-id: d6b86442361789a0c410db9a976aae3763f4a23e

--- scripts/check_for_xcodebuild_warnings.py
+++ scripts/check_for_xcodebuild_warnings.py
@@ -34,7 +34,7 @@ def main():
             "xcodebuild clean build-for-testing",
             "-workspace FacebookSDK.xcworkspace",
             "-scheme BuildAllKits-Dynamic",
-            "-destination 'platform=iOS Simulator,name=iPhone 12,OS=14.5'",
+            "-destination 'platform=iOS Simulator,name=iPhone 13,OS=15.0'",
         ]
     )
 
