96046e6b018c9f3e70d6705dd58e51ad326d9b3e
Zilin Zhang <kylinchang@fb.com>
2021-11-06 02:06:05 -0700

    Add FBAEMUtility
    Summary: As AEMKit grows larger, we add FBAEMUitlity class and will add util functions to this class.
    
    Reviewed By: wx0165927473
    
    Differential Revision: D32116160
    
    fbshipit-source-id: 077d66a3383f6dd81818fd798f90453aacbe7624

---
+++ Sources/FBAEMKit/FBAEMUtility.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import "TargetConditionals.h"
+
+#if !TARGET_OS_TV
+
+ #import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FBAEMUtility : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#endif
---
+++ Sources/FBAEMKit/FBAEMUtility.m
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import "TargetConditionals.h"
+
+#if !TARGET_OS_TV
+
+ #import "FBAEMUtility.h"
+
+@implementation FBAEMUtility
+
+@end
+
+#endif
