a8d6bfb94082a054e09f2587bfd70f437971fe5e
Jawwad Ahmad <jawwad@fb.com>
2021-11-19 09:42:05 -0800

    Move FBSDKGamingPayloadDelegate out of FBSDKGamingPayloadObserver and into its own file
    Reviewed By: samodom
    
    Differential Revision: D32546073
    
    fbshipit-source-id: f6a73c6c8d5da24476f31c8b353b50c16630ae1b

--- FBSDKGamingServicesKit/FacebookGamingServices/FBSDKGamingPayloadObserver.h
+++ FBSDKGamingServicesKit/FacebookGamingServices/FBSDKGamingPayloadObserver.h
@@ -8,29 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-@class FBSDKGamingPayload;
-
-NS_SWIFT_NAME(GamingPayloadDelegate)
-@protocol FBSDKGamingPayloadDelegate
-
-// MARK: Game Request
-
-/**
-  Delegate method will be triggered when a `GamingPayloadObserver` parses a url with a payload and game request ID
- @param payload The payload recieved in the url
- @param gameRequestID The game request ID recieved in the url
- */
-@optional
-- (void)parsedGameRequestURLContaining:(FBSDKGamingPayload *_Nonnull)payload gameRequestID:(NSString *_Nonnull)gameRequestID;
-
-/**
- Delegate method will be triggered when a `GamingPayloadObserver` parses a gaming context url with a payload and game context token ID. The current gaming context will be update with the context ID.
- @param payload The payload recieved in the url
- */
-@optional
-- (void)parsedGamingContextURLContaining:(FBSDKGamingPayload *_Nonnull)payload;
-
-@end
+#import "FBSDKGamingPayloadDelegate.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
