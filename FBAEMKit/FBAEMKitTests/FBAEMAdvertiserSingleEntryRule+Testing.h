8a9175f831d120fe3e70762cf1902fab308cdd6a
Sam Odom <samodom@fb.com>
2021-11-29 20:41:54 -0800

    Split AEMKit testing categories into separate files
    Summary: Also added Basics to AEMKit test target
    
    Reviewed By: joesus
    
    Differential Revision: D32655966
    
    fbshipit-source-id: 856484b1ec015e2127031c1df695906aa6dbeb54

---
+++ FBAEMKit/FBAEMKitTests/FBAEMAdvertiserSingleEntryRule+Testing.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import "FBAEMAdvertiserSingleEntryRule.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FBAEMAdvertiserSingleEntryRule (Testing)
+
+- (BOOL)isMatchedWithStringValue:(nullable NSString *)stringValue
+                  numericalValue:(nullable NSNumber *)numericalValue;
+
+- (BOOL)isMatchedWithAsteriskParam:(NSString *)param
+                   eventParameters:(NSDictionary<NSString *, id> *)eventParams
+                         paramPath:(NSArray<NSString *> *)paramPath;
+
+- (BOOL)isRegexMatch:(NSString *)stringValue;
+
+- (BOOL)isAnyOf:(NSArray<NSString *> *)arrayCondition
+    stringValue:(NSString *)stringValue
+     ignoreCase:(BOOL)ignoreCase;
+
+- (void)setOperator:(FBAEMAdvertiserRuleOperator)op;
+
+@end
+
+NS_ASSUME_NONNULL_END
