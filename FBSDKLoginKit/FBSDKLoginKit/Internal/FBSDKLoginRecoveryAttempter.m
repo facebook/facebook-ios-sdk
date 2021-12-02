24e943eeccf5da4ea4083a9992f27b071e27e329
Joe Susnick <joesusnick@fb.com>
2021-12-02 15:26:03 -0800

    Adding tests for LoginRecoveryAttempter 2/2
    Summary: $title
    
    Reviewed By: jawwad
    
    Differential Revision: D32795959
    
    fbshipit-source-id: a94056ed2060e3207716c1095e06b7676442637b

--- FBSDKLoginKit/FBSDKLoginKit/Internal/FBSDKLoginRecoveryAttempter.m
+++ FBSDKLoginKit/FBSDKLoginKit/Internal/FBSDKLoginRecoveryAttempter.m
@@ -34,14 +34,15 @@
 - (void)attemptRecoveryFromError:(NSError *)error
                completionHandler:(void (^)(BOOL didRecover))completionHandler
 {
-  NSSet<NSString *> *currentPermissions = FBSDKAccessToken.currentAccessToken.permissions;
+  NSSet<NSString *> *currentPermissions = [[self.accessTokenProvider currentAccessToken] permissions];
   if (currentPermissions.count > 0) {
-    FBSDKLoginManager *login = [FBSDKLoginManager new];
-    [login logInWithPermissions:currentPermissions.allObjects fromViewController:nil handler:^(FBSDKLoginManagerLoginResult *result, NSError *loginError) {
-      // we can only consider a recovery successful if there are no declines
-      // (note this could still set an updated currentAccessToken).
-      completionHandler(!loginError && !result.isCancelled && result.declinedPermissions.count == 0);
-    }];
+    [self.loginManager logInWithPermissions:currentPermissions.allObjects
+                         fromViewController:nil
+                                    handler:^(FBSDKLoginManagerLoginResult *result, NSError *loginError) {
+                                      // we can only consider a recovery successful if there are no declines
+                                      // (note this could still set an updated currentAccessToken).
+                                      completionHandler(!loginError && !result.isCancelled && result.declinedPermissions.count == 0);
+                                    }];
   } else {
     completionHandler(NO);
   }
--- FBSDKLoginKit/FBSDKLoginKitTests/LoginRecoveryAttempterTests.swift
+++ FBSDKLoginKit/FBSDKLoginKitTests/LoginRecoveryAttempterTests.swift
@@ -60,4 +60,94 @@ class LoginRecoveryAttempterTests: XCTestCase {
       "Should be created with the provided access token provider"
     )
   }
+
+  func testAttemptingRecoveryWithoutPermissions() {
+    var capturedSuccess = true
+    attempter.attemptRecovery(fromError: SampleError()) { success in
+      capturedSuccess = success
+    }
+
+    XCTAssertNil(
+      loginManager.capturedCompletion,
+      "Should not attempt to log in if there are no permissions to refresh"
+    )
+
+    XCTAssertFalse(
+      capturedSuccess,
+      "Should complete with false if there are no permissions to refresh"
+    )
+  }
+
+  func testAttemptingRecoveryFromErrorFails() throws {
+    accessTokenWallet.currentAccessToken = SampleAccessTokens.create(withPermissions: ["foo"])
+
+    let resultErrorPairs: [(result: LoginManagerLoginResult, error: Error?)] = [
+      (
+        result: createLoginManagerResult(isCancelled: true, declinedPermissions: []),
+        error: SampleError()
+      ),
+      (
+        result: createLoginManagerResult(isCancelled: true, declinedPermissions: ["email"]),
+        error: SampleError()
+      ),
+      (
+        result: createLoginManagerResult(isCancelled: false, declinedPermissions: []),
+        error: SampleError()
+      ),
+      (
+        result: createLoginManagerResult(isCancelled: false, declinedPermissions: ["email"]),
+        error: SampleError()
+      ),
+      (
+        result: createLoginManagerResult(isCancelled: true, declinedPermissions: []),
+        error: nil
+      ),
+      (
+        result: createLoginManagerResult(isCancelled: true, declinedPermissions: ["email"]),
+        error: nil
+      ),
+      (
+        result: createLoginManagerResult(isCancelled: false, declinedPermissions: ["email"]),
+        error: nil
+      )
+    ]
+
+    try resultErrorPairs.forEach { pair in
+      var capturedSuccess = true
+      attempter.attemptRecovery(fromError: SampleError()) { success in
+        capturedSuccess = success
+      }
+
+      let completion = try XCTUnwrap(loginManager.capturedCompletion)
+      completion(pair.result, pair.error)
+
+      let success = try XCTUnwrap(capturedSuccess)
+      XCTAssertFalse(success)
+    }
+  }
+
+  func testAttemptingRecoveryFromErrorSucceeds() throws {
+    accessTokenWallet.currentAccessToken = SampleAccessTokens.create(withPermissions: ["foo"])
+
+    var capturedSuccess = false
+    attempter.attemptRecovery(fromError: SampleError()) { success in
+      capturedSuccess = success
+    }
+
+    let completion = try XCTUnwrap(loginManager.capturedCompletion)
+    completion(createLoginManagerResult(isCancelled: false, declinedPermissions: []), nil)
+
+    let success = try XCTUnwrap(capturedSuccess)
+    XCTAssertTrue(success)
+  }
+
+  func createLoginManagerResult(isCancelled: Bool, declinedPermissions: [String]) -> LoginManagerLoginResult {
+    LoginManagerLoginResult(
+      token: SampleAccessTokens.validToken,
+      authenticationToken: SampleAuthenticationToken.validToken,
+      isCancelled: isCancelled,
+      grantedPermissions: [],
+      declinedPermissions: Set(declinedPermissions)
+    )
+  }
 }
