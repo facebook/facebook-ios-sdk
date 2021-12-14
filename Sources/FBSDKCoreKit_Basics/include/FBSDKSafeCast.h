700e1f3f90aaf60d6c1a29a8be08dcef7ed0b0ca
Jawwad Ahmad <jawwad@fb.com>
2021-12-14 08:07:00 -0800

    Remove unused "FBSDK_CAST_TO_" macros
    Summary: FBSDK_CAST_TO_CLASS_OR_NIL_FUNC and FBSDK_CAST_TO_PROTOCOL_OR_NIL_FUNC were unused
    
    Reviewed By: samodom
    
    Differential Revision: D33038674
    
    fbshipit-source-id: a72ae318c85e796e7e5a2551b3f8c5dc117d0b98

--- Sources/FBSDKCoreKit_Basics/include/FBSDKSafeCast.h
+++ Sources/FBSDKCoreKit_Basics/include/FBSDKSafeCast.h
@@ -8,32 +8,11 @@
 
 #import <Foundation/Foundation.h>
 
-#ifndef FBSDK_CAST_TO_CLASS_OR_NIL_FUNC
- #define FBSDK_CAST_TO_CLASS_OR_NIL_FUNC
- #ifdef __cplusplus
-extern "C" {
- #endif
-/** Use the type-safe FBSDK_CAST_TO_CLASS_OR_NIL instead. */
 id _FBSDKCastToClassOrNilUnsafeInternal(id object, Class klass);
- #ifdef __cplusplus
-}
- #endif
-#endif
+id _FBSDKCastToProtocolOrNilUnsafeInternal(id object, Protocol *protocol);
 
 #ifndef FBSDK_CAST_TO_CLASS_OR_NIL
  #define FBSDK_CAST_TO_CLASS_OR_NIL(obj_, class_) ((class_ *)_FBSDKCastToClassOrNilUnsafeInternal(obj_, [class_ class]))
-#endif
-
-#ifndef FBSDK_CAST_TO_PROTOCOL_OR_NIL_FUNC
- #define FBSDK_CAST_TO_PROTOCOL_OR_NIL_FUNC
- #ifdef __cplusplus
-extern "C" {
- #endif
-/** Use the type-safe FBSDK_CAST_TO_PROTOCOL_OR_NIL instead. */
-id _FBSDKCastToProtocolOrNilUnsafeInternal(id object, Protocol *protocol);
- #ifdef __cplusplus
-}
- #endif
 #endif
 
 #ifndef FBSDK_CAST_TO_PROTOCOL_OR_NIL
