3253b1b09d34b2081987ef40db108eca5315d18c
Jawwad Ahmad <jawwad@fb.com>
2022-02-01 08:50:43 -0800

    Move all tests out of Tests/FBSDKGamingServicesKit and into FacebookGamingServices/FacebookGamingServicesTests
    Summary: Also removed the FBSDKGamingServicesKitTests target and related code
    
    Reviewed By: KylinChang
    
    Differential Revision: D33910596
    
    fbshipit-source-id: 5f86e606d73dc36c7268252056f9afab5d9aa811

--- FBSDKGamingServicesKit/FacebookGamingServicesTests/FBSDKSwitchContextDialogTests.swift
+++ FBSDKGamingServicesKit/FacebookGamingServicesTests/FBSDKSwitchContextDialogTests.swift
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import FBSDKGamingServicesKit
+import FacebookGamingServices
 import TestTools
 import XCTest
 
--- FBSDKGamingServicesKit/FacebookGamingServicesTests/SymbolVisibilityTests_FBSDKGamingServicesKit.swift
+++ FBSDKGamingServicesKit/FacebookGamingServicesTests/SymbolVisibilityTests_FBSDKGamingServicesKit.swift
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import FBSDKGamingServicesKit
+import FacebookGamingServices
 import XCTest
 
 class SymbolVisibilityTests_FBSDKGamingServicesKit: XCTestCase { // swiftlint:disable:this type_name
