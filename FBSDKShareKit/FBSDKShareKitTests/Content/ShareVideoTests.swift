4e9845d474bb5e92fc6340d6e3e47e7ea52499de
Sam Odom <samodom@fb.com>
2022-03-04 06:37:25 -0800

    Backfill tests for ShareVideo in ShareKit
    Reviewed By: jawwad
    
    Differential Revision: D34584011
    
    fbshipit-source-id: ae42a9d2cbdd940e510443d5e61058b462105c8a

--- FBSDKShareKit/FBSDKShareKitTests/Content/ShareVideoTests.swift
+++ FBSDKShareKit/FBSDKShareKitTests/Content/ShareVideoTests.swift
@@ -92,6 +92,146 @@ final class ShareVideoTests: XCTestCase {
     XCTAssertNil(video.videoURL, .otherSourcesClearedWhenChangingSource)
     XCTAssertNil(video.previewPhoto, .previewPhotoClearedWhenChangingSource)
   }
+
+  func testValidatingWithoutSource() {
+    video = ShareVideo()
+
+    XCTAssertThrowsError(try video.validate(options: []), .failsValidationWithoutSource) { error in
+      guard let sdkError = error as? TestSDKError else {
+        return XCTFail(.failsValidationWithoutSource)
+      }
+
+      XCTAssertEqual(sdkError.domain, ShareErrorDomain, .failsValidationWithoutSource)
+      XCTAssertEqual(sdkError.name, "video", .failsValidationWithoutSource)
+      XCTAssertIdentical(sdkError.value as AnyObject, video, .failsValidationWithoutSource)
+      XCTAssertEqual(
+        sdkError.message,
+        "Must have an asset, data, or videoURL value.",
+        .failsValidationWithoutSource
+      )
+      XCTAssertNil(sdkError.underlyingError, .failsValidationWithoutSource)
+    }
+  }
+
+  func testValidationFailureWithDataSource() {
+    let data = Data()
+    video = ShareVideo(data: data, previewPhoto: nil)
+
+    XCTAssertThrowsError(
+      try video.validate(options: .excluding(.videoData)),
+      .failsBadOptionsValidationWithDataSource
+    ) { error in
+      guard let sdkError = error as? TestSDKError else {
+        return XCTFail(.failsBadOptionsValidationWithDataSource)
+      }
+
+      XCTAssertEqual(sdkError.domain, ShareErrorDomain, .failsBadOptionsValidationWithDataSource)
+      XCTAssertEqual(sdkError.name, "data", .failsBadOptionsValidationWithDataSource)
+      XCTAssertEqual(sdkError.value as? Data, data, .failsBadOptionsValidationWithDataSource)
+      XCTAssertEqual(
+        sdkError.message,
+        "Cannot share video data.",
+        .failsBadOptionsValidationWithDataSource
+      )
+      XCTAssertNil(sdkError.underlyingError, .failsBadOptionsValidationWithDataSource)
+    }
+  }
+
+  func testValidationSuccessWithDataSource() {
+    video = ShareVideo(data: Data(), previewPhoto: nil)
+    XCTAssertNoThrow(try video.validate(options: .videoData), .passesValidationWithDataSource)
+  }
+
+  func testValidationFailureWithAssetSource() {
+    let asset = TestPHAsset()
+    asset.stubbedMediaType = .audio
+    video = ShareVideo(videoAsset: asset, previewPhoto: nil)
+
+    XCTAssertThrowsError(
+      try video.validate(options: []),
+      .failsBadMediaTypeValidationWithAssetSource
+    ) { error in
+      guard let sdkError = error as? TestSDKError else {
+        return XCTFail(.failsBadMediaTypeValidationWithAssetSource)
+      }
+
+      XCTAssertEqual(sdkError.domain, ShareErrorDomain, .failsBadMediaTypeValidationWithAssetSource)
+      XCTAssertEqual(sdkError.name, "videoAsset", .failsBadMediaTypeValidationWithAssetSource)
+      XCTAssertIdentical(sdkError.value as AnyObject, asset, .failsBadMediaTypeValidationWithAssetSource)
+      XCTAssertEqual(
+        sdkError.message,
+        "Must refer to a video file.",
+        .failsBadMediaTypeValidationWithAssetSource
+      )
+      XCTAssertNil(sdkError.underlyingError, .failsBadMediaTypeValidationWithAssetSource)
+    }
+  }
+
+  func testValidationSuccessWithAssetSource() {
+    let asset = TestPHAsset()
+    asset.stubbedMediaType = .video
+    video = ShareVideo(videoAsset: asset, previewPhoto: nil)
+
+    XCTAssertNoThrow(try video.validate(options: .videoData), .passesValidationWithAssetSource)
+  }
+
+  func testValidationFailureWithRemoteURLSource() {
+    let url = URL.remoteVideo
+    video = ShareVideo(videoURL: url, previewPhoto: nil)
+
+    XCTAssertThrowsError(
+      try video.validate(options: []),
+      .failsValidationWithRemoteURLSource
+    ) { error in
+      guard let sdkError = error as? TestSDKError else {
+        return XCTFail(.failsValidationWithRemoteURLSource)
+      }
+
+      XCTAssertEqual(sdkError.domain, ShareErrorDomain, .failsValidationWithRemoteURLSource)
+      XCTAssertEqual(sdkError.name, "videoURL", .failsValidationWithRemoteURLSource)
+      XCTAssertEqual(sdkError.value as? URL, url, .failsValidationWithRemoteURLSource)
+      XCTAssertEqual(
+        sdkError.message,
+        "Must refer to an asset file.",
+        .failsValidationWithRemoteURLSource
+      )
+      XCTAssertNil(sdkError.underlyingError, .failsValidationWithRemoteURLSource)
+    }
+  }
+
+  func testValidationFailureWithFileURLSource() {
+    let url = URL.localVideo
+    video = ShareVideo(videoURL: url, previewPhoto: nil)
+
+    XCTAssertThrowsError(
+      try video.validate(options: .excluding(.videoData)),
+      .failsValidationWithFileURLSource
+    ) { error in
+      guard let sdkError = error as? TestSDKError else {
+        return XCTFail(.failsValidationWithFileURLSource)
+      }
+
+      XCTAssertEqual(sdkError.domain, ShareErrorDomain, .failsValidationWithFileURLSource)
+      XCTAssertEqual(sdkError.name, "videoURL", .failsValidationWithFileURLSource)
+      XCTAssertEqual(sdkError.value as? URL, url, .failsValidationWithFileURLSource)
+      XCTAssertEqual(
+        sdkError.message,
+        "Must refer to an asset file.",
+        .failsValidationWithFileURLSource
+      )
+      XCTAssertNil(sdkError.underlyingError, .failsValidationWithFileURLSource)
+    }
+  }
+
+  func testValidationSuccessWithAssetLibraryURLSource() throws {
+    video = ShareVideo(videoURL: .videoAsset, previewPhoto: nil)
+    XCTAssertNoThrow(try video.validate(options: []))
+  }
+
+  func testValidationSuccessWithFileURLSource() throws {
+    video = ShareVideo(videoURL: .localVideo, previewPhoto: nil)
+    XCTAssertNoThrow(try video.validate(options: .videoData))
+  }
 }
 
 // MARK: - Assumptions
@@ -108,4 +248,55 @@ fileprivate extension String {
   static let canHavePreviewPhoto = "A video should be able to have a preview photo"
   static let otherSourcesClearedWhenChangingSource = "Changing a video source should clear other sources"
   static let previewPhotoClearedWhenChangingSource = "Changing a video source should clear the preview photo"
+
+  static let failsValidationWithoutSource = "Validating a video without a source should throw an error"
+  static let failsBadOptionsValidationWithDataSource = """
+    Validating a data source video without using the video data bridge option should throw an error
+    """
+  static let passesValidationWithDataSource = """
+    Validating a data source video with the video data bridge option should not throw an error
+    """
+  static let failsBadMediaTypeValidationWithAssetSource = """
+    Validating an asset source video without the video media type should throw an error
+    """
+  static let passesValidationWithAssetSource = """
+    Validating an asset source video with the video media type should not throw an error
+    """
+  static let failsValidationWithRemoteURLSource = """
+    Validating a URL source video with a remote URL should throw an error
+    """
+  static let failsValidationWithFileURLSource = """
+    Validating a URL source video with a file URL without using the video data bridge option should throw an error
+    """
+  static let passesValidationWithAssetLibraryURLSource = """
+    Validating a URL source video with an asset library URL should not throw an error
+    """
+  static let passesValidationWithFileURLSource = """
+    Validating a URL source video with a file URL and the video data bridge option should not throw an error
+    """
+}
+
+// MARK: - Test Values
+
+fileprivate extension ShareBridgeOptions {
+  static let none: Self = []
+  static let all: Self = [.photoAsset, .photoImageURL, .videoAsset, .videoData, .webHashtag]
+
+  // Randomly selects either all options minus the one specified or no options
+  static func excluding(_ option: Self) -> Self {
+    switch Bool.random() {
+    case true:
+      return all.subtracting(option)
+    case false:
+      return none
+    }
+  }
+}
+
+fileprivate extension URL {
+  // swiftlint:disable force_unwrapping
+  static let videoAsset = URL(string: "assets-library://blah-blah-blah/video.mpg")!
+  static let remoteVideo = URL(string: "https://facebook.com/myVideo.mpg")!
+  static let localVideo = URL(string: "file:///Users/anyone/myVideo.mpg")!
+  // swiftlint:enable force_unwrapping
 }
