e0219e93d0061d4eb6e5c1ddbc445d7294d96df2
Sam Odom <samodom@fb.com>
2022-03-16 08:37:32 -0700

    Clean up ShareVideoContentTests
    Summary:
    To prepare for backfilling tests:
    * Add better test values
    * Add missing assertion messages
    
    Differential Revision: D34901024
    
    fbshipit-source-id: f71ff5a3168142722b1145867043dc07a73db554

--- FBSDKShareKit/FBSDKShareKitTests/Content/ShareVideoContentTests.swift
+++ FBSDKShareKit/FBSDKShareKitTests/Content/ShareVideoContentTests.swift
@@ -9,6 +9,7 @@
 @testable import FBSDKShareKit
 
 import FBSDKCoreKit
+import TestTools
 import UIKit
 import XCTest
 
@@ -17,6 +18,8 @@ final class ShareVideoContentTests: XCTestCase {
   // swiftlint:disable implicitly_unwrapped_optional
   var content: ShareVideoContent!
   var validator: TestShareUtility.Type!
+  var errorFactory: TestErrorFactory!
+  var testError: TestSDKError!
   // swiftlint:enable implicitly_unwrapped_optional
 
   override func setUp() {
@@ -26,13 +29,21 @@ final class ShareVideoContentTests: XCTestCase {
     validator.reset()
     ShareVideoContent.setDependencies(.init(validator: TestShareUtility.self))
 
-    content = ShareModelTestUtility.videoContentWithoutPreviewPhoto
+    errorFactory = TestErrorFactory()
+    testError = TestSDKError(type: .unknown)
+    errorFactory.stubbedError = testError
+    ShareVideo.setDependencies(.init(errorFactory: errorFactory))
+
+    content = .sample()
   }
 
   override func tearDown() {
+    ShareVideo.resetDependencies()
     ShareVideoContent.resetDependencies()
     validator.reset()
     validator = nil
+    errorFactory = nil
+    testError = nil
     content = nil
 
     super.tearDown()
@@ -54,13 +65,12 @@ final class ShareVideoContentTests: XCTestCase {
   }
 
   func testProperties() {
-    let content = ShareModelTestUtility.videoContentWithPreviewPhoto
-    XCTAssertEqual(content.contentURL, ShareModelTestUtility.contentURL)
-    XCTAssertEqual(content.peopleIDs, ShareModelTestUtility.peopleIDs)
-    XCTAssertEqual(content.placeID, ShareModelTestUtility.placeID)
-    XCTAssertEqual(content.ref, ShareModelTestUtility.ref)
-    XCTAssertEqual(content.video, ShareModelTestUtility.videoWithPreviewPhoto)
-    XCTAssertEqual(content.video.previewPhoto, ShareModelTestUtility.videoWithPreviewPhoto.previewPhoto)
+    content = .sample(includesPreviewPhoto: true)
+    XCTAssertEqual(content.contentURL, .content, .hasContentURL)
+    XCTAssertEqual(content.peopleIDs, .peopleIDs, .hasPeopleIDs)
+    XCTAssertEqual(content.placeID, .placeID, .hasPlaceID)
+    XCTAssertEqual(content.ref, .ref, .hasRef)
+    XCTAssertEqual(content.video, .withPreviewPhoto, .hasVideo)
   }
 
   func testValidationWithValidContent() throws {
@@ -73,79 +83,59 @@ final class ShareVideoContentTests: XCTestCase {
     validator.validateRequiredValueShouldThrow = true
     content = ShareVideoContent()
 
-    XCTAssertThrowsError(try content.validate(options: []), .validationValidatesVideo)
-    XCTAssertIdentical(validator.validateRequiredValueValue as? ShareVideo, content.video, .validationValidatesVideo)
-    XCTAssertEqual(validator.validateRequiredValueName, "video", .validationValidatesVideo)
+    XCTAssertThrowsError(try content.validate(options: []), .validationValidatesVideo) { _ in
+      XCTAssertIdentical(validator.validateRequiredValueValue as? ShareVideo, content.video, .validationValidatesVideo)
+      XCTAssertEqual(validator.validateRequiredValueName, "video", .validationValidatesVideo)
+    }
   }
 
   func testValidationWithInvalidVideoURL() {
     content = ShareVideoContent()
     content.video = ShareVideo()
-    // swiftlint:disable:next force_unwrapping
-    content.video.videoURL = URL(string: "/")!
+    content.video.videoURL = .invalid
 
     XCTAssertThrowsError(try content.validate(options: []), .validationValidatesVideo) { error in
-      let nsError = error as NSError
-      XCTAssertNotNil(
-        nsError,
-        "Attempting to validate video share content with an empty url should return a video url specific error"
-      )
-      XCTAssertEqual(nsError.code, CoreError.errorInvalidArgument.rawValue)
-      XCTAssertEqual(nsError.userInfo[ErrorArgumentNameKey] as? String, "videoURL")
+      XCTAssertIdentical(error as AnyObject, testError, .validationValidatesVideo)
+      XCTAssertIdentical(validator.validateRequiredValueValue as? ShareVideo, content.video, .validationValidatesVideo)
+      XCTAssertEqual(validator.validateRequiredValueName, "video", .validationValidatesVideo)
     }
-    XCTAssertIdentical(validator.validateRequiredValueValue as? ShareVideo, content.video, .validationValidatesVideo)
-    XCTAssertEqual(validator.validateRequiredValueName, "video", .validationValidatesVideo)
   }
 
   func testValidationWithNonVideoURL() {
     content = ShareVideoContent()
     content.video = ShareVideo()
-    content.video.videoURL = ShareModelTestUtility.photoImageURL
-    XCTAssertNotNil(content)
+    content.video.videoURL = .photo
 
-    XCTAssertThrowsError(
-      try content.validate(options: []),
-      "Should throw an error"
-    ) { error in
-      let nsError = error as NSError
-      XCTAssertNotNil(
-        nsError,
-        "Attempting to validate video share content with a non-video url should return a video url specific error"
-      )
-      XCTAssertEqual(nsError.code, CoreError.errorInvalidArgument.rawValue)
-      XCTAssertEqual(nsError.userInfo[ErrorArgumentNameKey] as? String, "videoURL")
+    XCTAssertThrowsError(try content.validate(options: []), .validationValidatesVideo) { error in
+      XCTAssertIdentical(error as AnyObject, testError, .validationValidatesVideo)
+      XCTAssertIdentical(validator.validateRequiredValueValue as? ShareVideo, content.video, .validationValidatesVideo)
+      XCTAssertEqual(validator.validateRequiredValueName, "video", .validationValidatesVideo)
     }
   }
 
   func testValidationWithNetworkVideoURL() {
-    let video = ShareVideo(videoURL: ShareModelTestUtility.videoURL)
-    content.video = video
-    XCTAssertNoThrow(try content.validate(options: []))
+    content.video = ShareVideo(videoURL: .videoAsset)
+    XCTAssertNoThrow(try content.validate(options: []), .validationValidatesVideo)
+    XCTAssertIdentical(validator.validateRequiredValueValue as? ShareVideo, content.video, .validationValidatesVideo)
+    XCTAssertEqual(validator.validateRequiredValueName, "video", .validationValidatesVideo)
   }
 
   func testValidationWithValidFileVideoURLWhenBridgeOptionIsDefault() throws {
-    let videoURL = try XCTUnwrap(Bundle.main.resourceURL?.appendingPathComponent("video.mp4"))
-    content.video = ShareVideo(videoURL: videoURL)
+    content.video = ShareVideo(videoURL: .localVideo)
 
-    XCTAssertThrowsError(
-      try content.validate(options: []),
-      "Should throw an error"
-    ) { error in
-      let nsError = error as NSError
-      XCTAssertNotNil(
-        nsError,
-        "Attempting to validate video share content with a valid file url should return a video url specific error when there is no specified bridge option to handle video data" // swiftlint:disable:this line_length
-      )
-      XCTAssertEqual(nsError.code, CoreError.errorInvalidArgument.rawValue)
-      XCTAssertEqual(nsError.userInfo[ErrorArgumentNameKey] as? String, "videoURL")
+    XCTAssertThrowsError(try content.validate(options: []), .validationValidatesVideo) { error in
+      XCTAssertIdentical(error as AnyObject, testError, .validationValidatesVideo)
+      XCTAssertIdentical(validator.validateRequiredValueValue as? ShareVideo, content.video, .validationValidatesVideo)
+      XCTAssertEqual(validator.validateRequiredValueName, "video", .validationValidatesVideo)
     }
   }
 
   func testValidationWithValidFileVideoURLWhenBridgeOptionIsVideoData() throws {
-    let videoURL = try XCTUnwrap(Bundle.main.resourceURL?.appendingPathComponent("video.mp4"))
-    content.video = ShareVideo(videoURL: videoURL)
+    content.video = ShareVideo(videoURL: .localVideo)
 
-    XCTAssertNoThrow(try content.validate(options: [.videoData]))
+    XCTAssertNoThrow(try content.validate(options: [.videoData]), .validationValidatesVideo)
+    XCTAssertIdentical(validator.validateRequiredValueValue as? ShareVideo, content.video, .validationValidatesVideo)
+    XCTAssertEqual(validator.validateRequiredValueName, "video", .validationValidatesVideo)
   }
 }
 
@@ -157,7 +147,60 @@ fileprivate extension String {
     """
   static let usesCustomShareValidator = "The share validator dependency should be configurable"
 
+  static let hasContentURL = "A share video content has a URL"
+  static let hasPeopleIDs = "A share video content has people IDs"
+  static let hasPlaceID = "A share video content has a place ID"
+  static let hasRef = "A share video content has a ref"
+  static let hasVideo = "A share video content has a video"
+
   static let validationValidatesVideo = """
     Validating a share video content should validate its video using its validator
     """
+}
+
+// MARK: - Test values
+
+fileprivate extension URL {
+  // swiftlint:disable force_unwrapping
+  static let invalid = URL(string: "/")!
+  static let content = URL(string: "https://developers.facebook.com/")!
+  static let videoAsset = URL(string: "assets-library://asset/asset.mp4?id=86C6970B-1266-42D0-91E8-4E68127D3864&ext=mp4")!
+  static let localVideo = Bundle.main.resourceURL!.appendingPathComponent("video.mp4")
+  static let photo = URL(string: "https://fbstatic-a.akamaihd.net/rsrc.php/v2/yC/r/YRwxe7CPWSs.png")!
+  // swiftlint:enable force_unwrapping
+}
+
+fileprivate extension Hashtag {
+  static let sample = Hashtag("#sample")
+}
+
+fileprivate extension String {
+  static let placeID = "141887372509674"
+  static let ref = "sample-ref"
+}
+
+fileprivate extension Array where Element == String {
+  static let peopleIDs = ["person1", "person2"]
+}
+
+fileprivate extension ShareVideo {
+  static let withPreviewPhoto = ShareVideo(videoURL: .videoAsset, previewPhoto: .withImageURL)
+  static let withoutPreviewPhoto = ShareVideo(videoURL: .videoAsset)
+}
+
+fileprivate extension SharePhoto {
+  static let withImageURL = SharePhoto(imageURL: .photo, isUserGenerated: true)
+}
+
+fileprivate extension ShareVideoContent {
+  static func sample(includesPreviewPhoto: Bool = false) -> ShareVideoContent {
+    let content = ShareVideoContent()
+    content.contentURL = .content
+    content.hashtag = .sample
+    content.peopleIDs = .peopleIDs
+    content.placeID = .placeID
+    content.ref = .ref
+    content.video = includesPreviewPhoto ? .withPreviewPhoto : .withoutPreviewPhoto
+    return content
+  }
 }
