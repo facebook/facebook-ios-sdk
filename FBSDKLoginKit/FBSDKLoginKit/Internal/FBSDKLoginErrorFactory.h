05b285cf1612fac04075d657c80ad89cc53f7291
Pablo Brizuela Guardado <josuebrizuelaxp@fb.com>
2022-04-12 18:41:04 -0700

    Removed dead code for FBSDKLoginErrorFactory in LoginKit
    Summary: I could not find a place where these methods are called, and `FBSDKLoginErrorFactory` is internal. So I decided to remove it. This has the effect of increasing `FBSDKLoginErrorFactory` test coverage to: 84.9%
    
    Reviewed By: samodom
    
    Differential Revision: D35451682
    
    fbshipit-source-id: eac5d91c1f1b27b00e6c5a38ac53327b5422b4aa

--- FBSDKLoginKit/FBSDKLoginKit/Internal/FBSDKLoginErrorFactory.h
+++ FBSDKLoginKit/FBSDKLoginKit/Internal/FBSDKLoginErrorFactory.h
@@ -18,10 +18,7 @@ NS_SWIFT_NAME(LoginErrorFactory)
 @interface FBSDKLoginErrorFactory : NSObject
 
 + (NSError *)fbErrorForFailedLoginWithCode:(FBSDKLoginError)code;
-+ (NSError *)fbErrorForSystemPasswordChange:(NSError *)innerError;
-
 + (nullable NSError *)fbErrorFromReturnURLParameters:(NSDictionary<NSString *, id> *)parameters;
-+ (nullable NSError *)fbErrorFromServerError:(NSError *)serverError;
 
 @end
 
--- FBSDKLoginKit/FBSDKLoginKit/Internal/FBSDKLoginErrorFactory.m
+++ FBSDKLoginKit/FBSDKLoginKit/Internal/FBSDKLoginErrorFactory.m
@@ -109,28 +109,6 @@ typedef NS_ERROR_ENUM(FBSDKLoginErrorDomain, FBSDKLoginErrorSubcode)
                          userInfo:userInfo];
 }
 
-+ (NSError *)fbErrorForSystemPasswordChange:(NSError *)innerError
-{
-  NSString *failureReasonAndDescription =
-  NSLocalizedStringWithDefaultValue(
-    @"LoginError.SystemAccount.PasswordChange",
-    @"FacebookSDK",
-    [FBSDKInternalUtility.sharedUtility bundleForStrings],
-    @"Your Facebook password has changed. To confirm your password, open Settings > Facebook and tap your name.",
-    @"The user facing error message when the device Facebook account password is incorrect and login fails."
-  );
-  NSMutableDictionary<NSString *, id> *userInfo = [@{
-                                                     FBSDKErrorLocalizedDescriptionKey : failureReasonAndDescription,
-                                                     NSLocalizedDescriptionKey : failureReasonAndDescription
-                                                   } mutableCopy];
-
-  [FBSDKTypeUtility dictionary:userInfo setObject:innerError forKey:NSUnderlyingErrorKey];
-
-  return [NSError errorWithDomain:FBSDKLoginErrorDomain
-                             code:FBSDKLoginErrorPasswordChanged
-                         userInfo:userInfo];
-}
-
 + (nullable NSError *)fbErrorFromReturnURLParameters:(NSDictionary<NSString *, id> *)parameters
 {
   NSError *error = nil;
@@ -154,83 +132,6 @@ typedef NS_ERROR_ENUM(FBSDKLoginErrorDomain, FBSDKLoginErrorSubcode)
   }
 
   return error;
-}
-
-+ (nullable NSError *)fbErrorFromServerError:(NSError *)serverError
-{
-  NSError *loginError = nil;
-
-  if ([serverError.domain isEqualToString:FBSDKErrorDomain]) {
-    NSDictionary<NSString *, id> *response = [FBSDKTypeUtility dictionaryValue:serverError.userInfo[FBSDKGraphRequestErrorParsedJSONResponseKey]];
-    NSDictionary<NSString *, id> *body = [FBSDKTypeUtility dictionaryValue:response[@"body"]];
-    NSDictionary<NSString *, id> *error = [FBSDKTypeUtility dictionaryValue:body[@"error"]];
-    NSInteger subcode = [FBSDKTypeUtility integerValue:error[@"error_subcode"]];
-
-    switch (subcode) {
-      case FBSDKLoginErrorSubcodeUserCheckpointed:
-        loginError = [self fbErrorForFailedLoginWithCode:FBSDKLoginErrorUserCheckpointed
-                                              innerError:serverError];
-        break;
-      case FBSDKLoginErrorSubcodePasswordChanged:
-        loginError = [self fbErrorForFailedLoginWithCode:FBSDKLoginErrorPasswordChanged
-                                              innerError:serverError];
-        break;
-      case FBSDKLoginErrorSubcodeUnconfirmedUser:
-        loginError = [self fbErrorForFailedLoginWithCode:FBSDKLoginErrorUnconfirmedUser
-                                              innerError:serverError];
-        break;
-    }
-  }
-
-  return loginError;
-}
-
-+ (NSError *)fbErrorWithSystemAccountStoreDeniedError:(NSError *)accountStoreError
-                                       isCancellation:(BOOL *)cancellation
-{
-  // The Accounts framework returns an ACErrorPermissionDenied error for both user denied errors,
-  // Facebook denied errors, and other things. Unfortunately examining the contents of the
-  // description is the only means available to determine the reason for the error.
-  NSString *description = accountStoreError.userInfo[NSLocalizedDescriptionKey];
-  NSError *err = nil;
-
-  if (description) {
-    // If a parenthetical error code exists, map it ot a Facebook server error
-    FBSDKLoginError errorCode = FBSDKLoginErrorReserved;
-    if ([description rangeOfString:@"(459)"].location != NSNotFound) {
-      // The Facebook server could not fulfill this access request: Error validating access token:
-      // You cannot access the app till you log in to www.facebook.com and follow the instructions given. (459)
-
-      // The OAuth endpoint directs people to www.facebook.com when an account has been
-      // checkpointed. If the web address is present, assume it's due to a checkpoint.
-      errorCode = FBSDKLoginErrorUserCheckpointed;
-    } else if ([description rangeOfString:@"(452)"].location != NSNotFound
-               || [description rangeOfString:@"(460)"].location != NSNotFound) {
-      // The Facebook server could not fulfill this access request: Error validating access token:
-      // Session does not match current stored session. This may be because the user changed the password since
-      // the time the session was created or Facebook has changed the session for security reasons. (452)or(460)
-
-      // If the login failed due to the session changing, maybe it's due to the password
-      // changing. Direct the user to update the password in the Settings > Facebook.
-      err = [self fbErrorForSystemPasswordChange:accountStoreError];
-    } else if ([description rangeOfString:@"(464)"].location != NSNotFound) {
-      // The Facebook server could not fulfill this access request: Error validating access token:
-      // Sessions for the user  are not allowed because the user is not a confirmed user. (464)
-      errorCode = FBSDKLoginErrorUnconfirmedUser;
-    }
-
-    if (errorCode != FBSDKLoginErrorReserved) {
-      err = [self fbErrorForFailedLoginWithCode:errorCode];
-    }
-  } else {
-    // If there is no description, assume this is a user cancellation. No error object is
-    // returned for a cancellation.
-    if (cancellation != NULL) {
-      *cancellation = YES;
-    }
-  }
-
-  return err;
 }
 
 @end
