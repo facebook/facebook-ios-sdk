8600debb571c58fea2b1338e85be41353fc2fdcf
Sam Odom <samodom@fb.com>
2022-04-26 09:09:15 -0700

    Add DependentAsType protocol to LoginKit
    Reviewed By: joesus
    
    Differential Revision: D35847741
    
    fbshipit-source-id: 61202a2ac78a06b479f63c55d58ef7bc84d904a9

---
+++ FBSDKLoginKit/FBSDKLoginKit/Internal/DependentAsType.swift
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+protocol DependentAsType {
+  associatedtype TypeDependencies
+
+  static var configuredDependencies: TypeDependencies? { get set }
+  static var defaultDependencies: TypeDependencies? { get }
+
+  static func setDependencies(_ dependencies: TypeDependencies)
+
+  #if DEBUG
+  static func resetDependencies()
+  #endif
+}
+
+extension DependentAsType {
+  static func setDependencies(_ dependencies: TypeDependencies) {
+    configuredDependencies = dependencies
+  }
+
+  #if DEBUG
+  static func resetDependencies() {
+    configuredDependencies = nil
+  }
+  #endif
+
+  static func getDependencies() throws -> TypeDependencies {
+    guard let dependencies = configuredDependencies ?? defaultDependencies else {
+      throw MissingTypeDependenciesError(for: Self.self)
+    }
+
+    return dependencies
+  }
+}
+
+struct MissingTypeDependenciesError<Dependent: DependentAsType>: Error, CustomStringConvertible {
+  private var dependent: Dependent.Type
+
+  fileprivate init(for dependent: Dependent.Type) {
+    self.dependent = dependent
+  }
+
+  var description: String {
+    "The dependencies for the type '\(dependent)' have not been set"
+  }
+}
