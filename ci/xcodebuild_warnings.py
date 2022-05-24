aee8111ac70dca7e6fe6aaaac14222e5b32719ac
Joe Susnick <joesusnick@fb.com>
2022-05-24 15:48:14 -0700

    Adding xcodebuild warnings ci job
    Summary: $title
    
    Test Plan: passing job on this diff
    
    Reviewers: samodom, #sdk
    
    Reviewed By: samodom
    
    Differential Revision: https://phabricator.intern.facebook.com/D36487208
    
    Tags: publish_when_ready

---
+++ ci/xcodebuild_warnings.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import json
+from common import *
+
+ONCALL = 'platform_sdks'
+
+my_job = [
+    {
+        "alias": "ios-sdk-xcodebuild-warnings",
+        "capabilities": ios_sdk_capabilities(),
+        "command": "SandcastleUniversalCommand",
+        "description": "Check for xcodebuild warnings",
+        "oncall": ONCALL,
+        "priority": 0,
+        "tags": ["facebook-ios-sdk"],
+        "args": {
+            "oncall": ONCALL,
+            "steps": [
+                {
+                    "name": "Generate Project files",
+                    "shell": "./internal/scripts/generate-projects.sh",
+                },
+                {
+                    "name": "Check for warnings",
+                    "required": False,
+                    "shell": "./scripts/check_for_xcodebuild_warnings.py",
+                },
+            ],
+        },
+    }
+]
+
+print(json.dumps(my_job, indent=4))
