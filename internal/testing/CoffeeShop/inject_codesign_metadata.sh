eefebf60a758d01c5b70a5cf37ce7b3f752ce908
Joe Susnick <joesusnick@fb.com>
2022-06-13 10:26:17 -0700

    Adding codesigning support for CoffeeShop
    Summary:
    Does a couple things.
    
    1. Updates the generated coffeeshop target to use correct casing.
    2. Adds files that autograph and the www build job will need in order to sign the files.
    3. Includes a script to move the new files into the build.
    
    Future optimizations would include sharing the script that moves these files but for now the duplication is not an issue and it will be easier to get both jobs working then refactor them to share code.
    
    Test Plan:
    N/A
    
    This is all part of moving files to this repo and making sure that we can build artifacts once it becomes the source of truth. Currently the working code that people and sandcastle jobs rely on is located in fbsource/fbobjc/ios-sdk.
    
    Changes to this repo effectively don't matter until we move everything over later.
    
    NOTE: The hackbook build IPA job is known broken. Not a big deal right now. It needed to break in order to get the CI version working. I'll fix it later before we actually move anything.
    
    Reviewers: danielhuri, ldelgado, jialichen, samodom, #sdk
    
    Reviewed By: samodom
    
    Differential Revision: https://phabricator.intern.facebook.com/D37099126
    
    Tags: publish_when_ready

---
+++ internal/testing/CoffeeShop/inject_codesign_metadata.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+set -e
+
+if [ $# -lt 1 ]
+then
+  SCRIPT_BASENAME=$(basename "$0")
+  printf "Usage: ./%s <App bundle path>\n\n" "$SCRIPT_BASENAME" 1>&2
+  exit 1
+fi
+
+SCRIPT_DIRNAME=$(dirname "$0")
+APP_BUNDLE_PATH="$1"
+
+# Root
+cp "$SCRIPT_DIRNAME"/BUCK_code_sign_args.plist "$APP_BUNDLE_PATH"/BUCK_code_sign_args.plist
+cp "$SCRIPT_DIRNAME"/BUCK_code_sign_entitlements.plist "$APP_BUNDLE_PATH"/BUCK_code_sign_entitlements.plist
+
+# Frameworks with default args
+for fwkname in \
+    "E2EUtils.framework" ; do \
+  cp "$SCRIPT_DIRNAME"/BUCK_code_sign_args.plist "$APP_BUNDLE_PATH"/Frameworks/"$fwkname"/BUCK_code_sign_args.plist;
+  cp "$SCRIPT_DIRNAME"/BUCK_code_sign_entitlements.plist "$APP_BUNDLE_PATH"/Frameworks/"$fwkname"/BUCK_code_sign_entitlements.plist;
+done;
