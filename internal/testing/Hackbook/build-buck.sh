de67a3a68953706d7b4eaa3bc487bff5356430fd
Joe Susnick <joesusnick@fb.com>
2022-06-23 12:27:15 -0700

    Delete BUCK files from internal directory
    Summary: $title
    
    Test Plan: N/A
    
    Reviewers: samodom, #sdk
    
    Reviewed By: samodom
    
    Differential Revision: https://phabricator.intern.facebook.com/D37380039
    
    Tags: publish_when_ready

--- internal/testing/Hackbook/build-buck.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
-
-REPO_ROOT=$(dirname "$0")/../../../..
-exec "$REPO_ROOT/Tools/build-buck.py" \
-  --buck-app-target //fbobjc/ios-sdk/internal/testing/Hackbook:Hackbook \
-  --buck-msdk-dependency-target //fbobjc/msdk:jackalope \
-  "$@"
