f74efc995b13b6a7d9162a9e146763fa00d47687
Sam Odom <samodom@fb.com>
2022-06-24 11:05:50 -0700

    Fix license text in recently-added files
    Test Plan: sandcastle
    
    Reviewers: josuebrizuelaxp, #sdk
    
    Reviewed By: josuebrizuelaxp
    
    Differential Revision: https://phabricator.intern.facebook.com/D37398481
    
    Tags: opensource-needs-export, accept2ship

--- FBSDKCoreKit_Basics/FBSDKCoreKit_Basics/NSURLSession+FBSDKSessionProviding.m
+++ FBSDKCoreKit_Basics/FBSDKCoreKit_Basics/NSURLSession+FBSDKSessionProviding.m
@@ -1,10 +1,10 @@
-//
-//  NSURLSession+NSURLSession_FBSDKSessionProviding.h
-//  FBSDKCoreKit_Basics
-//
-//  Created by Sam Odom on 6/21/22.
-//  Copyright © 2022 Facebook. All rights reserved.
-//
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
 #import <FBSDKCoreKit_Basics/FBSDKURLSessionProviding.h>
 
