5b56c41fc3b87f42f6d83ecc9da35720065edea8
Xin Hu <xinh@fb.com>
2022-07-26 18:44:21 -0700

    [Meta SDK] Implement KeychainStore to save userSession data
    Summary:
    The keychain is an encrypted container which will be used to store UserSession  (including user and authentication data).
    
    The ```KeychainStore``` is a wrapper to manage official keychain APIs. It will be injected in ```LocalStorage```  as a dependency to store/access UserSession in keychain (The related diff is on the way).
    
    Test Plan: The test will be covered in the LocalStorage diff
    
    Reviewers: joesusnick, jwujoyce, pansy0419, #horizon_identity
    
    Reviewed By: joesusnick
    
    Differential Revision: https://phabricator.intern.facebook.com/D37986505
    
    Tasks: T126405726

---
+++ internal/MetaSDK/Sources/MetaLogin/KeychainPersisting.swift
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import Foundation
+
+protocol KeychainPersisting {
+    func save(data: Data) -> OSStatus
+    func read() -> KeychainResult
+    func delete() -> OSStatus
+}
---
+++ internal/MetaSDK/Tests/MetaLoginTests/KeychainStorageTests.swift
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import XCTest
+@testable import MetaLogin
+
+final class KeychainStorageTests: XCTestCase {
+    var bundle: TestBundle!
+    var keychainStorage: KeychainStorage!
+
+    override func setUp() {
+        super.setUp()
+
+        bundle = TestBundle()
+        keychainStorage = KeychainStorage(bundle: bundle)
+    }
+
+    override func tearDown() {
+        bundle = nil
+        keychainStorage = nil
+
+        super.tearDown()
+    }
+
+    func testKeychainAccount() throws {
+        bundle.stubbedBundleIdentifier = "test"
+        XCTAssertEqual(
+            bundle.stubbedBundleIdentifier,
+            keychainStorage.keychainAccount,
+            "The keychain account should be set the the bundle identifier"
+        )
+
+        bundle.stubbedBundleIdentifier = nil
+        XCTAssertEqual(
+            "unknown",
+            keychainStorage.keychainAccount,
+            "The keychain account should have a default value of 'unknown' when the bundle identifier is nil"
+        )
+    }
+}
