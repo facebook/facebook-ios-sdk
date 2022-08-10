b268bc178e8ff32fcaf30534262f9fe7e9f354fc
Sam Odom <samodom@fb.com>
2022-08-10 09:40:08 -0700

    [Meta SDK] Introduce formal Permission type
    Summary:
    Facebook:
    We can slowly add permissions here as necessary instead of enumerating them all out of the box.  We'll leave out the ability to create a custom permission until we decide it's necessary.
    
    Test Plan: sandcastle
    
    Reviewers: pansy0419, #sdk, #horizon_identity
    
    Reviewed By: pansy0419
    
    Subscribers: joesusnick, pansy0419
    
    Differential Revision: https://phabricator.intern.facebook.com/D38517953
    
    Tags: swift-adoption

---
+++ internal/MetaSDK/Sources/MetaLogin/Permission.swift
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import Foundation
+
+/**
+ A value representing a Graph API permission.
+
+ Each permission has its own set of requirements and suggested use cases. Permission strings must contain only lowercase
+ letters and underscores. Only valid permissions supported by Graph API will be used for authorization.
+ */
+public struct Permission: RawRepresentable, Hashable, Codable {
+    public typealias RawValue = String
+
+    /// The permission's string value.
+    public let rawValue: String
+
+    /// Creates a new instance with the given raw value.
+    public init?(rawValue: String) {
+        guard
+            !rawValue.isEmpty,
+            rawValue.rangeOfCharacter(from: Self.invalidCharacters) == nil
+        else { return nil }
+
+        self.rawValue = rawValue
+    }
+
+    private static var invalidCharacters: CharacterSet {
+        CharacterSet(charactersIn: "\u{0061}" ... "\u{007A}")
+            .union(CharacterSet(charactersIn: "_"))
+            .inverted
+    }
+
+    // swiftlint:disable force_unwrapping
+
+    /// Provides access to a subset of items that are part of a user's public profile.
+    public static let publicProfile = Self(rawValue: "public_profile")!
+
+    /// Provides access to a user's avatar.
+    public static let userAvatar = Self(rawValue: "user_avatar")!
+
+    // swiftlint:enable force_unwrapping
+}
