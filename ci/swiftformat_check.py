472283a6aee6fc8641913e7ab3a21b41fa9827a5
Joe Susnick <joesusnick@fb.com>
2022-08-10 15:18:42 -0700

    Adding CI Job for formatting
    Summary: This is a little aggressive and just formats everything.
    
    Test Plan:
    Plan to rebase on top of diff with formatting fixes once this fails.
    
    (It did fail in v1 of the diff)
    
    Reviewers: samodom, #sdk
    
    Reviewed By: samodom
    
    Differential Revision: https://phabricator.intern.facebook.com/D38598533
    
    Tags: swift-adoption, accept2ship, publish_when_ready

---
+++ ci/swiftformat_check.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+import json
+from common import *
+
+ONCALL = 'platform_sdks'
+
+
+my_job = [
+    {
+        "alias": "facebook-ios-sdk-swiftformat-check",
+        "capabilities": ios_sdk_capabilities(),
+        "command": "SandcastleUniversalCommand",
+        "description": "Runs swiftformat on the Facebook iOS SDK",
+        "oncall": ONCALL,
+        "priority": 0,
+        "tags": ["facebook-ios-sdk"],
+        "args": {
+            "oncall": ONCALL,
+            "steps": [
+                {
+                    "name": "Format SDK",
+                    "shell": """
+                        internal/tools/swiftformat .
+
+                        if [ -z "$(git status --porcelain)" ]; then
+                            exit 0;
+                        else
+                            echo "Please run swiftformat and resubmit these changes";
+                        exit 1;
+                        fi
+                        """
+                },
+            ],
+        },
+    }
+]
+
+print(json.dumps(my_job, indent=4))
