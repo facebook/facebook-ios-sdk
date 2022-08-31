8f548fc57787cc741f0752d9fa4bcb3eaef638f2
Sam Odom <samodom@fb.com>
2022-08-31 13:59:26 -0700

    [iOS SDK] Introduce CoreUIApplication to avoid missing UIApplication.shared in tests
    Summary: Since UIApplication.shared doesn't exist at runtime in tests, performing an identity check against it crashes
    
    Test Plan: sandcastle
    
    Reviewers: josuebrizuelaxp, #sdk
    
    Reviewed By: josuebrizuelaxp
    
    Differential Revision: https://phabricator.intern.facebook.com/D39149112
    
    Tags: opensource-needs-export, swift-adoption

---
+++ FBSDKCoreKit/FBSDKCoreKit/Internal/CoreUIApplication.swift
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#if !os(tvOS)
+
+import UIKit
+
+/**
+ Internal type exposed to facilitate transition to Swift.
+ API Subject to change or removal without warning. Do not use.
+
+ @warning INTERNAL - DO NOT USE
+ */
+@objcMembers
+@available(tvOS, unavailable)
+@objc public final class CoreUIApplication: NSObject {
+  public static let shared: _InternalURLOpener = {
+    #if DEBUG
+    TestUIApplication()
+    #else
+    UIApplication.shared
+    #endif
+  }()
+}
+
+#if DEBUG
+private final class TestUIApplication: _InternalURLOpener {
+  func canOpen(_ url: URL) -> Bool { false }
+
+  func open(_ url: URL) -> Bool { false }
+
+  func open(
+    _ url: URL,
+    options: [UIApplication.OpenExternalURLOptionsKey: Any] = [:],
+    completionHandler completion: ((Bool) -> Void)? = nil
+  ) {}
+}
+#endif
+
+#endif
