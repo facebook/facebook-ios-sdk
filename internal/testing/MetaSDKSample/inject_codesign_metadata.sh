007d5797e144d12b6181742c4d68f6f0ce07a2d0
Xin Hu <xinh@fb.com>
2022-09-12 16:17:31 -0700

    [Meta SDK] Modify Inject script to fix sandcastle build failure
    Summary:
    Remove framework part(MetaSDKSample app doesn't have dependency on them) in Inject script to fix sandcastle build failure
    Sandcastle build job: https://fburl.com/sandcastle/l4krfau9
    
    Test Plan: unit test
    
    Reviewers: joesusnick, pansy0419, samodom, #horizon_identity
    
    Reviewed By: joesusnick
    
    Differential Revision: https://phabricator.intern.facebook.com/D39439115
    
    Tasks: T130270115

--- internal/testing/MetaSDKSample/inject_codesign_metadata.sh
+++ internal/testing/MetaSDKSample/inject_codesign_metadata.sh
@@ -16,11 +16,3 @@ APP_BUNDLE_PATH="$1"
 # Root
 cp "$SCRIPT_DIRNAME"/BUCK_code_sign_args.plist "$APP_BUNDLE_PATH"/BUCK_code_sign_args.plist
 cp "$SCRIPT_DIRNAME"/BUCK_code_sign_entitlements.plist "$APP_BUNDLE_PATH"/BUCK_code_sign_entitlements.plist
-
-# Frameworks with default args
-for fwkname in \
-    "E2EUtils.framework" \
-    "OHHTTPStubs.framework" ; do \
-  cp "$SCRIPT_DIRNAME"/BUCK_code_sign_args.plist "$APP_BUNDLE_PATH"/Frameworks/"$fwkname"/BUCK_code_sign_args.plist;
-  cp "$SCRIPT_DIRNAME"/BUCK_code_sign_entitlements.plist "$APP_BUNDLE_PATH"/Frameworks/"$fwkname"/BUCK_code_sign_entitlements.plist;
-done;
