d0559e34ce63d03b1cdb4c67ef93c1a83f6f687e
Sam Odom <samodom@fb.com>
2022-09-19 11:23:10 -0700

    Bump version to 15.0.0
    Summary:
    Facbook:
    Discarded the automatic changes to the podspecs (except TVOSKit) since they're generated.
    
    Reviewed By: mingcaozhang
    
    Differential Revision: D39626289
    
    fbshipit-source-id: ba1b46889e9e835d0e70c06c0bcce173e0eed36a

--- FBAEMKit/FBAEMKit/_AEMNetworker.swift
+++ FBAEMKit/FBAEMKit/_AEMNetworker.swift
@@ -29,11 +29,11 @@ public final class _AEMNetworker: NSObject, AEMNetworking, URLSessionDataDelegat
 
   private enum Values {
     static let newline = "\r\n"
-    static let versionString = "14.1.0"
-    static let defaultGraphAPIVersion = "v14.0"
+    static let versionString = "15.0.0"
+    static let defaultGraphAPIVersion = "v15.0"
     static let SDK = "ios"
     static let userAgentBase = "FBiOSAEM"
-    static let graphAPIEndpoint = "https://graph.facebook.com/v14.0/"
+    static let graphAPIEndpoint = "https://graph.facebook.com/v15.0/"
     static let graphAPIContentType = "application/json"
     static let errorDomain = "com.facebook.aemkit"
     static let agent = "\(Values.userAgentBase).\(Values.versionString)"
