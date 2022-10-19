6c04542126ba080735b4a046f5f2517a88f2b0fd
Jawwad Ahmad <jawwad@fb.com>
2022-10-19 13:09:08 -0700

    Update generate-projects.sh to handle spaces in paths
    Reviewed By: samodom
    
    Differential Revision: D40518803
    
    fbshipit-source-id: f2a0d4c90c46f30556dad712bbbacb2ebfea036e

--- generate-projects.sh
+++ generate-projects.sh
@@ -31,7 +31,7 @@ elif ! command -v xcodegen >/dev/null; then
     exit 1
 fi
 
-CURRENT_XCODEGEN_VERSION=$($XCODEGEN_BINARY --version)
+CURRENT_XCODEGEN_VERSION=$("$XCODEGEN_BINARY" --version)
 CURRENT_XCODEGEN_VERSION=${CURRENT_XCODEGEN_VERSION#"Version: "} # Strip "Version :" prefix
 if [ "$CURRENT_XCODEGEN_VERSION" != "$EXPECTED_XCODEGEN_VERSION" ]; then
     echo "${YELLOW}WARNING: Expected xcodegen version is $EXPECTED_XCODEGEN_VERSION. You have $CURRENT_XCODEGEN_VERSION.${RESET}"
@@ -49,9 +49,9 @@ for KIT_DIR in FBSDKCoreKit_Basics FBAEMKit FBSDKCoreKit TestTools FBSDKLoginKit
     # Set the env var XCODEGEN_USE_CACHE to anything to use the --use-cache flag
     if [ -n "$XCODEGEN_USE_CACHE" ]; then
         # Use rm -rf ~/.xcodegen/cache if you need to reset the cache
-        $XCODEGEN_BINARY generate --use-cache
+        "$XCODEGEN_BINARY" generate --use-cache
     else
-        $XCODEGEN_BINARY generate
+        "$XCODEGEN_BINARY" generate
     fi
     cd ..
 done
