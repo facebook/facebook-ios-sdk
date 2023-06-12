a8dbbd450e48858c2bdedc01ffa2892ac4ae3dd2
Xin Wu <xinwu330@meta.com>
2023-06-12 10:33:56 -0700

    Add new fields in config graph API
    Summary:
    Schema design: https://docs.google.com/document/d/1w39R4VCAodg3ciM9kkGzSpPC_Bs6xkI2-DQZMnfB2JQ/edit#heading=h.a90wcj6w8gsd
    
    Here is the new v4 schema looks like:
    ```
    {
      "data": [
        {
          "lock_window_rules": [
            {
              "lock_window_type": "time",
              "time": 36,
              "postback_sequence_index": 1
            },
            {
              "lock_window_type": "event",
              "events": [
                {
                  "event_name": "fb_mobile_purchase",
                  "values": [
                    {
                      "currency": "USD",
                      "amount": 10.1
                    }
                  ]
                }
              ]
              "postback_sequence_index": 2
            }
          ],
          "coarse_cv_configs": [
            {
              "postback_sequence_index": 1,
              "coarse_cv_rules": [
                {
                  "coarse_cv_value": "high",
                  "events": [
                    {
                      "event_name": "fb_mobile_purchase",
                      "values": [
                        {
                          "currency": "USD",
                          "amount": 10.1
                        }
                      ]
                    }
                  ]
                }
              ],
              "update_time": 1602399600
            }
          ],
          "is_coarse_cv_accumulative": false
        }
      ]
    }
    ```
    
    Reviewed By: KylinChang
    
    Differential Revision: D46347246
    
    fbshipit-source-id: 208531535673806479cf40056898c609ad3e11bb

---
+++ FBSDKCoreKit/FBSDKCoreKit/AppEvents/Internal/SKAdNetwork/FBSDKSKAdnetworkUtils.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#if !TARGET_OS_TV
+
+#import <FBSDKCoreKit/FBSDKCoreKit-Swift.h>
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+NS_SWIFT_NAME(SKAdNetworkUtils)
+@interface FBSDKSKAdnetworkUtils : NSObject
+
+- (instancetype)init NS_UNAVAILABLE;
++ (instancetype)new NS_UNAVAILABLE;
+
++ (nullable NSArray<FBSDKSKAdNetworkEvent *> *)parseEvents:(NSArray<NSDictionary<NSString *, id> *> *)events;
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#endif
