ab31d733a05b7cfb0b55dd3ddbdcf4632745fa10
Zilin Zhang <kylinchang@meta.com>
2023-06-21 15:30:54 -0700

    [MACA]Add CCRuleEngine StringComparison & getKey
    Summary:
    Facebook:
    
    We're implementing CCRuleEngine in SDKs and translate https://fburl.com/code/h9ylyd0a into swift. This diff adds the getKey and StringComparison functions
    
    Test Plan: Unit Test
    
    Reviewers: thw, amleshjk, jajiang, #ads_sdk
    
    Reviewed By: amleshjk, jajiang
    
    Subscribers: shawnzeng
    
    Differential Revision: https://phabricator.intern.facebook.com/D46789789
    
    Tags: opensource-needs-export

--- FBSDKCoreKit/FBSDKCoreKit/AppEvents/Internal/Integrity/MACARuleMatchingManager.swift
+++ FBSDKCoreKit/FBSDKCoreKit/AppEvents/Internal/Integrity/MACARuleMatchingManager.swift
@@ -14,4 +14,77 @@ final class MACARuleMatchingManager: MACARuleMatching {
   func enable() {
     isEnable = true
   }
+
+  func getKey(logic: [String: Any]) -> String? {
+    logic.keys.first
+  }
+
+  // swiftlint:disable:next cyclomatic_complexity
+  func stringComparison(
+    variable: String,
+    values: [String: String],
+    data: [String: Any]
+  ) -> Bool {
+    // swiftlint:disable:next identifier_name
+    guard let op = getKey(logic: values),
+          let ruleValue = values[op],
+          let dataValue = data[variable.lowercased()] ?? data[variable]
+    else { return false }
+
+    switch op {
+    case "contains":
+      // swiftlint:disable:next blank_line_after_single_line_guard
+      guard let dataValue = stringValueOf(dataValue) else { return false }
+      return dataValue.contains(ruleValue)
+    case "i_contains":
+      // swiftlint:disable:next blank_line_after_single_line_guard
+      guard let dataValue = stringValueOf(dataValue) else { return false }
+      return dataValue.lowercased().contains(ruleValue.lowercased())
+    case "i_not_contains":
+      // swiftlint:disable:next blank_line_after_single_line_guard
+      guard let dataValue = stringValueOf(dataValue) else { return false }
+      return !dataValue.lowercased().contains(ruleValue.lowercased())
+    case "regex_match":
+      // swiftlint:disable:next blank_line_after_single_line_guard
+      guard let dataValue = stringValueOf(dataValue) else { return false }
+      return dataValue.range(of: ruleValue, options: .regularExpression, range: nil, locale: nil) != nil
+    case "eq":
+      // swiftlint:disable:next blank_line_after_single_line_guard
+      guard let dataValue = stringValueOf(dataValue) else { return false }
+      return dataValue == ruleValue
+    case "neq":
+      // swiftlint:disable:next blank_line_after_single_line_guard
+      guard let dataValue = stringValueOf(dataValue) else { return false }
+      return dataValue != ruleValue
+    case "lt":
+      return doubleValueOf(dataValue) < doubleValueOf(ruleValue)
+    case "lte":
+      return doubleValueOf(dataValue) <= doubleValueOf(ruleValue)
+    case "gt":
+      return doubleValueOf(dataValue) > doubleValueOf(ruleValue)
+    case "gte":
+      return doubleValueOf(dataValue) >= doubleValueOf(ruleValue)
+    default:
+      return false
+    }
+  }
+
+  func stringValueOf(_ value: Any?) -> String? {
+    if let str = value as? String {
+      return str
+    } else if let num = value as? NSNumber {
+      return num.stringValue
+    }
+    return nil
+  }
+
+  func doubleValueOf(_ value: Any?) -> Double {
+    if let num = value as? Double {
+      return num
+    } else if let str = value as? String,
+              let doubleValue = Double(str) {
+      return doubleValue
+    }
+    return 0
+  }
 }
