3015fe74a9df1dbf3644a08549cbc8f1cdb53483
Joseph Neves <Josephantonio193@gmail.com>
2023-08-21 18:20:49 -0300

    alteração do ingles para o Portgues

--- testing/TestXcodeIntegration/TestXcodeIntegration/SceneDelegate.swift
+++ testing/TestXcodeIntegration/TestXcodeIntegration/SceneDelegate.swift
@@ -13,7 +13,7 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // use esse method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
