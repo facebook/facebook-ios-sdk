73fdffc085bf9807bafefecd0cd1725b7c992344
Jacky Jiang <jajiang@meta.com>
2023-10-13 09:39:10 -0700

    [iOSSDK] Bump v16.2.1
    Summary: $title
    
    Test Plan: N/A
    
    Reviewers: xinwu330, #ads_sdk
    
    Reviewed By: xinwu330
    
    Differential Revision: https://phabricator.intern.facebook.com/D50246456
    
    Tags: opensource-needs-export

--- FBSDKCoreKit/FBSDKCoreKitTests/AppLinkNavigationTests.swift
+++ FBSDKCoreKit/FBSDKCoreKitTests/AppLinkNavigationTests.swift
@@ -44,7 +44,7 @@ final class AppLinkNavigationTests: XCTestCase {
     eventPoster = TestMeasurementEvent()
     resolver = TestAppLinkResolver()
     settings = TestSettings()
-    settings.sdkVersion = "16.2.0"
+    settings.sdkVersion = "16.2.1"
     urlOpener = URLOpener(canOpenURL: true)
     AppLinkNavigation.defaultResolver = resolver
 
