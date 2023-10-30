1a163f506af643fdb2ef87215c4bafbeef4759f2
Tim Oliver <timoliver@meta.com>
2023-10-29 19:10:35 -0700

    [facebook-ios-sdk][PR] Add missing dependencies
    Summary:
    Thanks for proposing a pull request!
    
    To help us review the request, please complete the following:
    
    - [x] sign [contributor license agreement](https://code.facebook.com/cla)
    - [x] I've ensured that all existing tests pass and added tests (when/where necessary)
    - [x] I've updated the documentation (when/where necessary) and [Changelog](CHANGELOG.md) (when/where necessary)
    - [ ] I've added the proper label to this pull request (e.g. `bug` for bug fixes)
    
    ## Pull Request Details
    
    I found the required dependencies are missing in the Package.swift.
    
    These are the related targets.
    
    - FacebookAEM( `.aem`)
    - FBAEMKit (`.Prefixed.aem`)
    - FBSDKCoreKit_Basics (`.Prefixed.basics`)
    
    FacebookAEM requires FBAEM. FacebookAEM just exports FBAEM interfaces.
    https://github.com/facebook/facebook-ios-sdk/blob/41044df838ef3fefe600fb96b6560dc8a2b2a18a/Sources/FacebookAEM/Exports.swift#L10
    
    `FBAEMKit` imports `FBSDKCoreKit_Basics`. In fact, `.swiftinterface` of distributed `FBAEMKit.xcframework` attempts to import `FBSDKCoreKit_Basics`.
    
    So it need to add dependencies for `FBSDKCoreKit_Basics`. But there are no dependencies.
    It causes error on some build environment.
    
    `FBAEMKit` is a binary target, so it can't have dependencies directly. So I added the dependencies to `FBSDKCoreKit_Basics` from `FacebookAEM`.
    
    Pull Request resolved: https://github.com/facebook/facebook-ios-sdk/pull/2254
    GitHub Author: Kohki Miki <giginet.net@gmail.com>
    
    Test Plan:
    This manifest works.
    
    Reviewers: joesusnick, wbailey, #sdk
    
    Reviewed By: joesusnick
    
    Differential Revision: https://phabricator.intern.facebook.com/D49624751
    
    Tasks: T162399334
    
    Tags: accept2ship

--- Package.swift
+++ Package.swift
@@ -137,7 +137,7 @@ extension Target {
         "https://github.com/facebook/facebook-ios-sdk/releases/download/v16.2.1/\(targetName)-Static_XCFramework.zip"
     }
 
-    static let aem = target(name: .aem, dependencies: [.Prefixed.aem])
+    static let aem = target(name: .aem, dependencies: [.Prefixed.basics, .Prefixed.aem])
 
     static let core = target(
         name: .core,
