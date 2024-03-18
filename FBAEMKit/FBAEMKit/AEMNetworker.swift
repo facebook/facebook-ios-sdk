98c573cc4e997fdc7c58113f84df56eed3a2dfd3
Jacky Jiang <jajiang@meta.com>
2024-03-18 12:37:37 -0700

    Bump Graph API version to v17.0
    Summary: Bump Graph API version to v17.0
    
    Reviewed By: ryantobinmeta
    
    Differential Revision: D55030141
    
    fbshipit-source-id: 4c5d1242a407f3156781c2187494012a711d1226

--- FBAEMKit/FBAEMKit/AEMNetworker.swift
+++ FBAEMKit/FBAEMKit/AEMNetworker.swift
@@ -20,10 +20,10 @@ final class AEMNetworker: NSObject, AEMNetworking, URLSessionDataDelegate {
   private enum Values {
     static let newline = "\r\n"
     static let versionString = "17.0.0"
-    static let defaultGraphAPIVersion = "v16.0"
+    static let defaultGraphAPIVersion = "v17.0"
     static let SDK = "ios"
     static let userAgentBase = "FBiOSAEM"
-    static let graphAPIEndpoint = "https://graph.facebook.com/v16.0/"
+    static let graphAPIEndpoint = "https://graph.facebook.com/v17.0/"
     static let graphAPIContentType = "application/json"
     static let errorDomain = "com.facebook.aemkit"
     static let agent = "\(Values.userAgentBase).\(Values.versionString)"
