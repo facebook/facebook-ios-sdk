c685294fb75a3f9cd2e8adebb17d56ab47410d36
Jacky Jiang <jajiang@meta.com>
2024-05-14 11:33:54 -0700

    Implement prevention test to ensure no new tracking domains added
    Summary: Implement prevention test to ensure no new tracking domains added to LoginKit.
    
    Reviewed By: bartoszniemczura
    
    Differential Revision: D57242844
    
    fbshipit-source-id: 09376465bc64b9bfeadfe549fec09b15be16b0d9

---
+++ FBSDKLoginKit/FBSDKLoginKitTests/PrivacyManifestTests.swift
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+@testable import FBSDKLoginKit
+
+import XCTest
+
+final class PrivacyManifestTests: XCTestCase {
+  func testTrackingDomains() {
+    let bundle = Bundle(for: LoginConfiguration.self)
+    let manifestUrl = bundle.url(forResource: "PrivacyInfo", withExtension: "xcprivacy")
+    guard let manifestUrl else {
+      return XCTFail("Could not find Privacy Manifest file")
+    }
+    let manifest = NSDictionary(contentsOf: manifestUrl)
+    guard let trackingDomains = manifest?["NSPrivacyTrackingDomains"] as? NSArray else {
+      return XCTFail("Could not find tracking domains")
+    }
+    XCTAssertTrue(trackingDomains.count == 1)
+    XCTAssertTrue(trackingDomains.contains("ep1.facebook.com"))
+  }
+}
