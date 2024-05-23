8d3dbb81d57e375ece1ad9df93c3f67513d38ebc
Jacky Jiang <jajiang@meta.com>
2024-05-23 11:50:50 -0700

    Implement RRAPI prevention test
    Summary: Implement RRAPI prevention test for AEMKit.
    
    Reviewed By: ryantobinmeta
    
    Differential Revision: D57597445
    
    fbshipit-source-id: f5180cd3214efc4a13d00488b62f4e06985b5014

--- FBAEMKit/FBAEMKitTests/PrivacyManifestTests.swift
+++ FBAEMKit/FBAEMKitTests/PrivacyManifestTests.swift
@@ -11,9 +11,20 @@
 import XCTest
 
 final class PrivacyManifestTests: XCTestCase {
-  func testTrackingDomains() {
+  var manifestUrl: URL?
+
+  override func setUp() {
+    super.setUp()
     let bundle = Bundle(for: AEMConfiguration.self)
-    let manifestUrl = bundle.url(forResource: "PrivacyInfo", withExtension: "xcprivacy")
+    manifestUrl = bundle.url(forResource: "PrivacyInfo", withExtension: "xcprivacy")
+  }
+
+  override func tearDown() {
+    manifestUrl = nil
+    super.tearDown()
+  }
+
+  func testTrackingDomains() {
     guard let manifestUrl else {
       return XCTFail("Could not find Privacy Manifest file")
     }
@@ -21,5 +32,36 @@ final class PrivacyManifestTests: XCTestCase {
     if manifest?["NSPrivacyTrackingDomains"] is NSArray {
       XCTFail("Should not contain tracking domains")
     }
+  }
+
+  func testRequiredReasonAPIs() {
+    guard let manifestUrl else {
+      return XCTFail("Could not find Privacy Manifest file")
+    }
+    let manifest = NSDictionary(contentsOf: manifestUrl)
+    guard let rrAPIs = manifest?["NSPrivacyAccessedAPITypes"] as? NSArray else {
+      return XCTFail("Could not find Privacy Accessed API Types")
+    }
+    XCTAssertTrue(rrAPIs.count == 1, "Should only expect to have one API in the Privacy Manifest file")
+    guard let rrAPIDict = rrAPIs[0] as? NSDictionary else {
+      return XCTFail("Could not find items in Privacy Accessed API Types")
+    }
+
+    XCTAssertEqual(
+      rrAPIDict["NSPrivacyAccessedAPIType"] as? String,
+      "NSPrivacyAccessedAPICategoryUserDefaults",
+      "Should match UserDefaults category"
+    )
+    guard let reasons = rrAPIDict["NSPrivacyAccessedAPITypeReasons"] as? NSArray else {
+      return XCTFail("Could not find Privacy Accessed API Reasons")
+    }
+    XCTAssertTrue(
+      reasons.count == 1,
+      """
+      Should only expect to have one reason for UserDefaults
+      in the Privacy Manifest file
+      """
+    )
+    XCTAssertEqual(reasons[0] as? String, "CA92.1", "Reason should match CA92.1")
   }
 }
