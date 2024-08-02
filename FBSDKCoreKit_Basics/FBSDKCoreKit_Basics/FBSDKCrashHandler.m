63bacf16f29cba6e914388f7eab04d73db36d5f8
Zilin Zhang <kylinchang@meta.com>
2024-08-02 13:12:41 -0700

    Bump iOS SDK version v17.0.3
    Summary: As title
    
    Reviewed By: xta0
    
    Differential Revision: D60686948
    
    fbshipit-source-id: 9b72c28d3e9f788f9ece59f4eec8d04aff1e89a0

--- FBSDKCoreKit_Basics/FBSDKCoreKit_Basics/FBSDKCrashHandler.m
+++ FBSDKCoreKit_Basics/FBSDKCoreKit_Basics/FBSDKCrashHandler.m
@@ -22,7 +22,7 @@
 #define FBSDK_MAX_CRASH_LOGS 5
 #define FBSDK_CRASH_PATH_NAME @"instrument"
 #ifndef FBSDK_VERSION_STRING
- #define FBSDK_VERSION_STRING @"17.0.2"
+ #define FBSDK_VERSION_STRING @"17.0.3"
 #endif
 
 static NSUncaughtExceptionHandler *previousExceptionHandler = NULL;
