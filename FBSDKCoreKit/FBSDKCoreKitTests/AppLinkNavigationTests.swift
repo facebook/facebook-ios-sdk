7a72ccde2df2341f72d561c6e16b0872908a2d37
Yuriy Tol <yuriyt@meta.com>
2024-08-27 12:17:01 -0700

    Bump iOS SDK version v17.1.0
    Summary: Bump iOS SDK version v17.1.0 for ShareDialog bug fix
    
    Reviewed By: caodoan
    
    Differential Revision: D61861488
    
    fbshipit-source-id: 92bc074015c0cf8d8b8b5f5952839d23f0db0472

--- FBSDKCoreKit/FBSDKCoreKitTests/AppLinkNavigationTests.swift
+++ FBSDKCoreKit/FBSDKCoreKitTests/AppLinkNavigationTests.swift
@@ -44,7 +44,7 @@ final class AppLinkNavigationTests: XCTestCase {
     eventPoster = TestMeasurementEvent()
     resolver = TestAppLinkResolver()
     settings = TestSettings()
-    settings.sdkVersion = "17.0.3"
+    settings.sdkVersion = "17.1.0"
     urlOpener = URLOpener(canOpenURL: true)
     AppLinkNavigation.defaultResolver = resolver
 
