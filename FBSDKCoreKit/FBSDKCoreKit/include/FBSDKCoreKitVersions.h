f18f5f22b8f4766e184d7198d450854bda853e7e
Yuriy Tol <yuriyt@meta.com>
2024-08-27 12:17:01 -0700

    Bump iOS SDK version v17.1.0
    Summary: Bump iOS SDK version v17.1.0 for ShareDialog bug fix
    
    Test Plan: n/a
    
    Reviewers: adamcmiel, jajiang, caodoan, ryantobin, #swift_wilde, #mobile_sdk
    
    Reviewed By: caodoan
    
    Differential Revision: https://phabricator.intern.facebook.com/D61861488
    
    Tags: opensource-needs-export, mobile_sdk

--- FBSDKCoreKit/FBSDKCoreKit/include/FBSDKCoreKitVersions.h
+++ FBSDKCoreKit/FBSDKCoreKit/include/FBSDKCoreKitVersions.h
@@ -6,5 +6,5 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#define FBSDK_VERSION_STRING @"17.0.3"
+#define FBSDK_VERSION_STRING @"17.1.0"
 #define FBSDK_DEFAULT_GRAPH_API_VERSION @"v17.0"
