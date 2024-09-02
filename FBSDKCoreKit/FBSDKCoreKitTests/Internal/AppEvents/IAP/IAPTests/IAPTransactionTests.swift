3312e7d2e5669a643b58c2e2805c12e868fae09c
Ryan Tobin <ryantobin@meta.com>
2024-09-01 18:06:08 -0700

    Create IAP Helper Functionality
    Summary: This diff creates some helper functions / structs that we will use in the IAP implementation.
    
    Reviewed By: jjiang10
    
    Differential Revision: D61823004
    
    fbshipit-source-id: afec479e49a6d1d802117c439db342a42fda2850

---
+++ FBSDKCoreKit/FBSDKCoreKitTests/Internal/AppEvents/IAP/IAPTests/IAPTransactionTests.swift
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+@testable import FBSDKCoreKit
+@testable import IAPTestsHostApp
+
+import StoreKitTest
+import XCTest
+
+@available(iOS 14.0, *)
+final class IAPTransactionTests: StoreKitTestCase {
+
+  @available(iOS 17.0, *)
+  func testValidIAPTransaction() async throws {
+    guard let transaction =
+      try? await testSession.buyProduct(identifier: Self.ProductIdentifiers.product1.rawValue) else {
+      return
+    }
+    let iapTransaction = IAPTransaction(transaction: transaction, isVerified: true)
+    XCTAssertEqual(iapTransaction.transaction, transaction)
+    XCTAssertTrue(iapTransaction.isVerified)
+  }
+
+  @available(iOS 17.0, *)
+  func testInvalidIAPTransaction() async throws {
+    guard let transaction =
+      try? await testSession.buyProduct(identifier: Self.ProductIdentifiers.product1.rawValue) else {
+      return
+    }
+    let iapTransaction = IAPTransaction(transaction: transaction, isVerified: false)
+    XCTAssertEqual(iapTransaction.transaction, transaction)
+    XCTAssertFalse(iapTransaction.isVerified)
+  }
+
+  @available(iOS 15.0, *)
+  func testVerificationResult() async throws {
+    guard let products = try? await Product.products(for: [Self.ProductIdentifiers.product1.rawValue]) else {
+      return
+    }
+    guard let purchaseResult = try? await products.first?.purchase() else {
+      return
+    }
+    switch purchaseResult {
+    case let .success(verificationResult):
+      let iapTransaction = verificationResult.iapTransaction
+      switch verificationResult {
+      case let .verified(transaction):
+        XCTAssertTrue(iapTransaction.isVerified)
+        XCTAssertEqual(iapTransaction.transaction, transaction)
+      case let .unverified(transaction, _):
+        XCTAssertFalse(iapTransaction.isVerified)
+        XCTAssertEqual(iapTransaction.transaction, transaction)
+      }
+    default:
+      XCTFail("Purchase failed")
+    }
+  }
+}
