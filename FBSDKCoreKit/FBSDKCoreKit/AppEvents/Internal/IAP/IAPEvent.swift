7112b0283c53bbd51cb650b5a9c3eddf63cdbbd0
Ryan Tobin <ryantobin@meta.com>
2024-09-15 10:49:49 -0700

    [FBSDK iOS][IAP][6/N] Create the event resolver
    Summary:
    One of the critical components of the IAP implementation is the event resolver. After the ```IAPTransactionObserver``` observes a new transaction, it calls the transaction logger. Before the transaction logger can log a high quality event based on the transaction, it must gather all the necessary data (i.e. event name, product title, product id, etc.) about that transaction.
    
    The transaction logger will call the event resolver to resolve all of this information. It will receive this information back in the form of an ```IAPEvent``` object. After this information is resolved, the transaction logger can log an event. This diff implements the ```IAPEventResolver```.
    
    Test Plan:
    - Created unit tests in next diff
    - Ran unit tests
    - CI / CD
    
    Reviewers: jajiang, kongxinzhu, yifan0726, #ads_sdk, maxalbright
    
    Reviewed By: jajiang
    
    Subscribers: taox
    
    Differential Revision: https://phabricator.intern.facebook.com/D62085759

---
+++ FBSDKCoreKit/FBSDKCoreKit/AppEvents/Internal/IAP/IAPEvent.swift
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import Foundation
+import StoreKit
+
+struct IAPEvent: Equatable {
+  let eventName: AppEvents.Name
+  let productID: String
+  let productTitle: String
+  let productDescription: String
+  let amount: Decimal
+  let quantity: Int
+  let currency: String?
+  let transactionID: UInt64
+  let originalTransactionID: UInt64
+  let transactionDate: Date
+  let originalTransactionDate: Date
+  let isVerified: Bool
+}
---
+++ FBSDKCoreKit/FBSDKCoreKit/AppEvents/Internal/IAP/IAPEventResolver.swift
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import Foundation
+import StoreKit
+
+@available(iOS 15.0, *)
+struct IAPEventResolver {
+
+  private static let freeTrialPaymentModeString = "FREE_TRIAL"
+
+  func resolveNewEventFor(iapTransaction: IAPTransaction) async -> IAPEvent? {
+    var eventName: AppEvents.Name = .purchased
+    if isSubscription(transaction: iapTransaction.transaction) {
+      eventName = resolveNewSubscriptionEventName(transaction: iapTransaction.transaction)
+    }
+    return await resolveEventFor(iapTransaction: iapTransaction, eventName: eventName)
+  }
+
+  func resolveRestoredEventFor(iapTransaction: IAPTransaction) async -> IAPEvent? {
+    var eventName: AppEvents.Name = .purchaseRestored
+    if isSubscription(transaction: iapTransaction.transaction) {
+      eventName = .subscribeRestore
+    }
+    return await resolveEventFor(iapTransaction: iapTransaction, eventName: eventName)
+  }
+
+  private func isSubscription(transaction: Transaction) -> Bool {
+    let subscriptionCheck = transaction.productType == .autoRenewable ||
+      transaction.productType == .nonRenewable
+    return subscriptionCheck
+  }
+
+  private func resolveNewSubscriptionEventName(transaction: Transaction) -> AppEvents.Name {
+    var isFreeTrial = false
+    if #available(iOS 17.2, *) {
+      isFreeTrial = transaction.offer?.paymentMode == .freeTrial
+    } else {
+      isFreeTrial = transaction.offerPaymentModeStringRepresentation == Self.freeTrialPaymentModeString
+    }
+    return isFreeTrial ? .startTrial : .subscribe
+  }
+
+  private func getProductFor(iapTransaction: IAPTransaction) async -> Product? {
+    guard let products = try? await Product.products(for: [iapTransaction.transaction.productID]),
+          let product = products.first else {
+      return nil
+    }
+    return product
+  }
+
+  private func resolveEventFor(iapTransaction: IAPTransaction, eventName: AppEvents.Name) async -> IAPEvent? {
+    guard let product = await getProductFor(iapTransaction: iapTransaction) else {
+      return nil
+    }
+    let transaction = iapTransaction.transaction
+    var currency = transaction.currencyCode
+    if #available(iOS 16.0, *) {
+      currency = transaction.currency?.identifier
+    }
+    return IAPEvent(
+      eventName: eventName,
+      productID: transaction.productID,
+      productTitle: product.displayName,
+      productDescription: product.description,
+      amount: transaction.price ?? 0.0,
+      quantity: transaction.purchasedQuantity,
+      currency: currency,
+      transactionID: iapTransaction.transaction.id,
+      originalTransactionID: iapTransaction.transaction.originalID,
+      transactionDate: transaction.purchaseDate,
+      originalTransactionDate: transaction.originalPurchaseDate,
+      isVerified: iapTransaction.isVerified
+    )
+  }
+}
