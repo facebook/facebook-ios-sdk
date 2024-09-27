68e167c353c7782c3ae8e8ce9b52c9d6ea5d5eab
Jacky Jiang <jajiang@meta.com>
2024-09-27 15:11:25 -0700

    [FBSDK iOS] Bump SDK version to 17.3.0
    Summary: $title
    
    Test Plan: N/A
    
    Reviewers: kylinchang, #mobile_sdk
    
    Reviewed By: kylinchang
    
    Differential Revision: https://phabricator.intern.facebook.com/D63564469
    
    Tasks: T202433317
    
    Tags: opensource-needs-export

--- FBSDKCoreKit/FBSDKCoreKitTests/AppLinkNavigationTests.swift
+++ FBSDKCoreKit/FBSDKCoreKitTests/AppLinkNavigationTests.swift
@@ -44,7 +44,7 @@ final class AppLinkNavigationTests: XCTestCase {
     eventPoster = TestMeasurementEvent()
     resolver = TestAppLinkResolver()
     settings = TestSettings()
-    settings.sdkVersion = "17.2.0"
+    settings.sdkVersion = "17.3.0"
     urlOpener = URLOpener(canOpenURL: true)
     AppLinkNavigation.defaultResolver = resolver
 
