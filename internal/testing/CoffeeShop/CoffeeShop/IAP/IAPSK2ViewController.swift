8f98c08a48cc54a20c3f77fd08e0e8dfd6e5fea0
Ryan Tobin <ryantobin@meta.com>
2024-10-25 15:36:14 -0700

    [FBSDK][IAP][22/N] Add Additional Functionality for QA Testing
    Summary: In Store Kit 2, there are two different ways that we can send purchase restored events. This diff adds some additional functionality to coffee shop so that we can test both scenarios.
    
    Test Plan:
    - CI / CD
    - Tested both purchase restored scenarios per the [QA Test Plan](https://docs.google.com/document/d/11u3oauy6AQoaqyljDs3ZIyCX0mG3gmAA-M2kWsaNAQ4/)
    
    Reviewers: jajiang, #ads_sdk
    
    Reviewed By: jajiang
    
    Differential Revision: https://phabricator.intern.facebook.com/D64806664
    
    Privacy Context Container: L1285343

--- internal/testing/CoffeeShop/CoffeeShop/IAP/IAPSK2ViewController.swift
+++ internal/testing/CoffeeShop/CoffeeShop/IAP/IAPSK2ViewController.swift
@@ -1,5 +1,6 @@
 // (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
 
+import FBSDKCoreKit
 import UIKit
 
 @MainActor
@@ -20,8 +21,31 @@ class IAPSK2ViewController: IAPSKViewController {
   }
 
   @objc override func restorePurchases() {
-    Task {
-      await SK2StoreManager.shared.restorePurchases()
+    showRestoreTestCasesOptions()
+  }
+
+  func showRestoreTestCasesOptions() {
+    let alert = UIAlertController(title: "Purchase Restored Test Cases", message: nil, preferredStyle: .actionSheet)
+    alert.addAction(UIAlertAction(title: "Test Case 1", style: .default, handler: { [weak self] _ in
+      self?.executePurchaseRestoredTestCase1()
+    }))
+    alert.addAction(UIAlertAction(title: "Test Case 2", style: .default, handler: { [weak self] _ in
+      self?.executePurchaseRestoredTestCase2()
+    }))
+    alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+    alert.popoverPresentationController?.sourceView = view
+    present(alert, animated: true, completion: nil)
+  }
+
+  private func executePurchaseRestoredTestCase1() {
+    UserDefaults.standard.set(false, forKey: "com.facebook.sdk:RestoredPurchasesKey")
+  }
+
+  private func executePurchaseRestoredTestCase2() {
+    guard let oneDayAgo = Calendar.current.date(byAdding: .day, value: -1, to: Date()) else {
+      return
     }
+    UserDefaults.standard.removeObject(forKey: "com.facebook.sdk:LoggedTransactionsKey")
+    UserDefaults.standard.set(oneDayAgo, forKey: "com.facebook.sdk:NewCandidatesDateKey")
   }
 }
--- internal/testing/CoffeeShop/CoffeeShop/IAP/ProductIdentifiers.swift
+++ internal/testing/CoffeeShop/CoffeeShop/IAP/ProductIdentifiers.swift
@@ -6,6 +6,7 @@ enum ProductIdentifiers: String, CaseIterable {
   case coffee = "com.facebook.coffeeshop.consumable.coffee"
   case giftCard = "com.facebook.coffeeshop.consumable.giftcard"
   case rewardsCard = "com.facebook.coffeeshop.nonconsumable.rewardscard"
+  case coffeeMug = "com.facebook.coffeeshop.nonconsumable.coffeemug"
   case nonRenewingMembership = "com.facebook.coffeeshop.nonrenewable.membership"
   case basicMonthlyMembership = "com.facebook.coffeeshop.autorenewing.basicmembership"
   case premiumMonthlyMembership = "com.facebook.coffeeshop.autorenewing.premiummembership"
