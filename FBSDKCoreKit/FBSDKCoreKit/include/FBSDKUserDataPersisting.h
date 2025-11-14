48c1d73a52703d3ef2cf8cdaab264e0e5f5ed051
Max Albright <maxalbright@meta.com>
2025-11-14 08:57:15 -0800

    [FB SDK][Bug Fix] Stop Clearing External ID Each Time the Advertiser Sets User Data
    Summary:
    # External Summary
    
    The external id is a field that is set by advertisers to uniquely identify a user.
    
    This is a bug wherein the API we surface to advertisers to set all fields at once does calls the UserDataStore with a ```nil``` externalID. The way the logic is written in the data store means that this will actually clear the persisted external id.
    
    Instead of doing that, we should set the external id value to be equivalent to the existing value. If the advertiser wants it to be cleared, they can call the clearUserData API.
    
    Test Plan:
    # Internal Summary
    # Context
    
    The external id is a field that is set by advertisers to uniquely identify a user. On the backend, once there is a strong identity match for an event with a given external id, we can use the field therafter to perform the same match, even in the absence of other stronger match keys.
    
    This is a bug that also existed in the Ads SDK wherein the API we surface to advertisers to set all fields at once does calls the UserDataStore with a ```nil``` externalID. The way the logic is written in the data store means that this will actually clear the persisted external id.
    
    Instead of doing that, we should set the external id value to be equivalent to the existing value. If the advertiser wants it to be cleared, they can call the clearUserData API or the
    ```
    - (void)setUserData:(nullable NSString *)data
                forType:(EnigmaUserDataType)type
    ```
    
    API with a nil value.
    
    # This Diff
    
    In D86738603, we fixed this for the ads sdk. In this diff, we introduce the fix for the FBSDK.
    
    {F1983473951}
    
    Rollback Plan:
    
    Reviewers: ryantobin, zhiyi, kylinchang
    
    Reviewed By: ryantobin, zhiyi
    
    Differential Revision: https://phabricator.intern.facebook.com/D86901688
    
    Tags: opensource-needs-export

--- FBSDKCoreKit/FBSDKCoreKit/include/FBSDKUserDataPersisting.h
+++ FBSDKCoreKit/FBSDKCoreKit/include/FBSDKUserDataPersisting.h
@@ -31,6 +31,18 @@ NS_SWIFT_NAME(_UserDataPersisting)
                state:(nullable NSString *)state
                  zip:(nullable NSString *)zip
              country:(nullable NSString *)country
+NS_SWIFT_NAME(setUser(email:firstName:lastName:phone:dateOfBirth:gender:city:state:zip:country:));
+
+- (void)setUserEmail:(nullable NSString *)email
+           firstName:(nullable NSString *)firstName
+            lastName:(nullable NSString *)lastName
+               phone:(nullable NSString *)phone
+         dateOfBirth:(nullable NSString *)dateOfBirth
+              gender:(nullable NSString *)gender
+                city:(nullable NSString *)city
+               state:(nullable NSString *)state
+                 zip:(nullable NSString *)zip
+             country:(nullable NSString *)country
           externalId:(nullable NSString *)externalId
 NS_SWIFT_NAME(setUser(email:firstName:lastName:phone:dateOfBirth:gender:city:state:zip:country:externalId:));
 // UNCRUSTIFY_FORMAT_ON
--- FBSDKCoreKit/FBSDKCoreKitTests/UserDataStoreTests.swift
+++ FBSDKCoreKit/FBSDKCoreKitTests/UserDataStoreTests.swift
@@ -14,6 +14,7 @@ final class UserDataStoreTests: XCTestCase {
 
   let store = _UserDataStore()
   let email = "apptest@fb.com"
+  let firstName = "Test User"
 
   override func setUp() {
     super.setUp()
@@ -50,7 +51,7 @@ final class UserDataStoreTests: XCTestCase {
 
   func testClearingUserDataByType() throws {
     store.setUserData(email, forType: .email)
-    store.setUserData(name, forType: .firstName)
+    store.setUserData(firstName, forType: .firstName)
     store.clearUserData(forType: .email)
 
     let retrieved = try XCTUnwrap(
@@ -67,6 +68,58 @@ final class UserDataStoreTests: XCTestCase {
     XCTAssertTrue(
       retrieved.contains("fn"),
       "Should not clear unspecified user data"
+    )
+  }
+
+  func testSettingUserDataWithoutExternalIdPreservesExistingExternalId() throws {
+    let externalId = "test_external_id_123"
+
+    // First, set the external ID explicitly
+    store.setUserData(externalId, forType: .externalId)
+
+    let initialData = try XCTUnwrap(
+      store.getUserData(),
+      "Should be able to retrieve initial user data"
+    )
+
+    XCTAssertTrue(
+      initialData.contains("external_id"),
+      "Should contain external_id after setting it"
+    )
+
+    // Now set other user data fields without passing external ID
+    store.setUser(
+      email: email,
+      firstName: "Test",
+      lastName: "User",
+      phone: "1234567890",
+      dateOfBirth: "01/01/1990",
+      gender: "m",
+      city: "Seattle",
+      state: "WA",
+      zip: "98101",
+      country: "US"
+    )
+
+    let finalData = try XCTUnwrap(
+      store.getUserData(),
+      "Should be able to retrieve final user data"
+    )
+
+    // Verify external ID is still present
+    XCTAssertTrue(
+      finalData.contains("external_id"),
+      "Should preserve external_id when setting other user data fields"
+    )
+
+    // Verify other fields were set
+    XCTAssertTrue(
+      finalData.contains("em"),
+      "Should contain email after setting user data"
+    )
+    XCTAssertTrue(
+      finalData.contains("fn"),
+      "Should contain first name after setting user data"
     )
   }
 }
